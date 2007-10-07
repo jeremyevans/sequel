2322cd20c9187b40ae92401456c1dab13dd270fb
Sharon Rosner <ciconia@gmail.com>
2007-10-07 10:58:50 +0000

    Added before_update, after_update hooks.

--- lib/sequel/model/hooks.rb
+++ lib/sequel/model/hooks.rb
@@ -21,6 +21,10 @@ module Sequel
       get_hooks(:before_create).unshift(block)
     end
     
+    def self.before_update(&block)
+      get_hooks(:before_update).unshift(block)
+    end
+    
     def self.before_destroy(&block)
       get_hooks(:before_destroy).unshift(block)
     end
@@ -31,6 +35,10 @@ module Sequel
     
     def self.after_create(&block)
       get_hooks(:after_create) << block
+    end
+    
+    def self.after_update(&block)
+      get_hooks(:after_update) << block
     end
     
     def self.after_destroy(&block)
