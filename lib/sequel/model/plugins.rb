4e35f987bf687d20fde2aff29a9663ba49581621
Sharon Rosner <ciconia@gmail.com>
2007-12-02 21:07:21 +0000

    doc for model/plugins.

--- lib/sequel/model/plugins.rb
+++ lib/sequel/model/plugins.rb
@@ -3,12 +3,15 @@ module Sequel
   
   class Model
     class << self
-      # Loads a plugin for use with the model class
+      # Loads a plugin for use with the model class, passing optional arguments
+      # to the plugin.
       def is(plugin, *args)
         plugin_module(plugin).apply(self, *args)
       end
       alias_method :is_a, :is
     
+      # Returns the module for the specified plugin. If the module is not 
+      # defined, the corresponding plugin gem is automatically loaded.
       def plugin_module(plugin)
         module_name = plugin.to_s.gsub(/(^|_)(.)/) {$2.upcase}
         if not Sequel::Plugins.const_defined?(module_name)
@@ -17,6 +20,7 @@ module Sequel
         Sequel::Plugins.const_get(module_name)
       end
 
+      # Returns the gem name for the given plugin.
       def plugin_gem(plugin)
         "sequel-#{plugin.to_s.gsub('_', '-')}"
       end
