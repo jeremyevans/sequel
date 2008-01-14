84d0e1ec82c7d56bb778e34457551e522393de32
Wayne Seguin <wayneeseguin@gmail.com>
2008-01-14 14:34:34 +0000

    Putting some more plugin structures in place. These plugins will hopefully be implemented soon.

---
+++ model_plugins/authenticated/lib/sequel_authenticated.rb
@@ -0,0 +1,26 @@
+module Sequel
+  module Plugins
+    module Authenticated
+      # Apply the plugin to the model.
+      def self.apply(model, options = {})
+        # Define each class method;
+        # model.class_def(:method_name) do |parameter1,parameter2,...|
+        #   dataset... # work with the dataset
+        # end
+
+        model.send(:include, InstanceMethods)
+      end
+
+      module InstanceMethods
+        # Define methods that will be instance-specific here.
+        def textile_to_html
+          # raw... to html
+        end
+
+        def markdown_to_html
+          # raw... to html
+        end
+      end
+    end
+  end
+end
