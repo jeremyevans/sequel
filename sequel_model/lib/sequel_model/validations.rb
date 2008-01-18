fa2792565b82a4a4cc224fb80a4405b5c88dc788
Sharon Rosner <ciconia@gmail.com>
2008-01-18 14:09:50 +0000

    docs for validations.

--- sequel_model/lib/sequel_model/validations.rb
+++ sequel_model/lib/sequel_model/validations.rb
@@ -1,27 +1,71 @@
 module Sequel
+  # The Validatable module provides validation capabilities as a mixin. When
+  # included into a class, it enhances the class with class and instance
+  # methods for defining validations and validating class instances.
+  # 
+  # The Validatable emulates the validation capabilities of ActiveRecord, and
+  # provides methods for validating acceptance, confirmation, presence, format, 
+  # length and numericality of attributes.
+  #
+  # To use validations, you need to include the Validatable module in your
+  # class:
+  #
+  #   class MyClass
+  #     include Sequel::Validatable
+  #     validates_length_of :password, :minimum => 6
+  #   end
   module Validatable
+    # Includes the Validatable class methods into the including class.
+    def self.included(c)
+      c.extend ClassMethods
+    end
+    
+    # Returns the validation errors associated with the object.
+    def errors
+      @errors ||= Errors.new
+    end
+
+    # Validates the object.
+    def validate
+      errors.clear
+      self.class.validate(self)
+    end
+
+    # Validates the object and returns true if no errors are reported.
+    def valid?
+      validate
+      errors.empty?
+    end
+
+    # Validatable::Errors represents validation errors.
     class Errors
+      # Initializes a new instance of validation errors.
       def initialize
         @errors = Hash.new {|h, k| h[k] = []}
       end
       
+      # Returns true if no errors are stored.
       def empty?
         @errors.empty?
       end
       
+      # Clears all errors.
       def clear
         @errors.clear
       end
       
+      # Returns the errors for the given attribute.
       def on(att)
         @errors[att]
       end
       alias_method :[], :on
       
+      # Adds an error for the given attribute.
       def add(att, msg)
         @errors[att] << msg
       end
       
+      # Returns an array of fully-formatted error messages.
       def full_messages
         @errors.inject([]) do |m, kv| att, errors = *kv
           errors.each {|e| m << "#{att} #{e}"}
@@ -30,59 +74,74 @@ module Sequel
       end
     end
     
+    # The Generator class is used to generate validation definitions using 
+    # the validates {} idiom.
     class Generator
+      # Initializes a new generator.
       def initialize(receiver ,&block)
         @receiver = receiver
         instance_eval(&block)
       end
 
+      # Delegates method calls to the receiver by calling receiver.validates_xxx.
       def method_missing(m, *args)
         @receiver.send(:"validates_#{m}", *args)
       end
     end
     
+    # Validatable class methods.
     module ClassMethods
+      # Defines validations by converting a longhand block into a series of 
+      # shorthand definitions. For example:
+      #
+      #   class MyClass
+      #     include Sequel::Validatable
+      #     validates do
+      #       length_of :name, :minimum => 6
+      #       length_of :password, :minimum => 8
+      #     end
+      #   end
+      #
+      # is equivalent to:
+      #   class MyClass
+      #     include Sequel::Validatable
+      #     validates_length_of :name, :minimum => 6
+      #     validates_length_of :password, :minimum => 8
+      #   end
       def validates(&block)
         Generator.new(self, &block)
       end
 
+      # Returns the validations hash for the class.
       def validations
         @validations ||= Hash.new {|h, k| h[k] = []}
       end
       
+      # Returns true if validations are defined.
       def has_validations?
         !validations.empty?
       end
 
+      # Validates the given instance.
       def validate(o)
         validations.each do |att, procs|
           v = o.send(att)
           procs.each {|p| p[o, att, v]}
         end
       end
-
+      
+      # Adds a validation for each of the given attributes using the supplied
+      # block. The block must accept three arguments: instance, attribute and 
+      # value, e.g.:
+      #
+      #   validates_each :name, :password do |object, attribute, value|
+      #     object.errors[attribute] << 'is not nice' unless value.nice?
+      #   end
       def validates_each(*atts, &block)
         atts.each {|a| validations[a] << block}
       end
-    end
 
-    def self.included(c)
-      c.extend ClassMethods
-    end
-
-    attr_accessor :errors
-
-    def validate
-      @errors = Errors.new
-      self.class.validate(self)
-    end
-
-    def valid?
-      validate
-      errors.empty?
-    end
-    
-    module ClassMethods
+      # Validates acceptance of an attribute.
       def validates_acceptance_of(*atts)
         opts = {
           :message => 'is not accepted',
@@ -96,6 +155,7 @@ module Sequel
         end
       end
 
+      # Validates confirmation of an attribute.
       def validates_confirmation_of(*atts)
         opts = {
           :message => 'is not confirmed',
@@ -108,6 +168,7 @@ module Sequel
         end
       end
 
+      # Validates the format of an attribute.
       def validates_format_of(*atts)
         opts = {
           :message => 'is invalid',
@@ -119,10 +180,11 @@ module Sequel
         
         validates_each(*atts) do |o, a, v|
           next if (v.nil? && opts[:allow_nil]) || (v.blank? && opts[:allow_blank])
-          o.errors[a] << opts[:message] unless v =~ opts[:with]
+          o.errors[a] << opts[:message] unless v.to_s =~ opts[:with]
         end
       end
 
+      # Validates the length of an attribute.
       def validates_length_of(*atts)
         opts = {
           :too_long     => 'is too long',
@@ -150,6 +212,7 @@ module Sequel
       NUMBER_RE = /^\d*\.{0,1}\d+$/
       INTEGER_RE = /\A[+-]?\d+\Z/
 
+      # Validates whether an attribute is a number.
       def validates_numericality_of(*atts)
         opts = {
           :message => 'is not a number',
@@ -163,6 +226,7 @@ module Sequel
         end
       end
 
+      # Validates the presence of an attribute.
       def validates_presence_of(*atts)
         opts = {
           :message => 'is not present',
