c42ec86517f2ee26601f681e75f77be85865f601
Sharon Rosner <ciconia@gmail.com>
2008-01-23 13:55:05 +0000

    Changed ODBC::Database to support connection using driver and database name, also added support for untitled columns in ODBC::Dataset (thanks Leonid Borisenko).

--- sequel_core/lib/sequel_core/adapters/odbc.rb
+++ sequel_core/lib/sequel_core/adapters/odbc.rb
@@ -5,12 +5,34 @@ module Sequel
     class Database < Sequel::Database
       set_adapter_scheme :odbc
     
+      # def connect
+      #   conn = ::ODBC::connect(@opts[:database], @opts[:user], @opts[:password])
+      #   conn.autocommit = true
+      #   conn
+      # end
+
+      GUARDED_DRV_NAME = /^\{.+\}$/.freeze
+      DRV_NAME_GUARDS = '{%s}'.freeze
+
       def connect
-        conn = ::ODBC::connect(@opts[:database], @opts[:user], @opts[:password])
+        if @opts.include? :driver
+          drv = ::ODBC::Driver.new
+          drv.name = 'Sequel ODBC Driver130'
+          @opts.each do |param, value|
+            if :driver == param and not (value =~ GUARDED_DRV_NAME)
+              value = DRV_NAME_GUARDS % value
+            end
+            drv.attrs[param.to_s.capitalize] = value
+          end
+          db = ::ODBC::Database.new
+          conn = db.drvconnect(drv)
+        else
+          conn = ::ODBC::connect(@opts[:database], @opts[:user], @opts[:password])
+        end
         conn.autocommit = true
         conn
-      end
-      
+      end      
+
       def disconnect
         @pool.disconnect {|c| c.disconnect}
       end
@@ -49,19 +71,41 @@ module Sequel
         end
       end
 
+      UNTITLED_COLUMN = 'untitled_%d'.freeze
+
       def fetch_rows(sql, &block)
         @db.synchronize do
           s = @db.execute sql
           begin
-            @columns = s.columns(true).map {|c| c.name.to_sym}
+            untitled_count = 0
+            @columns = s.columns(true).map do |c|
+              if (n = c.name).empty?
+                n = UNTITLED_COLUMN % (untitled_count += 1)
+              end
+              n.to_sym
+            end
             rows = s.fetch_all
-            rows.each {|row| yield hash_row(row)}
+            rows.each {|row| yield hash_row(row)} if rows
           ensure
             s.drop unless s.nil? rescue nil
           end
         end
         self
       end
+    
+      # def fetch_rows(sql, &block)
+      #   @db.synchronize do
+      #     s = @db.execute sql
+      #     begin
+      #       @columns = s.columns(true).map {|c| c.name.to_sym}
+      #       rows = s.fetch_all
+      #       rows.each {|row| yield hash_row(row)}
+      #     ensure
+      #       s.drop unless s.nil? rescue nil
+      #     end
+      #   end
+      #   self
+      # end
       
       def hash_row(row)
         hash = {}
