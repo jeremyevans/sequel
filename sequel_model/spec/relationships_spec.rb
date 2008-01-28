37183ebfc2f35facdd34366f6eb78668f2710bb3
Lance Carlson <lancecarlson@gmail.com>
2008-01-28 02:16:41 +0000

    Updated relationship specs

--- sequel_model/spec/relationships_spec.rb
+++ sequel_model/spec/relationships_spec.rb
@@ -10,30 +10,34 @@ require File.join(File.dirname(__FILE__), "spec_helper")
 #     has :many, :authors, :required => true  # authors_posts join table, requires at least one author
 #   end
 # end
+
+class User < Sequel::Model; end
+
 describe Sequel::Model, "relationships" do
-  before :all do
-    class Smurf < Sequel::Model
-    end
-  end
-  
-  after :each do
-    Smurf.model_relationships.clear
-  end
   
   describe "has" do
 
   end
   
   describe "has_one" do
-
+    it "should pass arguments to has :one" do
+      User.should_receive(:has).with(:one, :boss, {}).and_return(true)
+      User.send(:has_one, :boss)
+    end
   end
   
   describe "has_many" do
-
+    it "should pass arguments to has :many" do
+      User.should_receive(:has).with(:many, :addresses, {}).and_return(true)
+      User.send(:has_many, :addresses)
+    end
   end
 
   describe "belongs_to" do
-
+    it "should pass arguments to has :one" do
+      User.should_receive(:has).with(:one, :boss, {}).and_return(true)
+      User.send(:belongs_to, :boss)
+    end
   end
   
   describe "has_relationships?" do
