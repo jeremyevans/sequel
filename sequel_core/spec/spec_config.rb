dc31229cac5381d96f16aab03817fed012baf59b
Sharon Rosner <ciconia@gmail.com>
2008-03-08 17:49:19 +0000

    Removed config file.

--- sequel_core/spec/spec_config.rb
+++
@@ -1,6 +0,0 @@
-# connection URL's for running adapter specs
-INFORMIX_URL    = 'informix://localhost/mydb'
-MYSQL_URL       = 'mysql://root@localhost/sandbox'
-ORACLE_URL      = 'oracle://hr:hr@localhost/XE'
-POSTGRES_URL    = 'postgres://postgres:postgres@localhost:5432/reality_spec'
-SQLITE_URL      = 'sqlite:/'
