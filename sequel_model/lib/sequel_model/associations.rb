07cc3c816e65fdd2b07b5535c24fa146fe0b9cd0
Jeremy Evans <code@jeremyevans.net>
2008-04-04 19:19:56 -0700

    Minor eager loading refactoring and documentation improvements
    Change symbols used for storing eager loading information in the reflection
    Make many_to_many associations select be more database independent by using symbols instead of .lit
    Document that select option can be used for many_to_many associations to change what is selected
    Document that you can order by multiple columns
    Other documentation improvements
    Add some specs for multicolumn ordering and the new many_to_many :select option
    Order methods alphabetically in associations.rb

--- sequel_model/lib/sequel_model/associations.rb
+++ sequel_model/lib/sequel_model/associations.rb
@@ -6,8 +6,8 @@
 # given in the definition. Example:
 # 
 #   class Project < Sequel::Model
-#     belongs_to :portfolio
-#     has_many   :milestones
+#     many_to_one :portfolio
+#     one_to_many :milestones
 #   end
 # 
 # The project class now has the following methods:
@@ -17,7 +17,7 @@
 #
 # By default the classes for the associations are inferred from the association
 # name, so for example the Project#portfolio will return an instance of 
-# Portfolio, and Project#milestones will return a dataset of Milestone 
+# Portfolio, and Project#milestones will return an array of Milestone 
 # instances, in similar fashion to how ActiveRecord infers class names.
 #
 # Association definitions are also reflected by the class, e.g.:
@@ -26,24 +26,6 @@
 #   => [:portfolio, :milestones]
 #   >> Project.association_reflection(:portfolio)
 #   => {:type => :many_to_one, :name => :portfolio, :class_name => "Portfolio"}
-#
-# The following association kinds are supported:
-# * :many_to_one - Foreign key in current model's table points to 
-#   associated model's primary key.  Each associated model object can
-#   be associated with more than one current model objects.  Each current
-#   model object can be associated with only one associated model object.
-#   Similar to ActiveRecord/DataMapper's belongs_to.
-# * :one_to_many - Foreign key in associated model's table points to this
-#   model's primary key.   Each current model object can be associated with
-#   more than one associated model objects.  Each associated model object
-#   can be associated with only one current model object.
-#   Similar to ActiveRecord/DataMapper's has_many.
-# * :many_to_many - A join table is used that has a foreign key that points
-#   to this model's primary key and a foreign key that points to the
-#   associated model's primary key.  Each current model object can be
-#   associated with many associated model objects, and each associated
-#   model object can be associated with many current model objects.
-#   Similar to ActiveRecord/DataMapper's has_and_belongs_to_many.
 #
 # Associations can be defined by either using the associate method, or by
 # calling one of the three methods: many_to_one, one_to_many, many_to_many.
@@ -96,7 +78,8 @@ module Sequel::Model::Associations
   #   - :key - foreign key in associated model's table that references
   #     current model's primary key, as a symbol.  Defaults to
   #     :"#{self.name.underscore}_id".
-  #   - :order - the column by which to order the association dataset.
+  #   - :order - the column(s) by which to order the association dataset.  Can be a
+  #     singular column or an array.
   # * :many_to_many:
   #   - :join_table - name of table that includes the foreign keys to both
   #     the current model and the associated model, as a symbol.  Defaults to the name
@@ -106,7 +89,14 @@ module Sequel::Model::Associations
   #     primary key, as a symbol.
   #   - :right_key - foreign key in join table that points to associated
   #     model's primary key, as a symbol.
-  #   - :order - the column by which to order the association dataset.
+  #   - :select - the attributes to select.  Defaults to the associated class's
+  #     table_name.*, which means it doesn't include the attributes from the join
+  #     join table.  If you want to include the join table attributes, you can
+  #     use this option, but beware that the join table attributes can clash with
+  #     attributes from the model table, so you should alias any attributes that have
+  #     the same name in both the join table and the associated table.
+  #   - :order - the column(s) by which to order the association dataset. Can be a
+  #     singular column or an array.
   def associate(type, name, opts = {}, &block)
     # check arguments
     raise ArgumentError unless [:many_to_one, :one_to_many, :many_to_many].include?(type) && Symbol === name
@@ -145,24 +135,24 @@ module Sequel::Model::Associations
     association_reflections.keys
   end
 
-  # Shortcut for adding a many_to_one association, see associate
-  def many_to_one(*args, &block)
-    associate(:many_to_one, *args, &block)
-  end
-  alias_method :belongs_to, :many_to_one
-  
-  # Shortcut for adding a one_to_many association, see associate
-  def one_to_many(*args, &block)
-    associate(:one_to_many, *args, &block)
-  end
-  alias_method :has_many, :one_to_many
-  
   # deprecated, please use many_to_one instead
   def one_to_one(*args, &block)
     STDERR << "one_to_one relation definitions are deprecated, please use many_to_one instead.\r\n"
     many_to_one(*args, &block)
   end
 
+  # Shortcut for adding a one_to_many association, see associate
+  def one_to_many(*args, &block)
+    associate(:one_to_many, *args, &block)
+  end
+  alias_method :has_many, :one_to_many
+  
+  # Shortcut for adding a many_to_one association, see associate
+  def many_to_one(*args, &block)
+    associate(:many_to_one, *args, &block)
+  end
+  alias_method :belongs_to, :many_to_one
+  
   # Shortcut for adding a many_to_many association, see associate
   def many_to_many(*args, &block)
     associate(:many_to_many, *args, &block)
@@ -170,121 +160,30 @@ module Sequel::Model::Associations
   alias_method :has_and_belongs_to_many, :many_to_many
   
   private
-  def association_ivar(name)
-    :"@#{name}"
-  end
-  
-  def association_add_method_name(name)
-    :"add_#{name.to_s.singularize}"
-  end
-
-  def association_remove_method_name(name)
-    :"remove_#{name.to_s.singularize}"
-  end
-  
-  def default_remote_key
-    :"#{name.demodulize.underscore}_id"
-  end
-
   # The class related to the given association reflection
   def associated_class(opts)
     opts[:class] ||= opts[:class_name].constantize
   end
 
+  # Name symbol for add association method
+  def association_add_method_name(name)
+    :"add_#{name.to_s.singularize}"
+  end
+
+  # Name symbol of association instance variable
+  def association_ivar(name)
+    :"@#{name}"
+  end
+  
+  # Name symbol for remove_method_name
+  def association_remove_method_name(name)
+    :"remove_#{name.to_s.singularize}"
+  end
+  
   # Hash storing the association reflections.  Keys are association name
   # symbols, values are association reflection hashes.
   def association_reflections
     @association_reflections ||= {}
-  end
-  
-  def def_many_to_one(name, opts)
-    assoc_class = method(:associated_class) # late binding of association dataset
-    ivar = association_ivar(name)
-    
-    key = (opts[:key] ||= :"#{name}_id")
-    opts[:class_name] ||= name.to_s.camelize
-    
-    def_association_getter(name) {(fk = send(key)) ? assoc_class[opts][fk] : nil}
-    class_def(:"#{name}=") do |o|
-      instance_variable_set(ivar, o)
-      send(:"#{key}=", (o.pk if o))
-    end
-  end
-  
-  def def_one_to_many(name, opts)
-    assoc_class = method(:associated_class) # late binding of association dataset
-    ivar = association_ivar(name)
-    key = (opts[:key] ||= default_remote_key)
-    opts[:class_name] ||= name.to_s.singularize.camelize
-    
-    def_association_dataset_methods(name, opts) {assoc_class[opts].filter(key => pk)}
-    
-    # define add_xxx, remove_xxx methods
-    class_def(association_add_method_name(name)) do |o|
-      o.send(:"#{key}=", pk); o.save!
-      if arr = instance_variable_get(ivar)
-        arr.push(o)
-      end
-      o
-    end
-    class_def(association_remove_method_name(name)) do |o|
-      o.send(:"#{key}=", nil); o.save!
-      if arr = instance_variable_get(ivar)
-        arr.delete(o)
-      end
-      o
-    end
-  end
-  
-  def default_join_table_name(opts)
-    ([opts[:class_name], self.name.demodulize]. \
-      map{|i| i.pluralize.underscore}.sort.join('_')).to_sym
-  end
-  
-  def def_many_to_many(name, opts)
-    assoc_class = method(:associated_class) # late binding of association dataset
-    ivar = association_ivar(name)
-    left = (opts[:left_key] ||= default_remote_key)
-    right = (opts[:right_key] ||= :"#{name.to_s.singularize}_id")
-    opts[:class_name] ||= name.to_s.singularize.camelize
-    join_table = (opts[:join_table] ||= default_join_table_name(opts))
-    database = db
-    
-    def_association_dataset_methods(name, opts) do
-      klass = assoc_class[opts]
-      key = (opts[:right_primary_key] ||= :"#{klass.table_name}__#{klass.primary_key}")
-      selection = (opts[:selection] ||= "#{klass.table_name}.*".lit)
-      klass.select(selection).inner_join(join_table, right => key, left => pk)
-    end
-
-    class_def(association_add_method_name(name)) do |o|
-      database[join_table].insert(left => pk, right => o.pk)
-      if arr = instance_variable_get(ivar)
-        arr.push(o)
-      end
-      o
-    end
-    class_def(association_remove_method_name(name)) do |o|
-      database[join_table].filter(left => pk, right => o.pk).delete
-      if arr = instance_variable_get(ivar)
-        arr.delete(o)
-      end
-      o
-    end
-  end
-  
-  # Defines an association getter method, caching the block result in an 
-  # instance variable. The defined method takes an optional reload parameter
-  # that can be set to true in order to bypass the cache.
-  def def_association_getter(name, &block)
-    ivar = association_ivar(name)
-    class_def(name) do |*reload|
-      if !reload[0] && obj = instance_variable_get(ivar)
-        obj
-      else
-        instance_variable_set(ivar, instance_eval(&block))
-      end
-    end
   end
   
   # Defines an association
@@ -329,6 +228,107 @@ module Sequel::Model::Associations
     end
   end
   
+  # Defines an association getter method, caching the block result in an 
+  # instance variable. The defined method takes an optional reload parameter
+  # that can be set to true in order to bypass the cache.
+  def def_association_getter(name, &block)
+    ivar = association_ivar(name)
+    class_def(name) do |*reload|
+      if !reload[0] && obj = instance_variable_get(ivar)
+        obj
+      else
+        instance_variable_set(ivar, instance_eval(&block))
+      end
+    end
+  end
+
+  # Adds many_to_many association instance methods
+  def def_many_to_many(name, opts)
+    assoc_class = method(:associated_class) # late binding of association dataset
+    ivar = association_ivar(name)
+    left = (opts[:left_key] ||= default_remote_key)
+    right = (opts[:right_key] ||= :"#{name.to_s.singularize}_id")
+    opts[:class_name] ||= name.to_s.singularize.camelize
+    join_table = (opts[:join_table] ||= default_join_table_name(opts))
+    database = db
+    
+    def_association_dataset_methods(name, opts) do
+      klass = assoc_class[opts]
+      key = (opts[:right_primary_key] ||= :"#{klass.table_name}__#{klass.primary_key}")
+      selection = (opts[:select] ||= klass.table_name.all)
+      klass.select(selection).inner_join(join_table, right => key, left => pk)
+    end
+
+    class_def(association_add_method_name(name)) do |o|
+      database[join_table].insert(left => pk, right => o.pk)
+      if arr = instance_variable_get(ivar)
+        arr.push(o)
+      end
+      o
+    end
+    class_def(association_remove_method_name(name)) do |o|
+      database[join_table].filter(left => pk, right => o.pk).delete
+      if arr = instance_variable_get(ivar)
+        arr.delete(o)
+      end
+      o
+    end
+  end
+  
+  # Adds many_to_one association instance methods
+  def def_many_to_one(name, opts)
+    assoc_class = method(:associated_class) # late binding of association dataset
+    ivar = association_ivar(name)
+    
+    key = (opts[:key] ||= :"#{name}_id")
+    opts[:class_name] ||= name.to_s.camelize
+    
+    def_association_getter(name) {(fk = send(key)) ? assoc_class[opts][fk] : nil}
+    class_def(:"#{name}=") do |o|
+      instance_variable_set(ivar, o)
+      send(:"#{key}=", (o.pk if o))
+    end
+  end
+  
+  # Adds one_to_many association instance methods
+  def def_one_to_many(name, opts)
+    assoc_class = method(:associated_class) # late binding of association dataset
+    ivar = association_ivar(name)
+    key = (opts[:key] ||= default_remote_key)
+    opts[:class_name] ||= name.to_s.singularize.camelize
+    
+    def_association_dataset_methods(name, opts) {assoc_class[opts].filter(key => pk)}
+    
+    class_def(association_add_method_name(name)) do |o|
+      o.send(:"#{key}=", pk)
+      o.save!
+      if arr = instance_variable_get(ivar)
+        arr.push(o)
+      end
+      o
+    end
+    class_def(association_remove_method_name(name)) do |o|
+      o.send(:"#{key}=", nil)
+      o.save!
+      if arr = instance_variable_get(ivar)
+        arr.delete(o)
+      end
+      o
+    end
+  end
+  
+  # Name symbol for default join table
+  def default_join_table_name(opts)
+    ([opts[:class_name], self.name.demodulize]. \
+      map{|i| i.pluralize.underscore}.sort.join('_')).to_sym
+  end
+  
+  # Name symbol for default foreign key
+  def default_remote_key
+    :"#{name.demodulize.underscore}_id"
+  end
+
+  # Sets the reciprocal association variable in the reflection, if one exists
   def reciprocal_association(reflection)
     if reflection[:type] != :one_to_many
       nil
