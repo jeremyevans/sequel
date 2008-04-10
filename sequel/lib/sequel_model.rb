819b6b09db03d0f5bf6a07f59175eeccf002f6a9
Jeremy Evans <code@jeremyevans.net>
2008-04-10 07:30:50 -0700

    Update Associations section of Sequel::Model RDoc

--- sequel/lib/sequel_model.rb
+++ sequel/lib/sequel_model.rb
@@ -150,22 +150,11 @@ module Sequel
   # Associations are defined in similar fashion to ActiveRecord:
   #
   #   class Post < Sequel::Model
-  #     belongs_to :author
+  #     many_to_one :author
   #   end
   #
   #   class Author < Sequel::Model
-  #     has_many :posts
-  #   end
-  # 
-  # Another way to define an association in a Sequel model is as a regular
-  # instance method:
-  # 
-  #   class Post < Sequel::Model
-  #     def author; Author[author_id]; end
-  #   end
-  # 
-  #   class Author < Sequel::Model
-  #     def posts; Post.filter(:author_id => pk); end
+  #     one_to_many :posts
   #   end
   # 
   # === Caching model instances with memcached
