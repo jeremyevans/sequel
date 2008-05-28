21fb70589e5bb644ad48ccf6918daaeae27642b9
Jeremy Evans <code@jeremyevans.net>
2008-05-28 16:04:06 -0700

    Update documentation in worker.rb

--- sequel_core/lib/sequel_core/worker.rb
+++ sequel_core/lib/sequel_core/worker.rb
@@ -1,8 +1,14 @@
 module Sequel
+  # A Worker is a thread that accepts jobs off a work queue and
+  # processes them in the background.  It accepts an optional
+  # database where it wruns all of its work inside a transaction.
   class Worker < Thread
     attr_reader :queue
     attr_reader :errors
   
+    # Setup the interal variables.  If a database is given,
+    # run the thread inside a database transaction. Continue
+    # to work until #join is called.
     def initialize(db = nil)
       @queue = Queue.new
       @errors = []
@@ -12,6 +18,8 @@ module Sequel
       db ? super {db.transaction {t.work}} : super {t.work}
     end
     
+    # Add a job to the queue, specified either as a proc argument
+    # or as a block.
     def async(proc = nil, &block)
       @queue << (proc || block)
       self
@@ -19,18 +27,19 @@ module Sequel
     alias_method :add, :async
     alias_method :<<, :async
   
+    # Whether the worker is actively working and/or has work scheduled
     def busy?
       @cur || !@queue.empty?
     end
   
+    # Wait until the worker is no longer busy and then stop working.
     def join
-      while busy?
-        sleep 0.1
-      end
+      sleep(0.1) while busy?
       self.raise Error::WorkerStop
       super
     end
 
+    # Continually get jobs from the work queue and process them.
     def work
       begin
         loop {next_job}
@@ -41,6 +50,8 @@ module Sequel
     end
     
     private
+
+    # Get the next job from the work queue and process it.
     def next_job
       begin
         @cur = @queue.pop
