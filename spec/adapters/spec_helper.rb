4519844f175ab006c6ea9c64fffbef4ef5d77a1b
Jeremy Evans <code@jeremyevans.net>
2008-07-10 10:22:29 -0700

    Load sequel_core before spec_config in the adapter specs

--- spec/adapters/spec_helper.rb
+++ spec/adapters/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
-require File.join(File.dirname(__FILE__), '../spec_config.rb')
 unless Object.const_defined?('Sequel')
   $:.unshift(File.join(File.dirname(__FILE__), "../../lib/"))
   require 'sequel_core'
   Sequel.quote_identifiers = false
 end
+require File.join(File.dirname(__FILE__), '../spec_config.rb')
