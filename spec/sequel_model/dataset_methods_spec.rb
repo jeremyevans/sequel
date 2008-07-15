d826da73882d9699b7ad8cb1cd6bf3f5d0ee4b8d
Jeremy Evans <code@jeremyevans.net>
2008-07-15 13:26:10 -0700

    Add a spec for Model.to_hash with a key specified

--- spec/sequel_model/dataset_methods_spec.rb
+++ spec/sequel_model/dataset_methods_spec.rb
@@ -56,7 +56,19 @@ describe Sequel::Model::DatasetMethods, "#to_hash"  do
     h.should be_a_kind_of(Hash)
     a = h.to_a
     a.collect{|x| x[1].class}.should == [@c, @c]
-    [[[1, {:name=>1}], [2, {:name=>2}]], [[2, {:name=>2}], [1, {:name=>1}]]].should(include(a.collect{|x| [x[0], x[1].values]}))
+    a.sort_by{|x| x[0]}.collect{|x| [x[0], x[1].values]}.should == [[1, {:name=>1}], [2, {:name=>2}]]
+  end
+
+  it "should result in a hash with given value keys and model object values" do
+    def @d.fetch_rows(sql)
+      yield({:name=>1, :number=>3})
+      yield({:name=>2, :number=>4})
+    end
+    h = @d.to_hash(:number)
+    h.should be_a_kind_of(Hash)
+    a = h.to_a
+    a.collect{|x| x[1].class}.should == [@c, @c]
+    a.sort_by{|x| x[0]}.collect{|x| [x[0], x[1].values]}.should == [[3, {:name=>1, :number=>3}], [4, {:name=>2, :number=>4}]]
   end
 
   it "should raise an error if the class doesn't have a primary key" do
