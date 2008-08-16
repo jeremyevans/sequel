2ec2acd4d685d6834b130802000b1f5dd58da7d9
Jeremy Evans <code@jeremyevans.net>
2008-08-16 14:06:39 -0700

    Update some RDoc comments in sequel_model/schema.rb

--- lib/sequel_model/schema.rb
+++ lib/sequel_model/schema.rb
@@ -1,13 +1,14 @@
 module Sequel
   class Model
-    # Creates table.
+    # Creates table, using the column information from set_schema.
     def self.create_table
       db.create_table(table_name, @schema)
       @db_schema = get_db_schema(true) unless @@lazy_load_schema
       columns
     end
     
-    # Drops the table if it exists and then runs create_table.
+    # Drops the table if it exists and then runs create_table.  Should probably
+    # not be used except in testing.
     def self.create_table!
       drop_table rescue nil
       create_table
@@ -27,8 +28,13 @@ module Sequel
 
     # Defines a table schema (see Schema::Generator for more information).
     #
-    # This is only needed if you want to use the create_table or drop_table
-    # methods.
+    # This is only needed if you want to use the create_table/create_table! methods.
+    # Will also set the dataset if you provide a name, as well as setting
+    # the primary key if you defined one in the passed block.
+    #
+    # In general, it is a better idea to use migrations for production code, as
+    # migrations allow changes to existing schema.  set_schema is mostly useful for
+    # test code or simple examples.
     def self.set_schema(name = nil, &block)
       set_dataset(db[name]) if name
       @schema = Schema::Generator.new(db, &block)
