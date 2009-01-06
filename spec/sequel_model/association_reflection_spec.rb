557b4c58c30bcb489c6060159d741e31d1f913d7
Jeremy Evans <code@jeremyevans.net>
2009-01-06 09:11:07 -0800

    Minor change to AssociationReflection specs for greater compatibility

--- spec/sequel_model/association_reflection_spec.rb
+++ spec/sequel_model/association_reflection_spec.rb
@@ -8,12 +8,12 @@ describe Sequel::Model::Associations::AssociationReflection, "#associated_class"
 
   it "should use the :class value if present" do
     @c.many_to_one :c, :class=>ParParent
-    @c.association_reflection(:c).should include(:class)
+    @c.association_reflection(:c).keys.should include(:class)
     @c.association_reflection(:c).associated_class.should == ParParent
   end
   it "should figure out the class if the :class value is not present" do
     @c.many_to_one :c, :class=>'ParParent'
-    @c.association_reflection(:c).should_not include(:class)
+    @c.association_reflection(:c).keys.should_not include(:class)
     @c.association_reflection(:c).associated_class.should == ParParent
   end
 end
@@ -26,12 +26,12 @@ describe Sequel::Model::Associations::AssociationReflection, "#primary_key" do
 
   it "should use the :primary_key value if present" do
     @c.many_to_one :c, :class=>ParParent, :primary_key=>:blah__blah
-    @c.association_reflection(:c).should include(:primary_key)
+    @c.association_reflection(:c).keys.should include(:primary_key)
     @c.association_reflection(:c).primary_key.should == :blah__blah
   end
   it "should use the associated table's primary key if :primary_key is not present" do
     @c.many_to_one :c, :class=>'ParParent'
-    @c.association_reflection(:c).should_not include(:primary_key)
+    @c.association_reflection(:c).keys.should_not include(:primary_key)
     @c.association_reflection(:c).primary_key.should == :id
   end
 end
@@ -41,7 +41,7 @@ describe Sequel::Model::Associations::AssociationReflection, "#reciprocal" do
     @c = Class.new(Sequel::Model)
     @d = Class.new(Sequel::Model)
     @c.many_to_one :c, :class=>@d, :reciprocal=>:xx
-    @c.association_reflection(:c).should include(:reciprocal)
+    @c.association_reflection(:c).keys.should include(:reciprocal)
     @c.association_reflection(:c).reciprocal.should == :xx
   end
 
@@ -54,13 +54,13 @@ describe Sequel::Model::Associations::AssociationReflection, "#reciprocal" do
     ParParent.many_to_many :par_parent_threes
     ParParentThree.many_to_many :par_parents
 
-    ParParent.association_reflection(:par_parent_two).should_not include(:reciprocal)
+    ParParent.association_reflection(:par_parent_two).keys.should_not include(:reciprocal)
     ParParent.association_reflection(:par_parent_two).reciprocal.should == :par_parents
-    ParParentTwo.association_reflection(:par_parents).should_not include(:reciprocal)
+    ParParentTwo.association_reflection(:par_parents).keys.should_not include(:reciprocal)
     ParParentTwo.association_reflection(:par_parents).reciprocal.should == :par_parent_two
-    ParParent.association_reflection(:par_parent_threes).should_not include(:reciprocal)
+    ParParent.association_reflection(:par_parent_threes).keys.should_not include(:reciprocal)
     ParParent.association_reflection(:par_parent_threes).reciprocal.should == :par_parents
-    ParParentThree.association_reflection(:par_parents).should_not include(:reciprocal)
+    ParParentThree.association_reflection(:par_parents).keys.should_not include(:reciprocal)
     ParParentThree.association_reflection(:par_parents).reciprocal.should == :par_parent_threes
   end
 end
@@ -73,20 +73,20 @@ describe Sequel::Model::Associations::AssociationReflection, "#select" do
 
   it "should use the :select value if present" do
     @c.many_to_one :c, :class=>ParParent, :select=>[:par_parents__id]
-    @c.association_reflection(:c).should include(:select)
+    @c.association_reflection(:c).keys.should include(:select)
     @c.association_reflection(:c).select.should == [:par_parents__id]
   end
   it "should be the associated_table.* if :select is not present for a many_to_many associaiton" do
     @c.many_to_many :cs, :class=>'ParParent'
-    @c.association_reflection(:cs).should_not include(:select)
+    @c.association_reflection(:cs).keys.should_not include(:select)
     @c.association_reflection(:cs).select.should == :par_parents.*
   end
   it "should be if :select is not present for a many_to_one and one_to_many associaiton" do
     @c.one_to_many :cs, :class=>'ParParent'
-    @c.association_reflection(:cs).should_not include(:select)
+    @c.association_reflection(:cs).keys.should_not include(:select)
     @c.association_reflection(:cs).select.should == nil
     @c.many_to_one :c, :class=>'ParParent'
-    @c.association_reflection(:c).should_not include(:select)
+    @c.association_reflection(:c).keys.should_not include(:select)
     @c.association_reflection(:c).select.should == nil
   end
 end
