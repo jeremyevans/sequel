9b429ccc50502ddf1933e5d6871afed1c79837e6
Jeremy Evans <code@jeremyevans.net>
2009-03-26 21:01:07 -0700

    Add validation_helpers model plugin, which adds instance level validation support similar to previously standard validations, with a different API
    This plugin should satisfy most validation needs.  All previously
    standard validations were ported except for acceptance_of and
    confirmation_of, both of which are anti-patterns, as they encourage
    error checking in the model that should be done in the controller.
    Model validations should deal with the errors in the model's data,
    not whether a user checked a box or put an identical value in a
    confirmation field.
    
    validates_each was not ported as it is much easier to just write your
    own validation code if a standard validation doesn't handle it:
    
      # Old class level validation
      validates_each(:date) do |o,a,v|
        o.errors[a] << '...' unless v > Date.today
      end
    
      # New instance level validation
      def validate
        errors[:date] << '...' unless date > Date.today
      end
    
    The other validations were mostly renamed:
    
      # Old class level validations
      validates_format_of :col, :with=>/.../
      validates_length_of :col, :maximum=>5
      validates_length_of :col, :minimum=>3
      validates_length_of :col, :is=>4
      validates_length_of :col, :within=>3..5
      validates_not_string :col
      validates_numericality_of :col
      validates_numericality_of :col, :only_integer=>true
      validates_presence_of :col
      validates_inclusion_of :col, :in=>[3, 4, 5]
      validates_uniqueness_of :col, :col2
      validates_uniqueness_of([:col, :col2])
    
      # New instance level validations
      def validate
        validates_format /.../, :col
        validates_max_length 5, :col
        validates_min_length 3, :col
        validates_exact_length 4, :col
        validates_length_range 3..5, :col
        validates_not_string :col
        validates_numeric :col
        validates_integer :col
        validates_presence :col
        validates_includes([3,4,5], :col)
        validates_unique :col, :col2
        validates_unique([:col, :col2])
      end
    
    Another change made is to specify the same type of validation on
    multiple attributes, you must use an array:
    
      # Old
      validates_length_of :name, :password, :within=>3..5
    
      # New
      def validate
        validates_length_range 3..5, [:name, :password]
      end
    
    The :message, :allow_blank, :allow_missing, and :allow_nil options
    are still respected.  The :tag option is not needed as instance level
    validations work with code reloading without workarounds. The :if
    option is also not needed for instance level validations:
    
      # Old
      validates_presence_of :name, :if=>:new?
      validates_presence_of :pass, :if=>{flag > 3}
    
      # New
      def validate
        validates_presence(:name) if new?
        validates_presence(:pass) if flag > 3
      end
    
    validates_unique is a little different than the other new
    validations.  It doesn't accept the :allow_* options, and you can
    specify multiple attributes instead of using an array.  If you
    use an array, it is intepreted that you want the combination of
    values unique, instead of each value unique, which is how it
    operated previously.  The new uniqueness logic is also much
    simpler and hopefully more robust, though you should still have
    a unique database index for integrity purposes.
    
    The validation_helpers plugin is half the size of the
    validation_class_methods plugin, and has better specs, IMO.
    
    This commit also removes some duplicative code from the
    validation_class_methods specs.

---
+++ spec/extensions/validation_helpers_spec.rb
@@ -0,0 +1,281 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe "Sequel::Plugins::ValidationHelpers" do
+  before do
+    @c = Class.new(Sequel::Model) do
+      def self.set_validations(&block)
+        define_method(:validate, &block)
+      end
+      columns :value
+    end
+    @c.plugin :validation_helpers
+    @m = @c.new
+  end
+
+  specify "should take an :allow_blank option" do
+    @c.set_validations{validates_format(/.+_.+/, :value, :allow_blank=>true)}
+    @m.value = 'abc_'
+    @m.should_not be_valid
+    o = Object.new
+    @m.value = o
+    @m.should_not be_valid
+    def o.blank?
+      true
+    end
+    @m.should be_valid
+  end
+
+  specify "should take an :allow_missing option" do
+    @c.set_validations{validates_format(/.+_.+/, :value, :allow_missing=>true)}
+    @m.values.clear
+    @m.should be_valid
+    @m.value = nil
+    @m.should_not be_valid
+  end
+
+  specify "should take an :allow_blank option" do
+    @c.set_validations{validates_format(/.+_.+/, :value, :allow_nil=>true)}
+    @m.value = 'abc_'
+    @m.should_not be_valid
+    @m.value = nil
+    @m.should be_valid
+  end
+
+  specify "should take a :message option" do
+    @c.set_validations{validates_format(/.+_.+/, :value, :message=>"is so blah")}
+    @m.value = 'abc_'
+    @m.should_not be_valid
+    @m.errors.full_messages.should == ['value is so blah']
+  end
+
+  specify "should take multiple attributes in the same call" do
+    @c.columns :value, :value2
+    @c.set_validations{validates_presence([:value, :value2])}
+    @m.should_not be_valid
+    @m.value = 1
+    @m.should_not be_valid
+    @m.value2 = 1
+    @m.should be_valid
+  end
+
+  specify "should support validates_exact_length" do
+    @c.set_validations{validates_exact_length(3, :value)}
+    @m.should_not be_valid
+    @m.value = '123'
+    @m.should be_valid
+    @m.value = '12'
+    @m.should_not be_valid
+    @m.value = '1234'
+    @m.should_not be_valid
+  end
+  
+  specify "should support validate_format" do
+    @c.set_validations{validates_format(/.+_.+/, :value)}
+    @m.value = 'abc_'
+    @m.should_not be_valid
+    @m.value = 'abc_def'
+    @m.should be_valid
+  end
+  
+  specify "should support validates_includes with an array" do
+    @c.set_validations{validates_includes([1,2], :value)}
+    @m.should_not be_valid
+    @m.value = 1
+    @m.should be_valid
+    @m.value = 1.5
+    @m.should_not be_valid
+    @m.value = 2
+    @m.should be_valid    
+    @m.value = 3
+    @m.should_not be_valid 
+  end
+  
+  specify "should support validates_includes with a range" do
+    @c.set_validations{validates_includes(1..4, :value)}
+    @m.should_not be_valid
+    @m.value = 1
+    @m.should be_valid
+    @m.value = 1.5
+    @m.should be_valid
+    @m.value = 0
+    @m.should_not be_valid
+    @m.value = 5
+    @m.should_not be_valid    
+  end
+  
+  specify "should supports validates_integer" do
+    @c.set_validations{validates_integer(:value)}
+    @m.value = 'blah'
+    @m.should_not be_valid
+    @m.value = '123'
+    @m.should be_valid
+    @m.value = '123.1231'
+    @m.should_not be_valid
+  end
+  
+  specify "should support validates_length_range" do
+    @c.set_validations{validates_length_range(2..5, :value)}
+    @m.should_not be_valid
+    @m.value = '12345'
+    @m.should be_valid
+    @m.value = '1'
+    @m.should_not be_valid
+    @m.value = '123456'
+    @m.should_not be_valid
+  end
+
+  specify "should support validates_max_length" do
+    @c.set_validations{validates_max_length(5, :value)}
+    @m.should_not be_valid
+    @m.value = '12345'
+    @m.should be_valid
+    @m.value = '123456'
+    @m.should_not be_valid
+  end
+
+  specify "should support validates_min_length" do
+    @c.set_validations{validates_min_length(5, :value)}
+    @m.should_not be_valid
+    @m.value = '12345'
+    @m.should be_valid
+    @m.value = '1234'
+    @m.should_not be_valid
+  end
+
+  specify "should support validates_not_string" do
+    @c.set_validations{validates_not_string(:value)}
+    @m.value = 123
+    @m.should be_valid
+    @m.value = '123'
+    @m.should_not be_valid
+    @m.errors.full_messages.should == ['value is a string']
+    @m.meta_def(:db_schema){{:value=>{:type=>:integer}}}
+    @m.should_not be_valid
+    @m.errors.full_messages.should == ['value is not a valid integer']
+  end
+
+  specify "should support validates_numeric" do
+    @c.set_validations{validates_numeric(:value)}
+    @m.value = 'blah'
+    @m.should_not be_valid
+    @m.value = '123'
+    @m.should be_valid
+    @m.value = '123.1231'
+    @m.should be_valid
+    @m.value = '+1'
+    @m.should be_valid
+    @m.value = '-1'
+    @m.should be_valid
+    @m.value = '+1.123'
+    @m.should be_valid
+    @m.value = '-0.123'
+    @m.should be_valid
+    @m.value = '-0.123E10'
+    @m.should be_valid
+    @m.value = '32.123e10'
+    @m.should be_valid
+    @m.value = '+32.123E10'
+    @m.should be_valid
+    @m.should be_valid
+    @m.value = '.0123'
+  end
+
+  specify "should support validates_presence" do
+    @c.set_validations{validates_presence(:value)}
+    @m.should_not be_valid
+    @m.value = ''
+    @m.should_not be_valid
+    @m.value = 1234
+    @m.should be_valid
+    @m.value = nil
+    @m.should_not be_valid
+    @m.value = true
+    @m.should be_valid
+    @m.value = false
+    @m.should be_valid
+  end
+  
+  it "should support validates_unique with a single attribute" do
+    @c.columns(:id, :username, :password)
+    @c.set_dataset MODEL_DB[:items]
+    @c.set_validations{validates_unique(:username)}
+    @c.dataset.extend(Module.new {
+      def fetch_rows(sql)
+        @db << sql
+        
+        case sql
+        when /COUNT.*username = '0records'/
+          yield({:v => 0})
+        when /COUNT.*username = '1record'/
+          yield({:v => 1})
+        end
+      end
+    })
+    
+    @user = @c.new(:username => "0records", :password => "anothertest")
+    @user.should be_valid
+    @user = @c.load(:id=>3, :username => "0records", :password => "anothertest")
+    @user.should be_valid
+
+    @user = @c.new(:username => "1record", :password => "anothertest")
+    @user.should_not be_valid
+    @user.errors.full_messages.should == ['username is already taken']
+    @user = @c.load(:id=>4, :username => "1record", :password => "anothertest")
+    @user.should_not be_valid
+    @user.errors.full_messages.should == ['username is already taken']
+
+    ds1 = @c.dataset.filter([[:username, '0records']])
+    ds2 = ds1.exclude(:id=>1)
+    @c.dataset.should_receive(:filter).with([[:username, '0records']]).twice.and_return(ds1)
+    ds1.should_receive(:exclude).with(:id=>1).once.and_return(ds2)
+
+    @user = @c.load(:id=>1, :username => "0records", :password => "anothertest")
+    @user.should be_valid
+    MODEL_DB.sqls.last.should == "SELECT COUNT(*) FROM items WHERE ((username = '0records') AND (id != 1)) LIMIT 1"
+    @user = @c.new(:username => "0records", :password => "anothertest")
+    @user.should be_valid
+    MODEL_DB.sqls.last.should == "SELECT COUNT(*) FROM items WHERE (username = '0records') LIMIT 1"
+  end
+  
+  it "should support validates_unique with multiple attributes" do
+    @c.columns(:id, :username, :password)
+    @c.set_dataset MODEL_DB[:items]
+    @c.set_validations{validates_unique([:username, :password])}
+    @c.dataset.extend(Module.new {
+      def fetch_rows(sql)
+        @db << sql
+        
+        case sql
+        when /COUNT.*username = '0records'/
+          yield({:v => 0})
+        when /COUNT.*username = '1record'/
+          yield({:v => 1})
+        end
+      end
+    })
+    
+    @user = @c.new(:username => "0records", :password => "anothertest")
+    @user.should be_valid
+    @user = @c.load(:id=>3, :username => "0records", :password => "anothertest")
+    @user.should be_valid
+
+    @user = @c.new(:username => "1record", :password => "anothertest")
+    @user.should_not be_valid
+    @user.errors.full_messages.should == ['username and password is already taken']
+    @user = @c.load(:id=>4, :username => "1record", :password => "anothertest")
+    @user.should_not be_valid
+    @user.errors.full_messages.should == ['username and password is already taken']
+
+    ds1 = @c.dataset.filter([[:username, '0records'], [:password, 'anothertest']])
+    ds2 = ds1.exclude(:id=>1)
+    @c.dataset.should_receive(:filter).with([[:username, '0records'], [:password, 'anothertest']]).twice.and_return(ds1)
+    ds1.should_receive(:exclude).with(:id=>1).once.and_return(ds2)
+
+    @user = @c.load(:id=>1, :username => "0records", :password => "anothertest")
+    @user.should be_valid
+    MODEL_DB.sqls.last.should == "SELECT COUNT(*) FROM items WHERE (((username = '0records') AND (password = 'anothertest')) AND (id != 1)) LIMIT 1"
+    @user = @c.new(:username => "0records", :password => "anothertest")
+    @user.should be_valid
+    MODEL_DB.sqls.last.should == "SELECT COUNT(*) FROM items WHERE ((username = '0records') AND (password = 'anothertest')) LIMIT 1"
+  end
+end 
