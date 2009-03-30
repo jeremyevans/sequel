3066d5d1750b4861bad1df246edc207fe9786936
Jeremy Evans <code@jeremyevans.net>
2009-03-30 12:05:19 -0700

    RDoc updates in remaining model files, small fixes to reduce memory use
    The majority of the lines changed are due to using constants instead of
    creating new string and regexp objects in every call (which need to be
    garbage collected).
    
    This also fixes Model::Errors#count to not create full_messages, so
    it should be faster.
    
    This should be the last of the RDoc related commits.

--- lib/sequel/model/exceptions.rb
+++ lib/sequel/model/exceptions.rb
@@ -1,7 +1,7 @@
 module Sequel
-  # This exception will be raised when raise_on_save_failure is set and a validation fails
-  class ValidationFailed < Error;end
+  # This exception will be raised when raise_on_save_failure is set and validation fails
+  class ValidationFailed < Error; end
 
-  # This exception will be raised when raise_on_save_failure is set and a before hook fails
-  class BeforeHookFailed < Error;end
+  # This exception will be raised when raise_on_save_failure is set and a before hook returns false
+  class BeforeHookFailed < Error; end
 end
