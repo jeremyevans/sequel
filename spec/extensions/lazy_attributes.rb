7025f986df3a73e4f9dcfc856eba2f04a3c10890
Jeremy Evans <code@jeremyevans.net>
2009-05-18 16:00:24 -0700

    Add lazy_attributes plugin, for creating attributes that can be lazily loaded from the database
    The lazy_attributes plugin builds on the tactical_eager_loading and
    identity_map plugins.  It allows you set an attribute as being lazily
    loaded:
    
      Album.plugin :lazy_attributes
      Album.lazy_attributes :review
    
    This does 2 things:
    
    1) It removes the :review attribute from being selected by default,
       so if you do Model.first, it won't include the :review column.
    
    2) It'll define a review instance method that will query the
       database for value of :review when called, if that value doesn't
       already exist.
    
    Of course, if it just did the above, it wouldn't require the
    identity_map or tactical_eager_loading plugins.  It requires those
    plugins because if you select a group of objects that have a lazy
    loaded attribute and ask for the lazy loaded attribute for one of
    them, it'll get the lazy loaded attribute for all of the objects
    in one query, instead of using a query for each object, similar to
    how the tactical_eager_loading plugin loads associations:
    
      # 2 queries total
      Sequel::Model.with_identity_map do
        Album.filter{id<100}.all do |a|
          a.review
        end
      end

---
+++ spec/extensions/lazy_attributes.rb
@@ -0,0 +1,99 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe "Sequel::Plugins::LazyAttributes" do
+  before do
+    class ::LazyAttributesModel < Sequel::Model(:la)
+      plugin :identity_map
+      plugin :tactical_eager_loading
+      plugin :lazy_attributes
+      columns :id, :name
+      meta_def(:columns){[:id, :name]}
+      lazy_attributes :name
+      meta_def(:columns){:id}
+      ds = dataset
+      def ds.fetch_rows(sql)
+        execute(sql)
+        select = @opts[:select]
+        where = @opts[:where]
+        if !where
+          if select.include?(:name)
+            yield(:id=>1, :name=>'1')
+            yield(:id=>2, :name=>'2')
+          else
+            yield(:id=>1)
+            yield(:id=>2)
+          end
+        else
+          i = where.args.last
+          i = i.instance_variable_get(:@array) if i.is_a?(Sequel::SQL::SQLArray)
+          Array(i).each do |x|
+            if sql =~ /SELECT name FROM/
+              yield(:name=>x.to_s)
+            else
+              yield(:id=>x, :name=>x.to_s)
+            end
+          end
+        end
+      end
+    end
+    @c = ::LazyAttributesModel
+    @ds = LazyAttributesModel.dataset
+    MODEL_DB.reset
+  end
+  after do
+    Object.send(:remove_const, :LazyAttributesModel)
+  end
+
+  it "should remove the attributes given from the SELECT columns of the model's dataset" do
+    @ds.opts[:select].should == [:id]
+    @ds.sql.should == 'SELECT id FROM la'
+  end
+
+  it "should lazily load the attribute for a single model object if there is an active identity map" do
+    @c.with_identity_map do
+      m = @c.first
+      m.values.should == {:id=>1}
+      m.name.should == '1'
+      m.values.should == {:id=>1, :name=>'1'}
+      MODEL_DB.sqls.should == ['SELECT id FROM la LIMIT 1', 'SELECT name FROM la WHERE (id = 1) LIMIT 1']
+    end
+  end
+
+  it "should lazily load the attribute for a single model object if there is no active identity map" do
+    m = @c.first
+    m.values.should == {:id=>1}
+    m.name.should == '1'
+    m.values.should == {:id=>1, :name=>'1'}
+    MODEL_DB.sqls.should == ['SELECT id FROM la LIMIT 1', 'SELECT name FROM la WHERE (id = 1) LIMIT 1']
+  end
+
+  it "should not lazily load the attribute for a single model object if the value already exists" do
+    @c.with_identity_map do
+      m = @c.first
+      m.values.should == {:id=>1}
+      m[:name] = '1'
+      m.name.should == '1'
+      m.values.should == {:id=>1, :name=>'1'}
+      MODEL_DB.sqls.should == ['SELECT id FROM la LIMIT 1']
+    end
+  end
+
+  it "should not lazily load the attribute for a single model object if it is a new record" do
+    @c.with_identity_map do
+      m = @c.new
+      m.values.should == {}
+      m.name.should == nil
+      MODEL_DB.sqls.should == []
+    end
+  end
+
+  it "should eagerly load the attribute for all model objects reteived with it" do
+    @c.with_identity_map do
+      ms = @c.all
+      ms.map{|m| m.values}.should == [{:id=>1}, {:id=>2}]
+      ms.map{|m| m.name}.should == %w'1 2'
+      ms.map{|m| m.values}.should == [{:id=>1, :name=>'1'}, {:id=>2, :name=>'2'}]
+      MODEL_DB.sqls.should == ['SELECT id FROM la', 'SELECT id, name FROM la WHERE (id IN (1, 2))']
+    end
+  end
+end
