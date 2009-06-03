2bb56bd2352dab15343913cffc9724847192a861
Jeremy Evans <code@jeremyevans.net>
2009-06-02 21:54:17 -0700

    Make the tactical_eager_loading plugin not depend on the identity_map plugin
    After giving it some thought, the tactical_eager_loading plugin
    doesn't use the identity map at all, other than to not work when
    there isn't an active identity map.  That seems like a pointless
    restriction to me, so I've eliminated it.  The reason that it works
    without the identity_map plugin is that it basically builds a type
    of identity map internally which is used inside the :eager_loader
    block.
    
    The lazy_attributes plugin still depends on both, since it needs
    the identity_map plugin and the tactical_eager_loading plugin in
    order to load the lazy attribute for a bunch of models at once.

--- spec/extensions/tactical_eager_loading_spec.rb
+++ spec/extensions/tactical_eager_loading_spec.rb
@@ -13,12 +13,16 @@ describe "Sequel::Plugins::TacticalEagerLoading" do
         where = @opts[:where]
         if !where
           yield(:id=>1, :parent_id=>101)
-          yield(:id=>2, :parent_id=>201)
+          yield(:id=>2, :parent_id=>102)
           yield(:id=>101, :parent_id=>nil)
-          yield(:id=>201, :parent_id=>nil)
-        else
+          yield(:id=>102, :parent_id=>nil)
+        elsif where.args.first.column == :id
           Array(where.args.last).each do |x|
-            yield(:id=>x, :parent_id=>100+x) if x
+            yield(:id=>x, :parent_id=>nil)
+          end
+        elsif where.args.first.column == :parent_id
+          Array(where.args.last).each do |x|
+            yield(:id=>x-100, :parent_id=>x) if x > 100
           end
         end
       end
@@ -31,54 +35,31 @@ describe "Sequel::Plugins::TacticalEagerLoading" do
     Object.send(:remove_const, :TaticalEagerLoadingModel)
   end
 
-  it "Dataset#all should set the retrieved_by and reteived_with attributes if there is an active identity map" do
-    TaticalEagerLoadingModel.with_identity_map do
-      ts = TaticalEagerLoadingModel.all
-      ts.map{|x| [x.retrieved_by, x.retrieved_with]}.should == [[@ds,ts], [@ds,ts], [@ds,ts], [@ds,ts]]
-    end
+  it "Dataset#all should set the retrieved_by and reteived_with attributes" do
+    ts = @c.all
+    ts.map{|x| [x.retrieved_by, x.retrieved_with]}.should == [[@ds,ts], [@ds,ts], [@ds,ts], [@ds,ts]]
   end
 
   it "Dataset#all shouldn't raise an error if a Sequel::Model instance is not returned" do
-    TaticalEagerLoadingModel.with_identity_map do
-      ts = TaticalEagerLoadingModel.naked.all
-    end
+    proc{@c.naked.all}.should_not raise_error
   end
 
-  it "Dataset#all should not set the retrieved_by and reteived_with attributes if there is no active identity map" do
-    ts = TaticalEagerLoadingModel.all
-    ts.map{|x| [x.retrieved_by, x.retrieved_with]}.should == [[nil,nil], [nil,nil], [nil,nil], [nil,nil]]
-  end
-
-  it "association getter methods should eagerly load the association if there is an active identity map and the association isn't cached" do
-    TaticalEagerLoadingModel.with_identity_map do
-      MODEL_DB.sqls.length.should == 0
-      ts = TaticalEagerLoadingModel.all
-      MODEL_DB.sqls.length.should == 1
-      ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
-      MODEL_DB.sqls.length.should == 2
-      ts.map{|x| x.children}.should == [[], [], [ts[0]], [ts[1]]]
-      MODEL_DB.sqls.length.should == 3
-    end
-  end
-
-  it "association getter methods should not eagerly load the association if there is no active identity map" do
+  it "association getter methods should eagerly load the association if the association isn't cached" do
     MODEL_DB.sqls.length.should == 0
-    ts = TaticalEagerLoadingModel.all
+    ts = @c.all
     MODEL_DB.sqls.length.should == 1
-    ts.map{|x| x.parent}
+    ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
+    MODEL_DB.sqls.length.should == 2
+    ts.map{|x| x.children}.should == [[], [], [ts[0]], [ts[1]]]
     MODEL_DB.sqls.length.should == 3
-    ts.map{|x| x.children}
-    MODEL_DB.sqls.length.should == 7
   end
 
   it "association getter methods should not eagerly load the association if the association is cached" do
-    TaticalEagerLoadingModel.with_identity_map do
-      MODEL_DB.sqls.length.should == 0
-      ts = TaticalEagerLoadingModel.all
-      MODEL_DB.sqls.length.should == 1
-      ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
-      @ds.should_not_receive(:eager_load)
-      ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
-    end
+    MODEL_DB.sqls.length.should == 0
+    ts = @c.all
+    MODEL_DB.sqls.length.should == 1
+    ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
+    @ds.should_not_receive(:eager_load)
+    ts.map{|x| x.parent}.should == [ts[2], ts[3], nil, nil]
   end
 end
