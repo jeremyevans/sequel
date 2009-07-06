b90faeb797a3600b804598de44fd684800199f9c
Jeremy Evans <code@jeremyevans.net>
2009-07-06 08:03:36 -0700

    Fix MySQL alter_table actions

--- lib/sequel/adapters/shared/mysql.rb
+++ lib/sequel/adapters/shared/mysql.rb
@@ -91,14 +91,13 @@ module Sequel
         when :rename_column, :set_column_type, :set_column_null, :set_column_default
           o = op[:op]
           opts = schema(table).find{|x| x.first == op[:name]}
-          old_opts = opts ? opts.last : {}
-          name = o == :rename_column ? op[:new_name] : op[:name]
-          type = o == :set_column_type ? op[:type] : old_opts[:db_type]
-          null = o == :set_column_null ? op[:null] : old_opts[:allow_null]
-          default = o == :set_column_default ? op[:default] : old_opts[:ruby_default]
-          new_opts = {:name=>name, :type=>type, :null=>null, :default=>default}.merge(op)
-          new_opts.delete(:default) if new_opts[:default] == nil
-          "ALTER TABLE #{quote_schema_table(table)} CHANGE COLUMN #{quote_identifier(op[:name])} #{column_definition_sql(new_opts)}"
+          opts = opts ? opts.last.dup : {}
+          opts[:name] = o == :rename_column ? op[:new_name] : op[:name]
+          opts[:type] = o == :set_column_type ? op[:type] : opts[:db_type]
+          opts[:null] = o == :set_column_null ? op[:null] : opts[:allow_null]
+          opts[:default] = o == :set_column_default ? op[:default] : opts[:ruby_default]
+          opts.delete(:default) if opts[:default] == nil
+          "ALTER TABLE #{quote_schema_table(table)} CHANGE COLUMN #{quote_identifier(op[:name])} #{column_definition_sql(opts)}"
         when :drop_index
           "#{drop_index_sql(table, op)} ON #{quote_schema_table(table)}"
         else
