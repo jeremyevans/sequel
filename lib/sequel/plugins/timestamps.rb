18c22e4ac149e6684600ac47fe329a0795db15c9
Jeremy Evans <code@jeremyevans.net>
2009-08-04 12:22:15 -0700

    Add a timestamps plugin for automatically creating hooks for create and update timestamps
    I was planning on merging one of the existing timestamping plugins,
    but I couldn't find an existing implementation that I liked.  So I
    wrote this myself.  It's more configurable than the other plugins,
    and it's written in modern Sequel plugin style (using instance
    level hooks with super).
    
    The following options are supported (default):
    
    * :create - The field to hold the create timestamp (:created_at)
    * :force - Whether to overwrite an existing create timestamp (false)
    * :update - The field to hold the update timestamp (:updated_at)
    * :update_on_create - Whether to set the update timestamp to the
      create timestamp when creating (false)

---
+++ lib/sequel/plugins/timestamps.rb
@@ -0,0 +1,87 @@
+module Sequel
+  module Plugins
+    # The timestamps plugin creates hooks that automatically set create and
+    # update timestamp fields.  Both field names used are configurable, and you 
+    # can also set whether to overwrite existing create timestamps (false
+    # by default), or whether to set the update timestamp when creating (also
+    # false by default).
+    module Timestamps
+      # Configure the plugin by setting the avialable options.  Note that
+      # if this method is run more than once, previous settings are ignored,
+      # and it will just use the settings given or the default settings.  Options:
+      # * :create - The field to hold the create timestamp (default: :created_at)
+      # * :force - Whether to overwrite an existing create timestamp (default: false)
+      # * :update - The field to hold the update timestamp (default: :updated_at)
+      # * :update_on_create - Whether to set the update timestamp to the create timestamp when creating (default: false)
+      def self.configure(model, opts={})
+        model.instance_eval do
+          @create_timestamp_field = opts[:create]||:created_at
+          @update_timestamp_field = opts[:update]||:updated_at
+          @create_timestamp_overwrite = opts[:force]||false
+          @set_update_timestamp_on_create = opts[:update_on_create]||false
+        end
+      end
+
+      module ClassMethods
+        # The field to store the create timestamp
+        attr_reader :create_timestamp_field
+
+        # The field to store the update timestamp
+        attr_reader :update_timestamp_field
+        
+        # Whether to overwrite the create timestamp if it already exists
+        def create_timestamp_overwrite?
+          @create_timestamp_overwrite
+        end
+        
+        # Copy the class instance variables used from the superclass to the subclass
+        def inherited(subclass)
+          super
+          [:@create_timestamp_field, :@update_timestamp_field, :@create_timestamp_overwrite, :@set_update_timestamp_on_create].each do |iv|
+            subclass.instance_variable_set(iv, instance_variable_get(iv))
+          end
+        end
+        
+        # Whether to set the update timestamp to the create timestamp when creating
+        def set_update_timestamp_on_create?
+          @set_update_timestamp_on_create
+        end
+      end
+
+      module InstanceMethods
+        # Set the create timestamp when creating
+        def before_create
+          super
+          set_create_timestamp
+        end
+        
+        # Set the update timestamp when updating
+        def before_update
+          super
+          set_update_timestamp
+        end
+        
+        private
+        
+        # If the object has accessor methods for the create timestamp field, and
+        # the create timestamp value is nil or overwriting it is allowed, set the
+        # create timestamp field to the time given or the current time.  If setting
+        # the update timestamp on creation is configured, set the update timestamp
+        # as well.
+        def set_create_timestamp(time=nil)
+          field = model.create_timestamp_field
+          meth = :"#{field}="
+          self.send(meth, time||=Sequel.datetime_class.now) if respond_to?(field) && respond_to?(meth) && (model.create_timestamp_overwrite? || send(field).nil?)
+          set_update_timestamp(time) if model.set_update_timestamp_on_create?
+        end
+        
+        # Set the update timestamp to the time given or the current time if the
+        # object has a setter method for the update timestamp field.
+        def set_update_timestamp(time=nil)
+          meth = :"#{model.update_timestamp_field}="
+          self.send(meth, time||Sequel.datetime_class.now) if respond_to?(meth)
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/timestamps_spec.rb
@@ -0,0 +1,150 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe "Sequel::Plugins::Timestamps" do
+  before do
+    dc = Object.new
+    dc.instance_eval do
+      def now
+        '2009-08-01'
+      end
+    end
+    Sequel.datetime_class = dc
+    @c = Class.new(Sequel::Model(:t))
+    @c.class_eval do
+      columns :id, :created_at, :updated_at
+      plugin :timestamps
+      db.reset
+      def _refresh(ds); self end
+    end
+  end 
+  after do
+    Sequel.datetime_class = Time
+  end
+  
+  it "should set the create timestamp field on creation" do
+    o = @c.create
+    @c.db.sqls.should == ["INSERT INTO t (created_at) VALUES ('2009-08-01')"]
+    o.created_at.should == '2009-08-01'
+  end
+
+  it "should set the update timestamp field on update" do
+    o = @c.load(:id=>1).save
+    @c.db.sqls.should == ["UPDATE t SET updated_at = '2009-08-01' WHERE (id = 1)"]
+    o.updated_at.should == '2009-08-01'
+  end
+
+  it "should not update the update timestamp on creation" do
+    @c.create.updated_at.should == nil
+  end
+
+  it "should use the same value for the creation and update timestamps when creating if the :update_on_create option is given" do
+    @c.plugin :timestamps, :update_on_create=>true
+    o = @c.create
+    @c.db.sqls.length.should == 1
+    @c.db.sqls.first.should =~ /INSERT INTO t \((creat|updat)ed_at, (creat|updat)ed_at\) VALUES \('2009-08-01', '2009-08-01'\)/
+    o.created_at.should === o.updated_at
+  end
+
+  it "should allow specifying the create timestamp field via the :create option" do
+    c = Class.new(Sequel::Model(:t))
+    c.class_eval do
+      columns :id, :c
+      plugin :timestamps, :create=>:c
+      db.reset
+      def _refresh(ds); self end
+    end
+    o = c.create
+    c.db.sqls.should == ["INSERT INTO t (c) VALUES ('2009-08-01')"]
+    o.c.should == '2009-08-01'
+  end
+
+  it "should allow specifying the update timestamp field via the :update option" do
+    c = Class.new(Sequel::Model(:t))
+    c.class_eval do
+      columns :id, :u
+      plugin :timestamps, :update=>:u
+      db.reset
+      def _refresh(ds); self end
+    end
+    o = c.load(:id=>1).save
+    c.db.sqls.should == ["UPDATE t SET u = '2009-08-01' WHERE (id = 1)"]
+    o.u.should == '2009-08-01'
+  end
+
+  it "should not raise an error if the model doesn't have the timestamp columns" do
+    c = Class.new(Sequel::Model(:t))
+    c.class_eval do
+      columns :id, :x
+      plugin :timestamps
+      db.reset
+      def _refresh(ds); self end
+    end
+    c.create(:x=>2)
+    c.load(:id=>1, :x=>2).save
+    c.db.sqls.should == ["INSERT INTO t (x) VALUES (2)", "UPDATE t SET x = 2 WHERE (id = 1)"] 
+  end
+
+  it "should not overwrite an existing create timestamp" do
+    o = @c.create(:created_at=>'2009-08-03')
+    @c.db.sqls.should == ["INSERT INTO t (created_at) VALUES ('2009-08-03')"]
+    o.created_at.should == '2009-08-03'
+  end
+
+  it "should overwrite an existing create timestamp if the :force option is used" do
+    @c.plugin :timestamps, :force=>true
+    o = @c.create(:created_at=>'2009-08-03')
+    @c.db.sqls.should == ["INSERT INTO t (created_at) VALUES ('2009-08-01')"]
+    o.created_at.should == '2009-08-01'
+  end
+
+  it "should have create_timestamp_field give the create timestamp field" do
+    @c.create_timestamp_field.should == :created_at
+    @c.plugin :timestamps, :create=>:c
+    @c.create_timestamp_field.should == :c
+  end
+
+  it "should have update_timestamp_field give the update timestamp field" do
+    @c.update_timestamp_field.should == :updated_at
+    @c.plugin :timestamps, :update=>:u
+    @c.update_timestamp_field.should == :u
+  end
+
+  it "should have create_timestamp_overwrite? give the whether to overwrite an existing create timestamp" do
+    @c.create_timestamp_overwrite?.should == false
+    @c.plugin :timestamps, :force=>true
+    @c.create_timestamp_overwrite?.should == true
+  end
+
+  it "should have set_update_timestamp_on_create? give whether to set the update timestamp on create" do
+    @c.set_update_timestamp_on_create?.should == false
+    @c.plugin :timestamps, :update_on_create=>true
+    @c.set_update_timestamp_on_create?.should == true
+  end
+
+  it "should work with subclasses" do
+    c = Class.new(@c)
+    o = c.create
+    o.created_at.should == '2009-08-01'
+    o.updated_at.should == nil
+    o = c.load(:id=>1).save
+    o.updated_at.should == '2009-08-01'
+    c.db.sqls.should == ["INSERT INTO t (created_at) VALUES ('2009-08-01')", "UPDATE t SET updated_at = '2009-08-01' WHERE (id = 1)"]
+    c.create(:created_at=>'2009-08-03').created_at.should == '2009-08-03'
+
+    c.class_eval do
+      columns :id, :c, :u
+      plugin :timestamps, :create=>:c, :update=>:u, :force=>true, :update_on_create=>true
+    end
+    c2 = Class.new(c)
+    c2.db.reset
+    o = c2.create
+    o.c.should == '2009-08-01'
+    o.u.should === o.c 
+    c2.db.sqls.first.should =~ /INSERT INTO t \([cu], [cu]\) VALUES \('2009-08-01', '2009-08-01'\)/
+    c2.db.reset
+    o = c2.load(:id=>1).save
+    o.u.should == '2009-08-01'
+    c2.db.sqls.should == ["UPDATE t SET u = '2009-08-01' WHERE (id = 1)"]
+    c2.create(:c=>'2009-08-03').c.should == '2009-08-01'
+  end
+end
