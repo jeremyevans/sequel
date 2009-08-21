96f08b9ede25ee776b2f79c15600b79874d89918
Jeremy Evans <code@jeremyevans.net>
2009-08-21 15:15:44 -0700

    Add a boolean_readers plugin for creating attribute? methods for boolean columns
    The BooleaReaders plugin allows for the creation of attribute?
    methods for boolean columns, which provide a nicer API.  By default,
    the accessors are created for all columns of type :boolean.  However,
    you can provide a block to the plugin to change the criteria used to
    determine if a column is boolean:
    
      Sequel::Model.plugin(:boolean_readers) do |c|
        db_schema[c][:db_type] =~ /\Atinyint/
      end
    
    This may be useful if you are using MySQL and have some tinyint
    columns that represent booleans and others that represent integers.
    You can turn the convert_tinyint_to_bool setting off and use the
    attribute methods for the integer value and the attribute? methods
    for the boolean value.

---
+++ lib/sequel/plugins/boolean_readers.rb
@@ -0,0 +1,51 @@
+module Sequel
+  module Plugins
+    # The BooleaReaders plugin allows for the creation of attribute? methods
+    # for boolean columns, which provide a nicer API.  By default, the accessors
+    # are created for all columns of type :boolean.  However, you can provide a
+    # block to the plugin to change the criteria used to determine if a
+    # column is boolean:
+    #
+    #   Sequel::Model.plugin(:boolean_readers){|c| db_schema[c][:db_type] =~ /\Atinyint/}
+    #
+    # This may be useful if you are using MySQL and have some tinyint columns
+    # that represent booleans and others that represent integers.  You can turn
+    # the convert_tinyint_to_bool setting off and use the attribute methods for
+    # the integer value and the attribute? methods for the boolean value.
+    module BooleanReaders
+      # Default proc for determining if given column is a boolean, which
+      # just checks that the :type is boolean.
+      DEFAULT_BOOLEAN_ATTRIBUTE_PROC = lambda{|c| db_schema[c][:type] == :boolean}
+
+      # Add the boolean_attribute? class method to the model, and create
+      # attribute? boolean reader methods for the class's columns if the class has a dataset.
+      def self.configure(model, &block)
+        model.meta_def(:boolean_attribute?, &(block || DEFAULT_BOOLEAN_ATTRIBUTE_PROC))
+        model.instance_eval{send(:create_boolean_readers) if @dataset}
+      end
+
+      module ClassMethods
+        # Create boolean readers for the class using the columns from the new dataset.
+        def set_dataset(*args)
+          super
+          create_boolean_readers
+        end
+
+        private
+
+        # Add a attribute? method for the column to a module included in the class.
+        def create_boolean_reader(column)
+          overridable_methods_module.module_eval do
+            define_method("#{column}?"){model.db.typecast_value(:boolean, send(column))}
+          end
+        end
+
+        # Add attribute? methods for all of the boolean attributes for this model.
+        def create_boolean_readers
+          im = instance_methods.collect{|x| x.to_s}
+          columns.each{|c| create_boolean_reader(c) if boolean_attribute?(c) && !im.include?("#{c}?")}
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/boolean_readers_spec.rb
@@ -0,0 +1,86 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe Sequel::Model, "BooleanReaders plugin" do
+  before do
+    @db = Sequel::Database.new({})
+    def @db.schema(*args)
+      [[:id, {}], [:y, {:type=>:integer, :db_type=>'tinyint(1)'}], [:b, {:type=>:boolean, :db_type=>'boolean'}]]
+    end
+
+    @c = Class.new(Sequel::Model(@db[:items]))
+    @p =lambda do
+      @columns = [:id, :b, :y]
+      def columns; @columns; end
+    end
+    @c.instance_eval(&@p)
+  end 
+
+  specify "should create attribute? readers for all boolean attributes" do
+    @c.plugin(:boolean_readers)
+    o = @c.new
+    o.b?.should == nil
+    o.b = '1'
+    o.b?.should == true
+    o.b = '0'
+    o.b?.should == false
+    o.b = ''
+    o.b?.should == nil
+  end
+
+  specify "should not create attribute? readers for non-boolean attributes" do
+    @c.plugin(:boolean_readers)
+    proc{@c.new.y?}.should raise_error(NoMethodError)
+    proc{@c.new.id?}.should raise_error(NoMethodError)
+  end
+
+  specify "should accept a block to determine if an attribute is boolean" do
+    @c.plugin(:boolean_readers){|c| db_schema[c][:db_type] == 'tinyint(1)'}
+    proc{@c.new.b?}.should raise_error(NoMethodError)
+    o = @c.new
+    o.y.should == nil
+    o.y?.should == nil
+    o.y = '1'
+    o.y.should == 1
+    o.y?.should == true
+    o.y = '0'
+    o.y.should == 0
+    o.y?.should == false
+    o.y = ''
+    o.y.should == nil
+    o.y?.should == nil
+  end
+
+  specify "should create boolean readers when set_dataset is defined" do
+    c = Class.new(Sequel::Model(@db))
+    c.instance_eval(&@p)
+    c.plugin(:boolean_readers)
+    c.set_dataset(@db[:a])
+    o = c.new
+    o.b?.should == nil
+    o.b = '1'
+    o.b?.should == true
+    o.b = '0'
+    o.b?.should == false
+    o.b = ''
+    o.b?.should == nil
+    proc{o.i?}.should raise_error(NoMethodError)
+
+    c = Class.new(Sequel::Model(@db))
+    c.instance_eval(&@p)
+    c.plugin(:boolean_readers){|x| db_schema[x][:db_type] == 'tinyint(1)'}
+    c.set_dataset(@db[:a])
+    o = c.new
+    o.y.should == nil
+    o.y?.should == nil
+    o.y = '1'
+    o.y.should == 1
+    o.y?.should == true
+    o.y = '0'
+    o.y.should == 0
+    o.y?.should == false
+    o.y = ''
+    o.y.should == nil
+    o.y?.should == nil
+    proc{o.b?}.should raise_error(NoMethodError)
+  end
+end
