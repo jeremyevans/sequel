a8c271dd5cd2ffd8bbafae30d19909f516ec2445
Jeremy Evans <code@jeremyevans.net>
2009-08-25 11:40:30 -0700

    Add an instance_hooks plugin for adding hooks to specific model instances
    The instance_hooks plugin allows you to add hooks to specific instances,
    by passing a block to a _hook method (e.g. before_save_hook{do_something}).
    The block executed when the hook is called (e.g. before_save).
    
    All of the standard hooks are supported, except for after_initialize.
    Instance level before hooks are executed in reverse order of addition before
    calling super.  Instance level after hooks are executed in order of addition
    after calling super.  If any of the instance level before hook blocks return
    false, no more instance level before hooks are called and false is returned.
    
    Instance level hooks are cleared when the object is saved successfully.

---
+++ lib/sequel/plugins/instance_hooks.rb
@@ -0,0 +1,57 @@
+module Sequel
+  module Plugins
+    # The instance_hooks plugin allows you to add hooks to specific instances,
+    # by passing a block to a _hook method (e.g. before_save_hook{do_something}).
+    # The block executed when the hook is called (e.g. before_save).
+    #
+    # All of the standard hooks are supported, except for after_initialize.
+    # Instance level before hooks are executed in reverse order of addition before
+    # calling super.  Instance level after hooks are executed in order of addition
+    # after calling super.  If any of the instance level before hook blocks return
+    # false, no more instance level before hooks are called and false is returned.
+    #
+    # Instance level hooks are cleared when the object is saved successfully.
+    module InstanceHooks
+      module InstanceMethods 
+        HOOKS = Sequel::Model::HOOKS - [:after_initialize]
+        HOOKS.each{|h| class_eval("def #{h}_hook(&block); add_instance_hook(:#{h}, &block) end", __FILE__, __LINE__)}
+        
+        BEFORE_HOOKS, AFTER_HOOKS = HOOKS.partition{|hook| hook.to_s =~ /\Abefore_/}
+        BEFORE_HOOKS.each{|h| class_eval("def #{h}; run_instance_hooks(:#{h}) == false ? false : super end", __FILE__, __LINE__)}
+        AFTER_HOOKS.each{|h| class_eval("def #{h}; super; run_instance_hooks(:#{h}) end", __FILE__, __LINE__)}
+        
+        # Clear the instance level hooks after saving the object.
+        def after_save
+          super
+          run_instance_hooks(:after_save)
+          @instance_hooks.clear if @instance_hooks
+        end
+        
+        private
+        
+        # Add the block as an instance level hook.  For before hooks, add it to
+        # the beginning of the instance hook's array.  For after hooks, add it
+        # to the end.
+        def add_instance_hook(hook, &block)
+          instance_hooks(hook).send(BEFORE_HOOKS.include?(hook) ? :unshift : :push, block)
+        end
+        
+        # An array of instance level hook blocks for the given hook type.
+        def instance_hooks(hook)
+          @instance_hooks ||= {}
+          @instance_hooks[hook] ||= []
+        end
+        
+        # Run all hook blocks of the given hook type.  If a before hook,
+        # immediately return false if any hook block call returns false.
+        def run_instance_hooks(hook)
+          if BEFORE_HOOKS.include?(hook)
+            instance_hooks(hook).each{|b| return false if b.call == false}
+          else
+            instance_hooks(hook).each{|b| b.call}
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/instance_hooks_spec.rb
@@ -0,0 +1,133 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe "InstanceHooks plugin" do
+  def r(x)
+    @r << x
+    x
+  end
+  
+  before do
+    @c = Class.new(Sequel::Model(:items))
+    @c.plugin :instance_hooks
+    @c.raise_on_save_failure = false
+    @o = @c.new
+    @x = @c.load({:id=>1})
+    @r = []
+  end
+  
+  it "should support before_create_hook and after_create_hook" do
+    @o.after_create_hook{r 1}
+    @o.before_create_hook{r 2}
+    @o.after_create_hook{r 3}
+    @o.before_create_hook{r 4}
+    @o.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+  end
+
+  it "should cancel the save if before_create_hook block returns false" do
+    @o.after_create_hook{r 1}
+    @o.before_create_hook{r false}
+    @o.before_create_hook{r 4}
+    @o.save.should == nil
+    @r.should == [4, false]
+    @r.clear
+    @o.save.should == nil
+    @r.should == [4, false]
+  end
+
+  it "should support before_update_hook and after_update_hook" do
+    @x.after_update_hook{r 1}
+    @x.before_update_hook{r 2}
+    @x.after_update_hook{r 3}
+    @x.before_update_hook{r 4}
+    @x.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+    @x.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+  end
+
+  it "should cancel the save if before_update_hook block returns false" do
+    @x.after_update_hook{r 1}
+    @x.before_update_hook{r false}
+    @x.before_update_hook{r 4}
+    @x.save.should == nil
+    @r.should == [4, false]
+    @r.clear
+    @x.save.should == nil
+    @r.should == [4, false]
+  end
+
+  it "should support before_save_hook and after_save_hook" do
+    @o.after_save_hook{r 1}
+    @o.before_save_hook{r 2}
+    @o.after_save_hook{r 3}
+    @o.before_save_hook{r 4}
+    @o.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+    @r.clear
+    
+    @x.after_save_hook{r 1}
+    @x.before_save_hook{r 2}
+    @x.after_save_hook{r 3}
+    @x.before_save_hook{r 4}
+    @x.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+    @x.save.should_not == nil
+    @r.should == [4, 2, 1, 3]
+  end
+
+  it "should cancel the save if before_save_hook block returns false" do
+    @x.after_save_hook{r 1}
+    @x.before_save_hook{r false}
+    @x.before_save_hook{r 4}
+    @x.save.should == nil
+    @r.should == [4, false]
+    @r.clear
+    
+    @x.after_save_hook{r 1}
+    @x.before_save_hook{r false}
+    @x.before_save_hook{r 4}
+    @x.save.should == nil
+    @r.should == [4, false]
+    @r.clear
+    @x.save.should == nil
+    @r.should == [4, false]
+  end
+
+  it "should support before_destroy_hook and after_destroy_hook" do
+    @x.after_destroy_hook{r 1}
+    @x.before_destroy_hook{r 2}
+    @x.after_destroy_hook{r 3}
+    @x.before_destroy_hook{r 4}
+    @x.destroy.should_not == nil
+    @r.should == [4, 2, 1, 3]
+  end
+
+  it "should cancel the destroy if before_destroy_hook block returns false" do
+    @x.after_destroy_hook{r 1}
+    @x.before_destroy_hook{r false}
+    @x.before_destroy_hook{r 4}
+    @x.destroy.should == nil
+    @r.should == [4, false]
+  end
+
+  it "should support before_validation_hook and after_validation_hook" do
+    @o.after_validation_hook{r 1}
+    @o.before_validation_hook{r 2}
+    @o.after_validation_hook{r 3}
+    @o.before_validation_hook{r 4}
+    @o.valid?.should == true
+    @r.should == [4, 2, 1, 3]
+  end
+
+  it "should cancel the save if before_validation_hook block returns false" do
+    @o.after_validation_hook{r 1}
+    @o.before_validation_hook{r false}
+    @o.before_validation_hook{r 4}
+    @o.valid?.should == false
+    @r.should == [4, false]
+    @r.clear
+    @o.valid?.should == false
+    @r.should == [4, false]
+  end
+end
