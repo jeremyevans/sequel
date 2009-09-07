a62c16c7cb85eb80c3650479c592d4d05c4e0ed2
Jeremy Evans <code@jeremyevans.net>
2009-09-06 21:07:42 -0700

    Add typecast_on_load plugin, for fixing bad database typecasting when loading model objects
    The TypecastOnLoad plugin exists because most of Sequel's database
    adapters don't have complete control over typecasting, and may return
    columns that aren't typecast correctly (with correct being defined as
    how the model object would typecast the same column values).
    
    This plugin modifies Model.load to call the setter methods (which
    typecast by default) for all columns given.  You can either specify
    the columns to typecast on load in the plugin call itself, or
    afterwards using add_typecast_on_load_columns:
    
      Album.plugin :typecast_on_load, :release_date, :record_date
      # or:
      Album.plugin :typecast_on_load
      Album.add_typecast_on_load_columns :release_date, :record_date
    
    If the database returns release_date and record_date columns as
    strings instead of dates, this will ensure that if you access those
    columns through the model object, you'll get Date objects instead of
    strings.

---
+++ lib/sequel/plugins/typecast_on_load.rb
@@ -0,0 +1,61 @@
+module Sequel
+  module Plugins
+    # The TypecastOnLoad plugin exists because most of Sequel's database adapters don't
+    # have complete control over typecasting, and may return columns that aren't
+    # typecast correctly (with correct being defined as how the model object
+    # would typecast the same column values).
+    #
+    # This plugin modifies Model.load to call the setter methods (which typecast
+    # by default) for all columns given.  You can either specify the columns to
+    # typecast on load in the plugin call itself, or afterwards using 
+    # add_typecast_on_load_columns:
+    #
+    #   Album.plugin :typecast_on_load, :release_date, :record_date
+    #   # or:
+    #   Album.plugin :typecast_on_load
+    #   Album.add_typecast_on_load_columns :release_date, :record_date
+    #
+    # If the database returns release_date and record_date columns as strings
+    # instead of dates, this will ensure that if you access those columns through
+    # the model object, you'll get Date objects instead of strings.
+    module TypecastOnLoad
+      # Call add_typecast_on_load_columns on the passed column arguments.
+      def self.configure(model, *columns, &block)
+        model.instance_eval do
+          @typecast_on_load_columns ||= []
+          add_typecast_on_load_columns(*columns)
+        end
+      end
+
+      module ClassMethods
+        # The columns to typecast on load for this model.
+        attr_reader :typecast_on_load_columns
+
+        # Add additional columns to typecast on load for this model.
+        def add_typecast_on_load_columns(*columns)
+          @typecast_on_load_columns.concat(columns)
+        end
+
+        # Give the subclass a copy of the typecast on load columns.
+        def inherited(subclass)
+          super
+          subclass.instance_variable_set(:@typecast_on_load_columns, typecast_on_load_columns.dup)
+        end
+
+        # Call the setter method for each of the typecast on load columns,
+        # ensuring the model object will have the correct typecasting even
+        # if the database doesn't typecast the columns correctly.
+        def load(values)
+          o = super
+          typecast_on_load_columns.each do |c|
+            if v = values[c]
+              o.send("#{c}=", v)
+            end
+          end
+          o.changed_columns.clear
+          o
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/typecast_on_load_spec.rb
@@ -0,0 +1,60 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe Sequel::Model, "TypecastOnLoad plugin" do
+  before do
+    @db = Sequel::Database.new({})
+    def @db.schema(*args)
+      [[:id, {}], [:y, {:type=>:boolean, :db_type=>'tinyint(1)'}], [:b, {:type=>:integer, :db_type=>'integer'}]]
+    end
+    @c = Class.new(Sequel::Model(@db[:items])) do
+      attr_accessor :bset
+      def b=(x)
+        self.bset = true
+        super
+      end
+    end
+    @c.instance_eval do
+      @columns = [:id, :b, :y]
+      def columns; @columns; end
+    end
+  end 
+
+  specify "should call setter method with value when loading the object, for all given columns" do
+    @c.plugin :typecast_on_load, :b
+    o = @c.load(:id=>1, :b=>"1", :y=>"0")
+    o.values.should == {:id=>1, :b=>1, :y=>"0"}
+    o.bset.should == true
+  end
+
+  specify "should allowing setting columns separately via add_typecast_on_load_columns" do
+    @c.plugin :typecast_on_load
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>"1", :y=>"0"}
+    @c.add_typecast_on_load_columns :b
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>1, :y=>"0"}
+    @c.add_typecast_on_load_columns :y
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>1, :y=>false}
+  end
+
+  specify "should work with subclasses" do
+    @c.plugin :typecast_on_load
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>"1", :y=>"0"}
+
+    c1 = Class.new(@c)
+    @c.add_typecast_on_load_columns :b
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>1, :y=>"0"}
+    c1.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>"1", :y=>"0"}
+
+    c2 = Class.new(@c)
+    @c.add_typecast_on_load_columns :y
+    @c.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>1, :y=>false}
+    c2.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>1, :y=>"0"}
+    
+    c1.add_typecast_on_load_columns :y
+    c1.load(:id=>1, :b=>"1", :y=>"0").values.should == {:id=>1, :b=>"1", :y=>false}
+  end
+
+  specify "should not mark the object as modified" do
+    @c.plugin :typecast_on_load, :b
+    @c.load(:id=>1, :b=>"1", :y=>"0").modified?.should == false
+  end
+end
