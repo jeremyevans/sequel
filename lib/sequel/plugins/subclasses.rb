b75bf1940da241f6f44a148fc607c72004f1e682
Jeremy Evans <code@jeremyevans.net>
2009-09-07 12:59:02 -0700

    Add subclasses plugin, for recording all of a models subclasses and descendent classes
    The Subclasses plugin keeps track of all subclasses of the
    current model class.  Direct subclasses are available via the
    subclasses method, and all descendent classes are available via the
    descendents method.
    
      c = Class.new(Sequel::Model)
      c.plugin :subclasses
      sc1 = Class.new(c)
      sc2 = Class.new(c)
      ssc1 = Class.new(sc1)
      c.subclasses    # [sc1, sc2]
      sc1.subclasses  # [ssc1]
      sc2.subclasses  # []
      ssc1.subclasses # []
      c.descendents   # [sc1, ssc1, sc2]

---
+++ lib/sequel/plugins/subclasses.rb
@@ -0,0 +1,45 @@
+module Sequel
+  module Plugins
+    # The Subclasses plugin keeps track of all subclasses of the
+    # current model class.  Direct subclasses are available via the
+    # subclasses method, and all descendent classes are available via the
+    # descendents method.
+    #
+    #   c = Class.new(Sequel::Model)
+    #   c.plugin :subclasses
+    #   sc1 = Class.new(c)
+    #   sc2 = Class.new(c)
+    #   ssc1 = Class.new(sc1)
+    #   c.subclasses    # [sc1, sc2]
+    #   sc1.subclasses  # [ssc1]
+    #   sc2.subclasses  # []
+    #   ssc1.subclasses # []
+    #   c.descendents   # [sc1, ssc1, sc2]
+    module Subclasses
+      # Initialize the subclasses instance variable for the model.
+      def self.apply(model, &block)
+        model.instance_variable_set(:@subclasses, [])
+      end
+
+      module ClassMethods
+        # All subclasses for the current model.  Does not
+        # include the model itself.
+        attr_reader :subclasses
+
+        # All descendent classes of this model.
+        def descendents
+          subclasses.map{|x| [x] + x.descendents}.flatten
+        end
+
+        # Add the subclass to this model's current subclasses,
+        # and initialize a new subclasses instance variable
+        # in the subclass.
+        def inherited(subclass)
+          super
+          subclasses << subclass
+          subclass.instance_variable_set(:@subclasses, [])
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/subclasses_spec.rb
@@ -0,0 +1,52 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe Sequel::Model, "Subclasses plugin" do
+  before do
+    @c = Class.new(Sequel::Model)
+    @c.plugin :subclasses
+  end
+
+  specify "#subclasses should record direct subclasses of the given model" do
+    @c.subclasses.should == []
+
+    sc1 = Class.new(@c)
+    @c.subclasses.should == [sc1]
+    sc1.subclasses.should == []
+
+    sc2 = Class.new(@c)
+    @c.subclasses.should == [sc1, sc2]
+    sc1.subclasses.should == []
+    sc2.subclasses.should == []
+
+    ssc1 = Class.new(sc1)
+    @c.subclasses.should == [sc1, sc2]
+    sc1.subclasses.should == [ssc1]
+    sc2.subclasses.should == []
+  end
+
+  specify "#descendents should record all descendent subclasses of the given model" do
+    @c.descendents.should == []
+
+    sc1 = Class.new(@c)
+    @c.descendents.should == [sc1]
+    sc1.descendents.should == []
+
+    sc2 = Class.new(@c)
+    @c.descendents.should == [sc1, sc2]
+    sc1.descendents.should == []
+    sc2.descendents.should == []
+
+    ssc1 = Class.new(sc1)
+    @c.descendents.should == [sc1, ssc1, sc2]
+    sc1.descendents.should == [ssc1]
+    sc2.descendents.should == []
+    ssc1.descendents.should == []
+
+    sssc1 = Class.new(ssc1)
+    @c.descendents.should == [sc1, ssc1, sssc1, sc2]
+    sc1.descendents.should == [ssc1, sssc1]
+    sc2.descendents.should == []
+    ssc1.descendents.should == [sssc1]
+    sssc1.descendents.should == []
+  end
+end
