aa6d76c998f4775f8a4db77f3c3d3634f17846db
Jeremy Evans <code@jeremyevans.net>
2009-09-09 16:57:20 -0700

    Add active_model plugin, which gives Sequel::Model an ActiveModel compliant API, passes the ActiveModel::Lint tests
    Honestly, I'm not sure how complete the ActiveModel::Lint tests
    are.  This fully supports the most current version on rails/rails
    at github.  I remember Yehuda mentioning that ActiveModel naming
    was necessary for compliance, but the Lint tests don't currently
    include that.
    
    The active_model plugin should be considered a work in progress
    until the ActiveModel::Lint tests have been finalized.

---
+++ lib/sequel/plugins/active_model.rb
@@ -0,0 +1,35 @@
+module Sequel
+  module Plugins
+    # The ActiveModel plugin makes Sequel::Model objects the
+    # pass the ActiveModel::Lint tests, which should
+    # hopefully mean full ActiveModel compliance.  This should
+    # allow the full support of Sequel::Model objects in Rails 3.
+    module ActiveModel
+      module InstanceMethods
+        # Record that an object was destroyed, for later use by
+        # destroyed?
+        def after_destroy
+          super
+          @destroyed = true
+        end
+        
+        # Whether the object was destroyed by destroy.  Not true
+        # for objects that were deleted.
+        def destroyed?
+          @destroyed == true
+        end
+        
+        # An alias for new?
+        def new_record?
+          new?
+        end
+
+        # With the ActiveModel plugin, Sequel model objects are already
+        # compliant, so this returns self.
+        def to_model
+          self
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/active_model_spec.rb
@@ -0,0 +1,47 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+if (begin
+  require 'active_model'
+  true
+  rescue LoadError
+  end)
+describe "ActiveModel plugin" do
+  before do
+    @c = Class.new(Sequel::Model) do
+      def delete; end
+    end
+    @c.plugin :active_model
+    @m = @c.new
+    @o = @c.load({})
+  end
+
+  specify "should be compliant to the ActiveModel spec" do
+    s = ''
+    IO.popen('-') do |f|
+      if f
+        s = f.read
+      else
+        ActiveModel::Lint.test(@m)
+      end
+    end
+    s.should =~ /0 failures, 0 errors/
+  end
+  
+  specify "to_model should return self" do
+    @m.to_model.object_id.should == @m.object_id
+  end
+  
+  specify "new_record? should be aliased to new" do
+    @m.new_record?.should == true
+    @o.new_record?.should == false
+  end
+  
+  specify "new_record? should be aliased to new" do
+    @m.destroyed?.should == false
+    @o.destroyed?.should == false
+    @m.destroy
+    @o.destroy
+    @m.destroyed?.should == true
+    @o.destroyed?.should == true
+  end
+end 
+end
