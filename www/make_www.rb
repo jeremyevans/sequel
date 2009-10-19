2e93636a1c64a129f12254878d0ec1fcadaa94f5
Daniel Bretoi <daniel@netwalk.org>
2009-10-19 10:21:07 -0700

    adding sequel version to documentation

--- www/make_www.rb
+++ www/make_www.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 require 'erb'
+$: << File.join(File.dirname(__FILE__), '..','lib', 'sequel')
+require 'version'
 Dir.chdir(File.dirname(__FILE__))
 erb = ERB.new(File.read('layout.html.erb'))
 Dir['pages/*'].each do |page|
   public_loc = "#{page.gsub(/\Apages\//, 'public/')}.html"
-  content = File.read(page)
+  content = ERB.new(File.read(page)).result(binding)
   title = File.basename(page)
   File.open(public_loc, 'wb'){|f| f.write(erb.result(binding))}
 end
