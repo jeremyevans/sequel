2a2aa5468886fc78c5428bae73f5ddf96cd61033
michael <michael@thinknasium.org>
2009-10-23 00:25:02 -0700

    moved dataset features into dataset/features.rb

--- lib/sequel/dataset.rb
+++ lib/sequel/dataset.rb
@@ -134,17 +134,6 @@ module Sequel
       ds
     end
     
-    # Whether this dataset quotes identifiers.
-    def quote_identifiers?
-      @quote_identifiers
-    end
-    
-    # Whether the dataset requires SQL standard datetimes (false by default,
-    # as most allow strings with ISO 8601 format.
-    def requires_sql_standard_datetimes?
-      false
-    end
-
     # Set the server for this dataset to use.  Used to pick a specific database
     # shard to run a query against, or to override the default (which is SELECT uses
     # :read_only database and all other queries use the :default database).
@@ -162,57 +151,6 @@ module Sequel
     # This hash is merged into the hash provided to insert or update.
     def set_overrides(hash)
       clone(:overrides=>hash.merge(@opts[:overrides]||{}))
-    end
-    
-    # Whether the dataset supports common table expressions (the WITH clause).
-    def supports_cte?
-      select_clause_methods.include?(WITH_SUPPORTED)
-    end
-
-    # Whether the dataset supports the DISTINCT ON clause, true by default.
-    def supports_distinct_on?
-      true
-    end
-
-    # Whether the dataset supports the INTERSECT and EXCEPT compound operations, true by default.
-    def supports_intersect_except?
-      true
-    end
-
-    # Whether the dataset supports the INTERSECT ALL and EXCEPT ALL compound operations, true by default.
-    def supports_intersect_except_all?
-      true
-    end
-
-    # Whether the dataset supports the IS TRUE syntax.
-    def supports_is_true?
-      true
-    end
-    
-    # Whether the dataset supports the JOIN table USING (column1, ...) syntax.
-    def supports_join_using?
-      true
-    end
-    
-    # Whether the IN/NOT IN operators support multiple columns when an
-    # array of values is given.
-    def supports_multiple_column_in?
-      true
-    end
-    
-    # Whether the dataset supports timezones in literal timestamps
-    def supports_timestamp_timezones?
-      false
-    end
-    
-    # Whether the dataset supports fractional seconds in literal timestamps
-    def supports_timestamp_usecs?
-      true
-    end
-    
-    # Whether the dataset supports window functions.
-    def supports_window_functions?
-      false
     end
     
     # Add the mutation methods via metaprogramming
