8625c8cbfdba54637752643e5b7783b279a5cf71
Jeremy Evans <code@jeremyevans.net>
2010-02-15 18:18:57 -0800

    Make active_model plugin work with ActiveModel 3.0 beta Lint specs, now requires active_model
    Refactor test suite to test the active_model plugin in a subprocess, since
    apparently requiring active_model screws up some unrelated specs.  Probably
    ActiveSupport related, but I don't have interest in troubleshooting it.

--- lib/sequel/plugins/active_model.rb
+++ lib/sequel/plugins/active_model.rb
@@ -1,9 +1,12 @@
+require 'active_model'
 module Sequel
   module Plugins
     # The ActiveModel plugin makes Sequel::Model objects the
     # pass the ActiveModel::Lint tests, which should
     # hopefully mean full ActiveModel compliance.  This should
     # allow the full support of Sequel::Model objects in Rails 3.
+    # This plugin requires active_model in order to use
+    # ActiveModel::Naming.
     module ActiveModel
       ClassMethods = ::ActiveModel::Naming
 
--- spec/extensions/active_model_spec.rb
+++ spec/extensions/active_model_spec.rb
@@ -1,56 +1,46 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
-if (begin
-  require 'active_model'
-  true
-  rescue LoadError
-  end)
 describe "ActiveModel plugin" do
-  before do
-    @c = Class.new(Sequel::Model) do
-      def delete; end
-    end
-    @c.plugin :active_model
-    @m = @c.new
-    @o = @c.load({})
-  end
-
   specify "should be compliant to the ActiveModel spec" do
     s = ''
     IO.popen('-') do |f|
       if f
         s = f.read
       else
+        require 'active_model'
         require 'test/unit'
         require "test/unit/ui/console/testrunner"
-        $c = @c
         class AMLintTest < Test::Unit::TestCase
           def setup
-            @model = $c.new
+            @c = Class.new(Sequel::Model) do
+              def delete; end
+            end
+            @c.plugin :active_model
+            @m = @model = @c.new
+            @o = @c.load({})
           end
           include ActiveModel::Lint::Tests
+
+          def test_to_model
+            assert_equal @m.to_model.object_id.should, @m.object_id
+          end
+
+          def test_new_record
+            assert_equal true, @m.new_record?
+            assert_equal false, @o.new_record?
+          end
+
+          def test_destroyed
+            assert_equal false, @m.destroyed?
+            assert_equal false, @o.destroyed?
+            @m.destroy
+            @o.destroy
+            assert_equal true, @m.destroyed?
+            assert_equal true, @o.destroyed?
+          end
         end
         Test::Unit::UI::Console::TestRunner.run(AMLintTest)
       end
     end
     s.should =~ /0 failures, 0 errors/
   end
-  
-  specify "to_model should return self" do
-    @m.to_model.object_id.should == @m.object_id
-  end
-  
-  specify "new_record? should be aliased to new" do
-    @m.new_record?.should == true
-    @o.new_record?.should == false
-  end
-  
-  specify "new_record? should be aliased to new" do
-    @m.destroyed?.should == false
-    @o.destroyed?.should == false
-    @m.destroy
-    @o.destroy
-    @m.destroyed?.should == true
-    @o.destroyed?.should == true
-  end
 end 
-end
