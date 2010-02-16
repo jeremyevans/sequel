fd652fda063f5d00e8b26573cc93dab7cabbefba
Jeremy Evans <code@jeremyevans.net>
2010-02-16 08:22:12 -0800

    Don't run active_model plugin specs on windows or jruby, and handle active_model not being installed
    The active_model plugin specs currently require IO.popen('-') do a
    fork, which isn't supported on Windows or Java.  Also, the specs
    would fail if active_model is not installed.  This commit fixes both
    errors.  All to avoid errors caused by ActiveSupport.

--- spec/extensions/active_model_spec.rb
+++ spec/extensions/active_model_spec.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
+if RUBY_PLATFORM !~ /(win|w)32|java$/
 describe "ActiveModel plugin" do
   specify "should be compliant to the ActiveModel spec" do
     s = ''
@@ -6,41 +7,47 @@ describe "ActiveModel plugin" do
       if f
         s = f.read
       else
-        require 'active_model'
-        require 'test/unit'
-        require "test/unit/ui/console/testrunner"
-        class AMLintTest < Test::Unit::TestCase
-          def setup
-            @c = Class.new(Sequel::Model) do
-              def delete; end
+        begin
+          require 'active_model'
+        rescue LoadError
+          puts "0 failures, 0 errors"
+        else
+          require 'test/unit'
+          require "test/unit/ui/console/testrunner"
+          class AMLintTest < Test::Unit::TestCase
+            def setup
+              @c = Class.new(Sequel::Model) do
+                def delete; end
+              end
+              @c.plugin :active_model
+              @m = @model = @c.new
+              @o = @c.load({})
             end
-            @c.plugin :active_model
-            @m = @model = @c.new
-            @o = @c.load({})
-          end
-          include ActiveModel::Lint::Tests
+            include ActiveModel::Lint::Tests
 
-          def test_to_model
-            assert_equal @m.to_model.object_id.should, @m.object_id
-          end
+            def test_to_model
+              assert_equal @m.to_model.object_id.should, @m.object_id
+            end
 
-          def test_new_record
-            assert_equal true, @m.new_record?
-            assert_equal false, @o.new_record?
-          end
+            def test_new_record
+              assert_equal true, @m.new_record?
+              assert_equal false, @o.new_record?
+            end
 
-          def test_destroyed
-            assert_equal false, @m.destroyed?
-            assert_equal false, @o.destroyed?
-            @m.destroy
-            @o.destroy
-            assert_equal true, @m.destroyed?
-            assert_equal true, @o.destroyed?
+            def test_destroyed
+              assert_equal false, @m.destroyed?
+              assert_equal false, @o.destroyed?
+              @m.destroy
+              @o.destroy
+              assert_equal true, @m.destroyed?
+              assert_equal true, @o.destroyed?
+            end
           end
+          Test::Unit::UI::Console::TestRunner.run(AMLintTest)
         end
-        Test::Unit::UI::Console::TestRunner.run(AMLintTest)
       end
     end
     s.should =~ /0 failures, 0 errors/
   end
 end 
+end
