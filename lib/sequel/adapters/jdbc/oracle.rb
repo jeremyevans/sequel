751eb48bf0431ea13aa763d89d08857c674ea552
Jeremy Evans <code@jeremyevans.net>
2010-03-25 11:24:08 -0700

    Fix transactions when connecting to Oracle via JDBC
    The native Oracle adapter uses methods on the connection.  I tried
    to modify the shared Oracle adapter to use SQL statements for
    transactions instead of relying on the underlying connection's
    methods, but my first try didn't work, and I don't have direct
    access to Oracle to test.
    
    This makes the Oracle JDBC subadapter use code very similar to the
    native Oracle adapter, using JDBC methods instead of the oci8
    methods.  Tested by Wei Hong, thanks!
    
    Note that while Oracle supports savepoints, neither the native
    Oracle adapter or the Oracle JDBC subadapter implements such
    support.

--- lib/sequel/adapters/jdbc/oracle.rb
+++ lib/sequel/adapters/jdbc/oracle.rb
@@ -7,10 +7,40 @@ module Sequel
       # Instance methods for Oracle Database objects accessed via JDBC.
       module DatabaseMethods
         include Sequel::Oracle::DatabaseMethods
+        TRANSACTION_BEGIN = 'Transaction.begin'.freeze
+        TRANSACTION_COMMIT = 'Transaction.commit'.freeze
+        TRANSACTION_ROLLBACK = 'Transaction.rollback'.freeze
         
         # Return Sequel::JDBC::Oracle::Dataset object with the given opts.
         def dataset(opts=nil)
           Sequel::JDBC::Oracle::Dataset.new(self, opts)
+        end
+        
+        private
+        
+        # Use JDBC connection's setAutoCommit to false to start transactions
+        def begin_transaction(conn)
+          log_info(TRANSACTION_BEGIN)
+          conn.setAutoCommit(false)
+          conn
+        end
+        
+        # Use JDBC connection's commit method to commit transactions
+        def commit_transaction(conn)
+          log_info(TRANSACTION_COMMIT)
+          conn.commit
+        end
+        
+        # Use JDBC connection's setAutoCommit to true to enable non-transactional behavior
+        def remove_transaction(conn)
+          conn.setAutoCommit(true) if conn
+          super
+        end
+        
+        # Use JDBC connection's rollback method to rollback transactions
+        def rollback_transaction(conn)
+          log_info(TRANSACTION_ROLLBACK)
+          conn.rollback
         end
       end
       
