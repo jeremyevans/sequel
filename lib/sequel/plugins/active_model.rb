09dd1c89902c9bbde603fcfc87336c6681062056
Jeremy Evans <code@jeremyevans.net>
2010-04-02 16:26:40 -0700

    Update active_model plugin to work with the ActiveModel::Lint 3.0.0beta2 specs

--- lib/sequel/plugins/active_model.rb
+++ lib/sequel/plugins/active_model.rb
@@ -11,6 +11,9 @@ module Sequel
       ClassMethods = ::ActiveModel::Naming
 
       module InstanceMethods
+        # The default string to join composite primary keys with in to_param.
+        DEFAULT_TO_PARAM_JOINER = '-'.freeze
+      
         # Record that an object was destroyed, for later use by
         # destroyed?
         def after_destroy
@@ -18,21 +21,37 @@ module Sequel
           @destroyed = true
         end
         
-        # Whether the object was destroyed by destroy.  Not true
-        # for objects that were deleted.
-        def destroyed?
-          @destroyed == true
+        # False if the object is new? or has been destroyed, true otherwise.
+        def persisted?
+          !new? && @destroyed != true
         end
         
-        # An alias for new?
-        def new_record?
-          new?
+        # An array of primary key values, or nil if the object is not persisted.
+        def to_key
+          if persisted?
+            primary_key.is_a?(Symbol) ? [pk] : pk
+          end
         end
 
         # With the ActiveModel plugin, Sequel model objects are already
         # compliant, so this returns self.
         def to_model
           self
+        end
+        
+        # An string representing the object's primary key.  For composite
+        # primary keys, joins them with to_param_joiner.
+        def to_param
+          if k = to_key
+            k.join(to_param_joiner)
+          end
+        end
+        
+        private
+        
+        # The string to use to join composite primary key param strings.
+        def to_param_joiner
+          DEFAULT_TO_PARAM_JOINER
         end
       end
     end
--- spec/extensions/active_model_spec.rb
+++ spec/extensions/active_model_spec.rb
@@ -8,15 +8,18 @@ describe "ActiveModel plugin" do
         s = f.read
       else
         begin
+          require 'rubygems'
           require 'active_model'
         rescue LoadError
-          puts "0 failures, 0 errors"
+          puts "0 failures, 0 errors, skipping tests"
         else
           require 'test/unit'
           require "test/unit/ui/console/testrunner"
           class AMLintTest < Test::Unit::TestCase
             def setup
               @c = Class.new(Sequel::Model) do
+                set_primary_key :id
+                columns :id, :id2
                 def delete; end
               end
               @c.plugin :active_model
@@ -25,22 +28,42 @@ describe "ActiveModel plugin" do
             end
             include ActiveModel::Lint::Tests
 
-            def test_to_model
+            # Should return self, not a proxy object
+            def test__to_model
               assert_equal @m.to_model.object_id.should, @m.object_id
             end
-
-            def test_new_record
-              assert_equal true, @m.new_record?
-              assert_equal false, @o.new_record?
+            
+            def test__to_key
+              assert_equal nil, @m.to_key
+              @o.id = 1
+              assert_equal [1], @o.to_key
+              @c.set_primary_key [:id2, :id]
+              @o.id2 = 2
+              assert_equal [2, 1], @o.to_key
+              @o.destroy
+              assert_equal nil, @o.to_key
+            end
+            
+            def test__to_param
+              assert_equal nil, @m.to_param
+              @o.id = 1
+              assert_equal '1', @o.to_param
+              @c.set_primary_key [:id2, :id]
+              @o.id2 = 2
+              assert_equal '2-1', @o.to_param
+              @o.meta_def(:to_param_joiner){'|'}
+              assert_equal '2|1', @o.to_param
+              @o.destroy
+              assert_equal nil, @o.to_param
             end
 
-            def test_destroyed
-              assert_equal false, @m.destroyed?
-              assert_equal false, @o.destroyed?
+            def test__persisted?
+              assert_equal false, @m.persisted?
+              assert_equal true, @o.persisted?
               @m.destroy
               @o.destroy
-              assert_equal true, @m.destroyed?
-              assert_equal true, @o.destroyed?
+              assert_equal false, @m.persisted?
+              assert_equal false, @o.persisted?
             end
           end
           Test::Unit::UI::Console::TestRunner.run(AMLintTest)
