ae885f4ec6283c20679c490aa4cb30e2097b0ce4
Jeremy Evans <code@jeremyevans.net>
2010-05-06 15:02:21 -0700

    Add integration tests for bad up and down migrations

--- spec/integration/migrator_test.rb
+++ spec/integration/migrator_test.rb
@@ -30,4 +30,26 @@ describe Sequel::Migrator do
     @db.table_exists?(:sm1111).should be_true
     @db[:schema_info].get(:version).should == 1
   end
+
+  specify "should correctly set migration version to the last successful migration if the migration raises an error when migrating up" do
+    @dir = 'spec/files/bad_up_migration'
+    proc{@m.apply(@db, @dir)}.should raise_error
+    [:schema_info, :sm11111].each{|n| @db.table_exists?(n).should be_true}
+    @db.table_exists?(:sm22222).should be_false
+    @db[:schema_info].get(:version).should == 1
+    @m.apply(@db, @dir, 0)
+    [:sm11111, :sm22222].each{|n| @db.table_exists?(n).should be_false}
+    @db[:schema_info].get(:version).should == 0
+  end
+
+  specify "should correctly set migration version to the last successful migration if the migration raises an error when migrating down" do
+    @dir = 'spec/files/bad_down_migration'
+    @m.apply(@db, @dir)
+    [:schema_info, :sm11111, :sm22222].each{|n| @db.table_exists?(n).should be_true}
+    @db[:schema_info].get(:version).should == 2
+    proc{@m.apply(@db, @dir, 0)}.should raise_error
+    [:sm22222].each{|n| @db.table_exists?(n).should be_false}
+    @db.table_exists?(:sm11111).should be_true
+    @db[:schema_info].get(:version).should == 1
+  end
 end
