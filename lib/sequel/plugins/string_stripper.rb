82354f5cbbf15b88f1943224cc419802818c0221
Jeremy Evans <code@jeremyevans.net>
2010-05-26 10:33:30 -0700

    Add a StringStripper plugin that strips strings before assigning them to model attributes

---
+++ lib/sequel/plugins/string_stripper.rb
@@ -0,0 +1,26 @@
+module Sequel
+  module Plugins
+    # StringStripper is a very simple plugin that strips all input strings
+    # when assigning to the model's values. Example:
+    #
+    #   album = Album.new(:name=>' A ')
+    #   album.name # => 'A'
+    # 
+    # Usage:
+    #
+    #   # Make all model subclass instances strip strings (called before loading subclasses)
+    #   Sequel::Model.plugin :string_stripper
+    #
+    #   # Make the Album class strip strings
+    #   Album.plugin :string_stripper
+    module StringStripper
+      module InstanceMethods
+        # Strip value if it is a string, before attempting to assign
+        # it to the model's values.
+        def []=(k, v)
+          v.is_a?(String) ? super(k, v.strip) : super
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/string_stripper_spec.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), "spec_helper")
+
+describe "Sequel::Plugins::StringStripper" do
+  before do
+    @db = Sequel::Database.new
+    @c = Class.new(Sequel::Model(@db))
+    @c.plugin :string_stripper
+    @c.columns :name
+    @o = @c.new
+  end
+
+  it "should strip all input strings" do
+    @o.name = ' name '
+    @o.name.should == 'name'
+  end
+
+  it "should not affect other types" do
+    @o.name = 1
+    @o.name.should == 1
+    @o.name = Date.today
+    @o.name.should == Date.today
+  end
+end
