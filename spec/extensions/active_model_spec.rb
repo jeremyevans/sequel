a7a37b2312e6f274be1543178d060b065e1197c8
Jeremy Evans <code@jeremyevans.net>
2010-06-04 14:13:30 -0700

    Switch the active_model plugin spec to work on Windows and Java
    Previously, the active_model plugin spec forked in order to run
    the active model lint tests, which use test/unit instead of
    rspec.  Now it uses the test/unit API to run them.
    
    Unfortunately, because test/unit was replaced by minitest in
    ruby 1.9, and the old test/unit API isn't supported, we have
    to roll our own test suite runner on ruby 1.9.

--- spec/extensions/active_model_spec.rb
+++ spec/extensions/active_model_spec.rb
@@ -1,78 +1,91 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
-if RUBY_PLATFORM !~ /(win|w)32|java$/
+
+begin
+  require 'active_model'
+  require 'test/unit'
+  if Test::Unit.respond_to?(:run=)
+    Test::Unit.run = false
+    require 'test/unit/testresult'
+  elsif defined?(MiniTest::Unit)
+    class << MiniTest::Unit
+      def autorun; end
+    end
+  end
+rescue LoadError => e
+  skip_warn "active_model plugin: can't load active_model (#{e.class}: #{e})"
+else
 describe "ActiveModel plugin" do
   specify "should be compliant to the ActiveModel spec" do
-    s = ''
-    IO.popen('-') do |f|
-      if f
-        s = f.read
-      else
-        begin
-          require 'rubygems'
-          require 'active_model'
-        rescue LoadError
-          puts "0 failures, 0 errors, skipping tests"
-        else
-          require 'test/unit'
-          require "test/unit/ui/console/testrunner"
-          class AMLintTest < Test::Unit::TestCase
-            def setup
-              @c = Class.new(Sequel::Model) do
-                set_primary_key :id
-                columns :id, :id2
-                def delete; end
-              end
-              @c.plugin :active_model
-              @m = @model = @c.new
-              @o = @c.load({})
-            end
-            include ActiveModel::Lint::Tests
-
-            # Should return self, not a proxy object
-            def test__to_model
-              assert_equal @m.to_model.object_id.should, @m.object_id
-            end
-            
-            def test__to_key
-              assert_equal nil, @m.to_key
-              @o.id = 1
-              assert_equal [1], @o.to_key
-              @c.set_primary_key [:id2, :id]
-              @o.id2 = 2
-              assert_equal [2, 1], @o.to_key
-              @o.destroy
-              assert_equal nil, @o.to_key
-            end
-            
-            def test__to_param
-              assert_equal nil, @m.to_param
-              @o.id = 1
-              assert_equal '1', @o.to_param
-              @c.set_primary_key [:id2, :id]
-              @o.id2 = 2
-              assert_equal '2-1', @o.to_param
-              @o.meta_def(:to_param_joiner){'|'}
-              assert_equal '2|1', @o.to_param
-              @o.destroy
-              assert_equal nil, @o.to_param
-            end
-
-            def test__persisted?
-              assert_equal false, @m.persisted?
-              assert_equal true, @o.persisted?
-              @m.destroy
-              @o.destroy
-              assert_equal false, @m.persisted?
-              assert_equal false, @o.persisted?
-            end
-          end
-          Test::Unit::UI::Console::TestRunner.run(AMLintTest)
+    tc = Class.new(Test::Unit::TestCase)
+    tc.class_eval do
+      define_method(:setup) do
+        class ::AMLintTest < Sequel::Model
+          set_primary_key :id
+          columns :id, :id2
+          def delete; end
         end
+        @c = AMLintTest
+        @c.plugin :active_model
+        @m = @model = @c.new
+        @o = @c.load({})
+        super()
+      end
+      def teardown
+        super
+        Object.send(:remove_const, :AMLintTest)
+      end
+      include ActiveModel::Lint::Tests
+
+      # Should return self, not a proxy object
+      def test__to_model
+        assert_equal @m.to_model.object_id.should, @m.object_id
+      end
+      
+      def test__to_key
+        assert_equal nil, @m.to_key
+        @o.id = 1
+        assert_equal [1], @o.to_key
+        @c.set_primary_key [:id2, :id]
+        @o.id2 = 2
+        assert_equal [2, 1], @o.to_key
+        @o.destroy
+        assert_equal nil, @o.to_key
+      end
+      
+      def test__to_param
+        assert_equal nil, @m.to_param
+        @o.id = 1
+        assert_equal '1', @o.to_param
+        @c.set_primary_key [:id2, :id]
+        @o.id2 = 2
+        assert_equal '2-1', @o.to_param
+        @o.meta_def(:to_param_joiner){'|'}
+        assert_equal '2|1', @o.to_param
+        @o.destroy
+        assert_equal nil, @o.to_param
+      end
+
+      def test__persisted?
+        assert_equal false, @m.persisted?
+        assert_equal true, @o.persisted?
+        @m.destroy
+        @o.destroy
+        assert_equal false, @m.persisted?
+        assert_equal false, @o.persisted?
       end
     end
-    s.should =~ /0 failures, 0 errors/
+    if defined?(MiniTest::Unit)
+      tc.instance_methods.map{|x| x.to_s}.reject{|n| n !~ /\Atest_/}.each do |m|
+        i = tc.new(m)
+        i.setup
+        i.send(m)
+        i.teardown
+      end
+    else
+      res = ::Test::Unit::TestResult.new
+      tc.suite.run(res){}
+      res.failure_count.should == 0
+    end
   end
 end 
-else
-  skip_warn "active_model plugin: currently requires forking, and doesn't work on windows or Java"
 end
