1fba9fb5e8cbb0a2274610b41cb2ebd6a51a0e0d
Greg Campbell <gtcampbell@gmail.com>
2010-07-27 11:43:42 -0700

    Add failing test for timezone support.
    There is a bug which causes times to be read from the database
    incorrectly if they should have a different UTC offset than the current
    local time. (i.e. if current local time is adjusted for Daylight Savings
    and the time in the database is not, or vice versa).

--- spec/integration/timezone_test.rb
+++ spec/integration/timezone_test.rb
@@ -2,25 +2,29 @@ require File.join(File.dirname(File.expand_path(__FILE__)), 'spec_helper.rb')
 
 describe "Sequel timezone support" do
   def test_timezone
-    t = Time.now
-    @db[:t].insert(t)
-    t2 = @db[:t].single_value
-    t2 = Sequel.database_to_application_timestamp(t2.to_s) unless t2.is_a?(Time)
-    (t2 - t).should be_close(0, 2)
-    t2.utc_offset.should == 0 if Sequel.application_timezone == :utc
-    t2.utc_offset.should == t.getlocal.utc_offset if Sequel.application_timezone == :local
-    @db[:t].delete
-    
-    dt = DateTime.now
+    # Tests should cover both DST and non-DST times.
+    [Time.now, Time.local(2010,1,1,12), Time.local(2010,6,1,12)].each do |t|
+      @db[:t].insert(t)
+      t2 = @db[:t].single_value
+      t2 = Sequel.database_to_application_timestamp(t2.to_s) unless t2.is_a?(Time)
+      (t2 - t).should be_close(0, 2)
+      t2.utc_offset.should == 0 if Sequel.application_timezone == :utc
+      t2.utc_offset.should == t.getlocal.utc_offset if Sequel.application_timezone == :local
+      @db[:t].delete
+    end
+
     Sequel.datetime_class = DateTime
-    @db[:t].insert(dt)
-    dt2 = @db[:t].single_value
-    dt2 = Sequel.database_to_application_timestamp(dt2.to_s) unless dt2.is_a?(DateTime)
-    (dt2 - dt).should be_close(0, 0.00002)
-    dt2.offset.should == 0 if Sequel.application_timezone == :utc
-    dt2.offset.should == dt.offset if Sequel.application_timezone == :local
+    [DateTime.now, DateTime.civil(2010,1,1,12,0,0,Rational(-8,24)), DateTime.civil(2010,6,1,12,0,0,Rational(-7,24))].each do |dt|
+      @db[:t].insert(dt)
+      dt2 = @db[:t].single_value
+      dt2 = Sequel.database_to_application_timestamp(dt2.to_s) unless dt2.is_a?(DateTime)
+      (dt2 - dt).should be_close(0, 0.00002)
+      dt2.offset.should == 0 if Sequel.application_timezone == :utc
+      dt2.offset.should == dt.offset if Sequel.application_timezone == :local
+      @db[:t].delete
+    end
   end
-  
+
   before do
     @db = INTEGRATION_DB
     @db.create_table!(:t){DateTime :t}
@@ -36,20 +40,20 @@ describe "Sequel timezone support" do
     Sequel.application_timezone = :local
     test_timezone
   end
-  
+
   cspecify "should support using local time for database storage and UTC for the application", [:do, proc{|db| db.database_type != :sqlite}] do
     Sequel.database_timezone = :local
     Sequel.application_timezone = :utc
     test_timezone
   end
-  
+
   cspecify "should support using UTC for both database storage and for application", [:do, proc{|db| db.database_type != :sqlite}] do
     Sequel.default_timezone = :utc
     test_timezone
   end
-  
+
   specify "should support using local time for both database storage and for application" do
     Sequel.default_timezone = :local
     test_timezone
   end
-end
+  end
