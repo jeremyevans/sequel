ae8f3733d9d3eee4d0a74c16635cc4dee2e806bb
Simon Harris <haruki_zaemon@mac.com>
2010-09-30 17:38:10 +1000

    Fix Issue 310: IdentityMap fails when looking up composite keys.

--- lib/sequel/plugins/identity_map.rb
+++ lib/sequel/plugins/identity_map.rb
@@ -3,7 +3,7 @@ module Sequel
     # The identity_map plugin allows the user to create temporary identity maps
     # via the with_identity_map method, which takes a block.  Inside the block,
     # objects have a 1-1 correspondence with rows in the database.
-    # 
+    #
     # For example, the following is true, and wouldn't be true if you weren't
     # using the identity map:
     #   Sequel::Model.with_identity_map do
@@ -25,7 +25,7 @@ module Sequel
     # The identity_map plugin is not compatible with the standard eager loading of
     # many_to_many and many_through_many associations.  If you want to use the identity_map plugin,
     # you should use +eager_graph+ instead of +eager+ for those associations.
-    # 
+    #
     # Usage:
     #
     #   # Use an identity map that will affect all model classes (called before loading subclasses)
@@ -41,13 +41,13 @@ module Sequel
         def identity_map
           Thread.current[:sequel_identity_map]
         end
-        
+
         # The identity map key for an object of the current class with the given pk.
         # May not always be correct for a class which uses STI.
         def identity_map_key(pk)
           "#{self}:#{pk ? Array(pk).join(',') : "nil:#{rand}"}"
         end
-        
+
         # If the identity map is in use, check it for a current copy of the object.
         # If a copy does not exist, create a new object and add it to the identity map.
         # If a copy exists, add any values in the given row that aren't currently
@@ -65,7 +65,7 @@ module Sequel
           end
           o
         end
-        
+
         # Take a block and inside that block use an identity map to ensure a 1-1
         # correspondence of objects to the database row they represent.
         def with_identity_map
@@ -77,14 +77,14 @@ module Sequel
             self.identity_map = nil
           end
         end
-        
+
         private
 
-        # Set the thread local identity map to the given value. 
-        def identity_map=(v) 
+        # Set the thread local identity map to the given value.
+        def identity_map=(v)
           Thread.current[:sequel_identity_map] = v
         end
-        
+
         # Check the current identity map if it exists for the object with
         # the matching pk.  If one is found, return it, otherwise call super.
         def primary_key_lookup(pk)
@@ -109,7 +109,7 @@ module Sequel
         end
 
         private
-        
+
         # If the association is a many_to_one and it has a :key option and the
         # key option has a value and the association uses the primary key of
         # the associated class as the :primary_key option, check the identity
@@ -117,12 +117,18 @@ module Sequel
         def _load_associated_objects(opts)
           klass = opts.associated_class
           if idm = model.identity_map and opts[:type] == :many_to_one and opts[:primary_key] == klass.primary_key and
-           opts[:key] and pk = send(opts[:key]) and o = idm[klass.identity_map_key(pk)]
+           opts[:key] and pk = _associated_object_pk(opts[:key]) and o = idm[klass.identity_map_key(pk)]
             o
           else
             super
           end
         end
+
+        def _associated_object_pk(column)
+          return send(column) unless column.is_a?(Array)
+          column.map { |column| send(column) }.inspect
+        end
+
       end
     end
   end
