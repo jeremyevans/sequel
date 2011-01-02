18d37970b5b1a6cbf571d3f96f4f2d889f3e8daa
Bharanee Rathna <deepfryed@gmail.com>
2011-01-02 11:26:42 +1100

    make swift adapter work with swift master

--- lib/sequel/adapters/swift.rb
+++ lib/sequel/adapters/swift.rb
@@ -23,14 +23,14 @@ module Sequel
         db.swift_class = ::Swift::DB::Mysql
       end,
     }
-      
+
     class Database < Sequel::Database
       set_adapter_scheme :swift
 
       # The Swift adapter class being used by this database.  Connections
       # in this database's connection pool will be instances of this class.
       attr_accessor :swift_class
-      
+
       # Call the DATABASE_SETUP proc directly after initialization,
       # so the object always uses sub adapter specific code.  Also,
       # raise an error immediately if the connection doesn't have a
@@ -38,7 +38,7 @@ module Sequel
       # subadapter.
       def initialize(opts)
         super
-        if db_type = opts[:db_type] and !db_type.to_s.empty? 
+        if db_type = opts[:db_type] and !db_type.to_s.empty?
           if prok = DATABASE_SETUP[db_type.to_s.to_sym]
             prok.call(self)
           else
@@ -48,77 +48,73 @@ module Sequel
           raise(Error, ":db_type option not valid, should be postgres or mysql")
         end
       end
-      
+
       # Create an instance of swift_class for the given options.
       def connect(server)
         setup_connection(swift_class.new(server_opts(server)))
       end
-      
+
       # Return a Sequel::Swift::Dataset object for this database.
       def dataset(opts = nil)
         Swift::Dataset.new(self, opts)
       end
-    
+
       # Execute the given SQL, yielding a Swift::Result if a block is given.
       def execute(sql, opts={})
         synchronize(opts[:server]) do |conn|
           begin
             res = nil
-            log_yield(sql){conn.execute(sql); res = conn.results}
+            log_yield(sql){res = conn.execute(sql)}
             yield res if block_given?
             nil
           rescue SwiftError => e
             raise_error(e)
-          ensure
-            res.finish if res
           end
         end
       end
-      
+
       # Execute the SQL on the this database, returning the number of affected
       # rows.
       def execute_dui(sql, opts={})
         synchronize(opts[:server]) do |conn|
           begin
-            log_yield(sql){conn.execute(sql)}
+            log_yield(sql){conn.execute(sql).rows}
           rescue SwiftError => e
             raise_error(e)
           end
         end
       end
-      
+
       # Execute the SQL on this database, returning the primary key of the
       # table being inserted to.
       def execute_insert(sql, opts={})
         synchronize(opts[:server]) do |conn|
           begin
             res = nil
-            log_yield(sql){conn.execute(sql); (res = conn.results).insert_id}
+            log_yield(sql){res = conn.execute(sql); res.insert_id}
           rescue SwiftError => e
             raise_error(e)
-          ensure
-            res.finish if res
           end
         end
       end
-      
+
       private
-      
+
       # Method to call on a statement object to execute SQL that does
       # not return any rows.
       def connection_execute_method
         :execute
       end
-      
+
       # Close the given database connection.
       def disconnect_connection(c)
       end
-      
+
       # Execute SQL on the connection
       def log_connection_execute(conn, sql)
         log_yield(sql){conn.execute(sql)}
       end
-      
+
       # Set the :db entry to the same as the :database entry, since
       # Swift uses :db.
       def server_opts(o)
@@ -126,14 +122,14 @@ module Sequel
         o[:db] ||= o[:database]
         o
       end
-      
+
       # Allow extending the given connection when it is first created.
       # By default, just returns the connection.
       def setup_connection(conn)
         conn
       end
     end
-    
+
     class Dataset < Sequel::Dataset
       # Set the columns and yield the hashes to the block.
       def fetch_rows(sql, &block)
--- lib/sequel/adapters/swift/postgres.rb
+++ lib/sequel/adapters/swift/postgres.rb
@@ -2,7 +2,7 @@ Sequel.require 'adapters/shared/postgres'
 
 module Sequel
   Postgres::CONVERTED_EXCEPTIONS << ::SwiftError
-  
+
   module Swift
     # Adapter, Database, and Dataset support for accessing a PostgreSQL
     # database via Swift.
@@ -11,7 +11,7 @@ module Sequel
       # with the shared PostgreSQL code.
       module AdapterMethods
         include Sequel::Postgres::AdapterMethods
-        
+
         # Log all SQL that goes through the execute method to the related
         # database object.
         def execute(sql, *args)
@@ -19,19 +19,19 @@ module Sequel
         rescue SwiftError => e
           @db.send(:raise_error, e)
         end
-        
+
         private
-        
+
         # Swift specific method of getting specific values from a result set.
         def single_value(row)
           row.values.at(0)
         end
       end
-    
+
       # Methods to add to Database instances that access PostgreSQL via Swift.
       module DatabaseMethods
         include Sequel::Postgres::DatabaseMethods
-        
+
         # Add the primary_keys and primary_key_sequences instance variables,
         # so we can get the correct return values for inserted rows.
         def self.extended(db)
@@ -40,39 +40,36 @@ module Sequel
             @primary_key_sequences = {}
           end
         end
-        
+
         # Return instance of Sequel::Swift::Postgres::Dataset with the given opts.
         def dataset(opts=nil)
           Sequel::Swift::Postgres::Dataset.new(self, opts)
         end
-        
+
         # Run the SELECT SQL on the database and yield the rows
         def execute(sql, opts={})
           synchronize(opts[:server]) do |conn|
             begin
-              conn.execute(sql)
-              res = conn.results
+              res = conn.execute(sql)
               yield res if block_given?
             rescue SwiftError => e
               raise_error(e)
-            ensure
-              res.finish if res
             end
           end
         end
-        
+
         # Run the DELETE/UPDATE SQL on the database and return the number
         # of matched rows.
         def execute_dui(sql, opts={})
           synchronize(opts[:server]) do |conn|
             begin
-              conn.execute(sql)
+              conn.execute(sql).rows
             rescue SwiftError => e
               raise_error(e)
             end
           end
         end
-      
+
         # Run the INSERT SQL on the database and return the primary key
         # for the record.
         def execute_insert(sql, opts={})
@@ -85,14 +82,14 @@ module Sequel
             end
           end
         end
-        
+
         private
-        
+
         # Execute SQL on the connection.
         def log_connection_execute(conn, sql)
           conn.execute(sql)
         end
-      
+
         # Extend the adapter with the Swift PostgreSQL AdapterMethods.
         def setup_connection(conn)
           conn = super(conn)
@@ -102,7 +99,7 @@ module Sequel
           conn
         end
       end
-      
+
       class Dataset < Swift::Dataset
         include Sequel::Postgres::DatasetMethods
       end
