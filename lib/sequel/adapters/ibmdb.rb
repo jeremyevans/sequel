7c833cde86917a52d78a33b2f3d06cbcaae69fb1
roylez <roylzuo@gmail.com>
2011-08-29 08:13:15 -0700

    some update

--- lib/sequel/adapters/ibmdb.rb
+++ lib/sequel/adapters/ibmdb.rb
@@ -1,82 +1,89 @@
 require 'ibm_db'
 
-module IBM_DB
-  class Connection
-    attr_accessor :stmt   # the IBM_DB statement holder
-    attr_accessor :prepared_statements, :prepared_db_stmt
-
-    def free_stmt
-      IBM_DB.free_result(@stmt)
-      @stmt = nil
-    end
-
-    def fetch_stmt_assoc
-      return nil  unless @stmt
-      IBM_DB.fetch_assoc(@stmt)
-    end
-
-    def close
-      IBM_DB.close(self)
-    end
-
-    def prepare(sql, ps_name)
-      if stmt = IBM_DB.prepare(self, sql)
-        ps_name = ps_name.to_sym
-        @prepared_statements[ps_name] = sql
-        @prepared_db_stmt[ps_name] = stmt
-      else
-        raise get_error_msg
-      end
-    end
-
-    def bind(ps, *variables)
-      stmt = @prepared_db_stmt[ps]
-      variables.each.with_index do |v, i|
-        res = IBM_DB.bind_param(stmt, i + 1 )
-        raise "Binding variable to statement #{ps} failed" unless res
-      end
-    end
-
-    def execute(sql)
-      @stmt = IBM_DB.exec(self, sql)
-      self
-    end
-
-    def execute_prepared(ps_name, *values)
-      @stmt = @prepared_db_stmt[ps_name]
-      res = IBM_DB.execute(@stmt, values)
-      raise "Error executing statement #{ps_name} " unless res
-    end
-
-    def stmt_affected
-      IBM_DB.num_rows(@stmt)
-    end
-
-    def stmt_field_type(key)
-      IBM_DB.field_type(@stmt, key)
-    end
-
-    def stmt_num_fields
-      IBM_DB.num_fields(@stmt)
-    end
-
-    def stmt_field_name(ind)
-      IBM_DB.field_name(@stmt, ind)
-    end
-
-    def get_error_msg
-      IBM_DB.getErrormsg(self, IBM_DB::DB_CONN)
-    end
-  end
-end
-
 module Sequel
   module IBMDB
+    class Connection
+      attr_accessor :prepared_statements
+
+      def initialize(connection_string)
+        @conn = IBM_DB.connect(connection_string, '', '')
+        @prepared_statements = {}
+      end
+
+      def close
+        IBM_DB.close(@conn)
+      end
+
+      def prepare(sql, ps_name)
+        if stmt = IBM_DB.prepare(@conn, sql)
+          ps_name = ps_name.to_sym
+          stmt = Statement.new(stmt)
+          @prepared_statements[ps_name] = [sql, stmt]
+        else
+          raise Sequel::DatabaseError, get_error_msg
+        end
+      end
+
+      def execute(sql)
+        Statement.new(IBM_DB.exec(@conn, sql))
+      end
+
+      def execute_prepared(ps_name, *values)
+        @stmt = @prepared_statements[ps_name].last
+        res = @stmt.execute(*values)
+        raise Sequel::DatabaseError, "Error executing statement #{ps_name} " unless res
+        @stmt
+      end
+
+      def get_error_msg
+        IBM_DB.getErrormsg(@conn, IBM_DB::DB_CONN)
+      end
+    end
+
+    class Statement
+      def initialize(stmt)
+        @stmt = stmt
+      end
+
+      def affected
+        IBM_DB.num_rows(@stmt)
+      end
+
+      def execute(*values)    # values are for prepared statement
+        IBM_DB.execute(@stmt, values)
+      end
+
+      def fail?
+        return true   unless @stmt
+      end
+
+      def fetch_assoc
+        return nil  unless @stmt
+        IBM_DB.fetch_assoc(@stmt)
+      end
+
+      def field_type(key)
+        IBM_DB.field_type(@stmt, key)
+      end
+
+      def field_name(ind)
+        IBM_DB.field_name(@stmt, ind)
+      end
+
+      def free
+        IBM_DB.free_result(@stmt)
+      end
+
+      def num_fields
+        IBM_DB.num_fields(@stmt)
+      end
+    end
+
     class Database < Sequel::Database
       set_adapter_scheme :ibmdb
 
+      PRIMARY_KEY = ' NOT NULL PRIMARY KEY'.freeze
       AUTOINCREMENT = 'GENERATED ALWAYS AS IDENTITY'.freeze
-
 
       def connect(server)
         opts = server_opts(server)
@@ -92,42 +99,30 @@ module Sequel
             "Pwd=#{opts[:password]};" \
         )
 
-        conn = IBM_DB.connect(connection_string, '', '')
-        conn.prepared_statements = {}
-        conn.prepared_db_stmt = {}
-
-        conn
+        Connection.new(connection_string)
       end
       
-      def test_connection(server=nil)
-        synchronize(server){|conn|}
-        true
-      end
-
       def dataset(opts = nil)
         IBMDB::Dataset.new(self, opts)
       end
-      
-      # Returns number of rows affected
-      def execute_dui(sql, opts={}) 
-        if sql.is_a?(Symbol)
-          execute_prepared_statement(sql, opts, &block)
-        else
-          synchronize(opts[:server]){|c| c = _execute(c, sql, opts); c.stmt_affected}
-        end
-      end
-      alias_method :do, :execute_dui
       
       def execute(sql, opts={}, &block)
         if sql.is_a?(Symbol)
           execute_prepared_statement(sql, opts, &block)
         else
-          synchronize(opts[:server]){|c| yield _execute(c, sql, opts)}
+          synchronize(opts[:server]){|c| _execute(c, sql, opts, &block)}
         end
       end
-      alias_method :query, :execute
 
-      def execute_prepared_statement(ps_name, opts, &block)
+      def execute_dui(sql, opts={}, &block)
+        if sql.is_a?(Symbol)
+          execute_prepared_statement(sql, opts, &block)
+        else
+          synchronize(opts[:server]){|c| _execute(c, sql, opts, &block)}
+        end
+      end
+
+      def execute_prepared_statement(ps_name, opts)
         args = opts[:arguments]
         ps = prepared_statements[ps_name]
         sql = ps.prepared_sql
@@ -135,9 +130,9 @@ module Sequel
           unless conn.prepared_statements[ps_name] == sql
             conn.prepare(sql, ps_name)
           end
-          conn.execute_prepared(ps_name, *args)
+          stmt = conn.execute_prepared(ps_name, *args)
 
-          yield conn
+          block_given? ? yield(stmt): stmt.affected
         end
       end
 
@@ -158,14 +153,47 @@ module Sequel
       end
 
       private
+
+      # db2 specific alter table
+      def alter_table_sql(table, op)
+        case op[:op]
+        when :add_column
+          "ALTER TABLE #{quote_schema_table(table)} ADD #{column_definition_sql(op)}"
+        when :drop_column
+          "ALTER TABLE #{quote_schema_table(table)} DROP #{column_definition_sql(op)}"
+        when :rename_column
+          "ALTER TABLE #{quote_schema_table(table)} RENAME COLUMN #{quote_identifier(op[:name])} TO #{quote_identifier(op[:new_name])}"
+        when :set_column_type
+          "ALTER TABLE #{quote_schema_table(table)} ALTER COLUMN #{quote_identifier(op[:name])} SET DATA TYPE #{type_literal(op)}"
+        else
+          super(table, op)
+        end
+      end
+
+      # db2 specific autoincrement
       def auto_increment_sql
         AUTOINCREMENT
       end
 
+      # Add null/not null SQL fragment to column creation SQL.
+      def column_definition_null_sql(sql, column)
+        # bypass null/not null fragment if primary_key is set
+        return  if column[:primary_key]
+
+        null = column.fetch(:null, column[:allow_null])
+        sql << NOT_NULL if null == false
+        sql << NULL if null == true
+      end
+
+      # Add primary key SQL fragment to column creation SQL.
+      def column_definition_primary_key_sql(sql, column)
+        sql << PRIMARY_KEY if column[:primary_key]
+      end
+
       def _execute(conn, sql, opts)
-        conn = log_yield(sql){ conn.execute(sql) }
-        raise conn.get_error_msg unless conn.stmt
-        conn
+        stmt = log_yield(sql){ conn.execute(sql) }
+        raise Sequel::DatabaseError, conn.get_error_msg if stmt.fail?
+        block_given? ? yield(stmt) : stmt.affected
       end
       
       def disconnect_connection(conn)
@@ -192,17 +220,21 @@ module Sequel
         include Sequel::Dataset::UnnumberedArgumentMapper
         
         private
-        
         # Execute the prepared statement with the bind arguments instead of
         # the given SQL.
         def execute(sql, opts={}, &block)
           super(prepared_statement_name, {:arguments=>bind_arguments}.merge(opts), &block)
         end
         
-        # Same as execute, explicit due to intricacies of alias and super.
         def execute_dui(sql, opts={}, &block)
           super(prepared_statement_name, {:arguments=>bind_arguments}.merge(opts), &block)
         end
+
+        # Execute the given SQL on the database using execute_insert.
+        def execute_insert(sql, opts={}, &block)
+          super(prepared_statement_name, {:arguments=>bind_arguments}.merge(opts), &block)
+        end
+
       end
       
       # emulate level of support of bind arguments same to MySQL
@@ -213,39 +245,27 @@ module Sequel
       end
 
       def fetch_rows(sql)
-        def set_columns(conn)
-          @column_info = {}
-          @columns = []
-          conn.stmt_num_fields.times do |i|
-            k = conn.stmt_field_name i
-            key = output_identifier(k)
-            @column_info[key] = output_identifier(conn.stmt_field_type k)
-            @columns << key
+        execute(sql) do |stmt|
+          break if stmt.fail?
+          unless @columns
+            @column_info = {}
+            @columns = []
+            stmt.num_fields.times do |i|
+              k = stmt.field_name i
+              key = output_identifier(k)
+              @column_info[key] = output_identifier(stmt.field_type k)
+              @columns << key
+            end
           end
-        end
 
-        execute(sql) do |conn|
-          break unless conn.stmt
-          set_columns(conn) unless @columns
-
-          while res = conn.fetch_stmt_assoc
+          while res = stmt.fetch_assoc
             #yield res
             yield hash_row(res)
           end
-          conn.free_stmt
+          stmt.free
         end
         self
       end
-
-      # DB2 supports window functions
-      def supports_window_functions?
-        true
-      end
-
-      def supports_prepared_transactions?
-        true
-      end
-      
       # Store the given type of prepared statement in the associated database
       # with the given name.
       def prepare(type, name=nil, *values)
@@ -257,6 +277,15 @@ module Sequel
         end
         ps
       end
+      def supports_timestamp_usecs?
+        false
+      end
+
+      # DB2 supports window functions
+      def supports_window_functions?
+        true
+      end
+
       private
 
       # Modify the sql to limit the number of rows returned
@@ -276,16 +305,16 @@ module Sequel
       
       def convert_type(v, type)
         case type
-        # conversioin of time to ruby +Time+ object is not used here, because the
-        # automatically added date part can be misleading and dangerous
-        #when :time;
-            #Time.parse(v)
-        when :date; 
-            Date.parse(v)
-        when :timestamp; 
-            DateTime.parse(v)
-        else
-            v
+        when :time;
+          Sequel::SQLTime.parse(v)
+        when :date;
+          Date.parse(v)
+        when :timestamp;
+          DateTime.parse(v)
+        when :int;
+          v.to_i
+        else;
+          v
         end
       end
     end
