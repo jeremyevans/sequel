d13151d6d19b299a62a6081d1ce05e9b9168702e
Jeremy Evans <code@jeremyevans.net>
2011-09-13 05:42:43 -0700

    Add updated DB2 support to the db2 odbc subadapter

--- lib/sequel/adapters/odbc/db2.rb
+++ lib/sequel/adapters/odbc/db2.rb
@@ -1,20 +1,20 @@
+Sequel.require 'adapters/shared/db2'
+
 module Sequel
   module ODBC
     # Database and Dataset instance methods for DB2 specific
     # support via ODBC.
     module DB2
       module DatabaseMethods
+        include ::Sequel::DB2::DatabaseMethods
+
         def dataset(opts=nil)
           Sequel::ODBC::DB2::Dataset.new(self, opts)
         end
       end
       
       class Dataset < ODBC::Dataset
-        def select_limit_sql(sql)
-          if l = @opts[:limit]
-            sql << " FETCH FIRST #{l == 1 ? 'ROW' : "#{literal(l)} ROWS"} ONLY"
-          end
-        end
+        include ::Sequel::DB2::DatasetMethods
       end
     end
   end
