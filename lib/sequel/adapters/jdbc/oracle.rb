0c94ba685de376b503f3f83f31e31aa7c16d3603
Jeremy Evans <code@jeremyevans.net>
2011-10-10 15:25:58 -0700

    Some oracle schema parsing fixes for the jdbc oracle subadapter
    Parse integer columns as integers instead of decimals.  Parse date
    columns as datetime since they contain time components.
    
    Unless a schema is is given, skip schemas that don't match the
    current user when parsing the column schema for a table.

--- lib/sequel/adapters/jdbc/oracle.rb
+++ lib/sequel/adapters/jdbc/oracle.rb
@@ -14,6 +14,24 @@ module Sequel
         def dataset(opts=nil)
           Sequel::JDBC::Oracle::Dataset.new(self, opts)
         end
+        
+        private
+
+        def schema_parse_table(*)
+          sch = super
+          sch.each do |c, s|
+            if s[:type] == :decimal && s[:scale] == -127
+              s[:type] = :integer
+            elsif s[:db_type] == 'DATE'
+              s[:type] = :datetime
+            end
+          end
+          sch
+        end
+
+        def schema_parse_table_skip?(h, schema)
+          super || (h[:table_schem] != current_user unless schema)
+        end
       end
       
       # Dataset class for Oracle datasets accessed via JDBC.
