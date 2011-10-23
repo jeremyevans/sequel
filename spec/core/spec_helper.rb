0935eae931195a353a31e1f1bfcace39816fd85c
Jeremy Evans <code@jeremyevans.net>
2011-10-22 19:42:59 -0700

    Remove the MockDatabase and MockDataset class from the core specs
    They have now been completely replaced by the mock adapter.

--- spec/core/spec_helper.rb
+++ spec/core/spec_helper.rb
@@ -10,45 +10,7 @@ if ENV['SEQUEL_COLUMNS_INTROSPECTION']
   Sequel::Dataset.introspect_all_columns
 end
 
-class MockDataset < Sequel::Dataset
-  def insert(*args)
-    @db.execute insert_sql(*args)
-  end
-  
-  def update(*args)
-    @db.execute update_sql(*args)
-  end
-  
-  def fetch_rows(sql)
-    @db.execute(sql)
-    yield({:id => 1, :x => 1})
-  end
-
-  def quoted_identifier(c)
-    "\"#{c}\""
-  end
-end
-
-class MockDatabase < Sequel::Database
-  set_adapter_scheme :mock_old
-  @@quote_identifiers = false
-  self.identifier_input_method = nil
-  self.identifier_output_method = nil
-  attr_reader :sqls
-  
-  def execute(sql, opts={})
-    @sqls ||= []
-    @sqls << sql
-  end
-
-  def reset
-    @sqls = []
-  end
-
-  def transaction(opts={}); yield; end
-  
-  def dataset; MockDataset.new(self); end
-end
+Sequel.quote_identifiers = false
 
 class SchemaDummyDatabase < Sequel::Database
   attr_reader :sqls
