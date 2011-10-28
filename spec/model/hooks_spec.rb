0fffc0d1fdc3047a24d8888976fc64f2ec6a0540
Jeremy Evans <code@jeremyevans.net>
2011-10-27 22:37:51 -0700

    Cleanup hooks_spec

--- spec/model/hooks_spec.rb
+++ spec/model/hooks_spec.rb
@@ -1,52 +1,40 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), "spec_helper")
 
 describe "Model#after_initialize" do
-  specify "should be called after initialization" do
-    $values1 = nil
-    $reached_after_initialized = false
+  specify "should be called after initialization for both new object and objects retrieved from the database" do
     
-    a = Class.new(Sequel::Model)
-    a.class_eval do
+    a = Class.new(Sequel::Model) do
+      self::Foo = []
       columns :x, :y
       def after_initialize
-        $values1 = @values.clone
-        $reached_after_initialized = true
+        model::Foo << @values.clone
       end
     end
     
     a.new(:x => 1, :y => 2)
-    $values1.should == {:x => 1, :y => 2}
-    $reached_after_initialized.should == true
+    a.call(:x => 1, :y => 3)
+    a::Foo.should == [{:x => 1, :y => 2}, {:x => 1, :y => 3}]
   end
 end
 
 describe "Model#before_create && Model#after_create" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
+    @c = Class.new(Sequel::Model(:items)) do
       columns :x
       set_primary_key :x
       unrestrict_primary_key
-       
-      def _save_refresh
-      end
       
       def after_create
         MODEL_DB << "BLAH after"
       end
     end
+    MODEL_DB.reset
   end
   
   specify "should be called around new record creation" do
     @c.send(:define_method, :before_create){MODEL_DB << "BLAH before"}
     @c.create(:x => 2)
-    MODEL_DB.sqls.should == [
-      'BLAH before',
-      'INSERT INTO items (x) VALUES (2)',
-      'BLAH after'
-    ]
+    MODEL_DB.sqls.should == ['BLAH before', 'INSERT INTO items (x) VALUES (2)', 'BLAH after', 'SELECT * FROM items WHERE (x = 2) LIMIT 1']
   end
 
   specify ".create should cancel the save and raise an error if before_create returns false and raise_on_save_failure is true" do
@@ -66,24 +54,20 @@ end
 
 describe "Model#before_update && Model#after_update" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
+    @c = Class.new(Sequel::Model(:items)) do
       columns :id, :x
-      def after_update; MODEL_DB << "BLAH after" end
+      def after_update
+        MODEL_DB << "BLAH after"
+      end
     end
+    MODEL_DB.reset
   end
   
   specify "should be called around record update" do
     @c.send(:define_method, :before_update){MODEL_DB << "BLAH before"}
     m = @c.load(:id => 2233, :x=>123)
     m.save
-    MODEL_DB.sqls.should == [
-      'BLAH before',
-      'UPDATE items SET x = 123 WHERE (id = 2233)',
-      'BLAH after'
-    ]
+    MODEL_DB.sqls.should == ['BLAH before', 'UPDATE items SET x = 123 WHERE (id = 2233)', 'BLAH after']
   end
 
   specify "#save should cancel the save and raise an error if before_update returns false and raise_on_save_failure is true" do
@@ -111,37 +95,28 @@ end
 
 describe "Model#before_save && Model#after_save" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
+    @c = Class.new(Sequel::Model(:items)) do
       columns :x
-      def after_save; MODEL_DB << "BLAH after" end
+      def after_save
+        MODEL_DB << "BLAH after"
+      end
     end
+    MODEL_DB.reset
   end
   
   specify "should be called around record update" do
     @c.send(:define_method, :before_save){MODEL_DB << "BLAH before"}
     m = @c.load(:id => 2233, :x=>123)
     m.save
-    MODEL_DB.sqls.should == [
-      'BLAH before',
-      'UPDATE items SET x = 123 WHERE (id = 2233)',
-      'BLAH after'
-    ]
+    MODEL_DB.sqls.should == ['BLAH before', 'UPDATE items SET x = 123 WHERE (id = 2233)', 'BLAH after']
   end
   
   specify "should be called around record creation" do
     @c.send(:define_method, :before_save){MODEL_DB << "BLAH before"}
-    @c.send(:define_method, :_save_refresh){}
     @c.set_primary_key :x
     @c.unrestrict_primary_key
     @c.create(:x => 2)
-    MODEL_DB.sqls.should == [
-      'BLAH before',
-      'INSERT INTO items (x) VALUES (2)',
-      'BLAH after'
-    ]
+    MODEL_DB.sqls.should == ['BLAH before', 'INSERT INTO items (x) VALUES (2)', 'BLAH after', 'SELECT * FROM items WHERE (x = 2) LIMIT 1']
   end
 
   specify "#save should cancel the save and raise an error if before_save returns false and raise_on_save_failure is true" do
@@ -169,27 +144,19 @@ end
 
 describe "Model#before_destroy && Model#after_destroy" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
-      def after_destroy; MODEL_DB << "BLAH after"; end
-      
-      def delete
-        MODEL_DB << "DELETE BLAH"
+    @c = Class.new(Sequel::Model(:items)) do
+      def after_destroy
+        MODEL_DB << "BLAH after"
       end
     end
+    MODEL_DB.reset
   end
   
   specify "should be called around record destruction" do
     @c.send(:define_method, :before_destroy){MODEL_DB << "BLAH before"}
     m = @c.load(:id => 2233)
     m.destroy
-    MODEL_DB.sqls.should == [
-      'BLAH before',
-      'DELETE BLAH',
-      'BLAH after'
-    ]
+    MODEL_DB.sqls.should == ['BLAH before', 'DELETE FROM items WHERE (id = 2233)', 'BLAH after']
   end
 
   specify "#destroy should cancel the destroy and raise an error if before_destroy returns false and raise_on_save_failure is true" do
@@ -215,16 +182,15 @@ end
 
 describe "Model#before_validation && Model#after_validation" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
-      def after_validation; MODEL_DB << "BLAH after" end
+    @c = Class.new(Sequel::Model(:items)) do
+      columns :id
+      def after_validation
+        MODEL_DB << "BLAH after"
+      end
 
       def validate
         errors.add(:id, 'not valid') unless id == 2233
       end
-      columns :id
     end
   end
   
@@ -234,7 +200,6 @@ describe "Model#before_validation && Model#after_validation" do
     m.should be_valid
     MODEL_DB.sqls.should == ['BLAH before', 'BLAH after']
 
-    MODEL_DB.sqls.clear
     m = @c.load(:id => 22)
     m.should_not be_valid
     MODEL_DB.sqls.should == ['BLAH before', 'BLAH after']
@@ -246,7 +211,6 @@ describe "Model#before_validation && Model#after_validation" do
     m.save.should == m
     MODEL_DB.sqls.should == ['BLAH before', 'BLAH after', 'UPDATE items SET x = 123 WHERE (id = 2233)']
 
-    MODEL_DB.sqls.clear
     m = @c.load(:id => 22)
     m.raise_on_save_failure = false
     m.save.should == nil
@@ -283,13 +247,10 @@ end
 
 describe "Model around filters" do
   before do
-    MODEL_DB.reset
-
-    @c = Class.new(Sequel::Model(:items))
-    @c.class_eval do
+    @c = Class.new(Sequel::Model(:items)) do
       columns :id, :x
-      def _save_refresh(*a) end
     end
+    MODEL_DB.reset
   end
   
   specify "around_create should be called around new record creation" do
@@ -301,7 +262,7 @@ describe "Model around filters" do
       end
     end
     @c.create(:x => 2)
-    MODEL_DB.sqls.should == [ 'ac_before', 'INSERT INTO items (x) VALUES (2)', 'ac_after' ]
+    MODEL_DB.sqls.should == ['ac_before', 'INSERT INTO items (x) VALUES (2)', 'ac_after', "SELECT * FROM items WHERE (id = 10) LIMIT 1"]
   end
 
   specify "around_delete should be called around record destruction" do
@@ -313,7 +274,7 @@ describe "Model around filters" do
       end
     end
     @c.load(:id=>1, :x => 2).destroy
-    MODEL_DB.sqls.should == [ 'ad_before', 'DELETE FROM items WHERE (id = 1)', 'ad_after' ]
+    MODEL_DB.sqls.should == ['ad_before', 'DELETE FROM items WHERE (id = 1)', 'ad_after']
   end
   
   specify "around_update should be called around updating existing records" do
@@ -325,10 +286,10 @@ describe "Model around filters" do
       end
     end
     @c.load(:id=>1, :x => 2).save
-    MODEL_DB.sqls.should == [ 'au_before', 'UPDATE items SET x = 2 WHERE (id = 1)', 'au_after' ]
+    MODEL_DB.sqls.should == ['au_before', 'UPDATE items SET x = 2 WHERE (id = 1)', 'au_after']
   end
 
-  specify "around_update should be called around saving both new and existing records, around either after_create and after_update" do
+  specify "around_save should be called around saving both new and existing records, around either after_create and after_update" do
     @c.class_eval do
       def around_update
         MODEL_DB << 'au_before'
@@ -347,10 +308,9 @@ describe "Model around filters" do
       end
     end
     @c.create(:x => 2)
-    MODEL_DB.sqls.should == [ 'as_before', 'ac_before', 'INSERT INTO items (x) VALUES (2)', 'ac_after', 'as_after' ]
-    MODEL_DB.sqls.clear
+    MODEL_DB.sqls.should == ['as_before', 'ac_before', 'INSERT INTO items (x) VALUES (2)', 'ac_after', 'as_after', "SELECT * FROM items WHERE (id = 10) LIMIT 1"]
     @c.load(:id=>1, :x => 2).save
-    MODEL_DB.sqls.should == [ 'as_before', 'au_before', 'UPDATE items SET x = 2 WHERE (id = 1)', 'au_after', 'as_after' ]
+    MODEL_DB.sqls.should == ['as_before', 'au_before', 'UPDATE items SET x = 2 WHERE (id = 1)', 'au_after', 'as_after']
   end
 
   specify "around_validation should be called around validating records" do
@@ -438,24 +398,7 @@ end
 
 describe "Model#after_commit and #after_rollback" do
   before do
-    @logger = Object.new
-    def @logger.method_missing(meth, sql, args=nil)
-      (@sqls ||= []) << sql
-    end
-    def @logger.sqls
-      @sqls
-    end
-    @db = Class.new(Sequel::Database) do
-      def connect(*)
-        Object.new
-      end
-      def log_connection_execute(conn, sql)
-        execute(sql)
-      end
-      def execute(sql, opts={})
-        @loggers.each{|l| l.info(sql)}
-      end
-    end.new(:loggers=>[@logger], :servers=>{:test=>{}})
+    @db = Sequel.mock(:servers=>{:test=>{}})
     @m = Class.new(Sequel::Model(@db[:items])) do
       attr_accessor :rb
       def _delete
@@ -482,27 +425,26 @@ describe "Model#after_commit and #after_rollback" do
       end
     end
     @m.use_transactions = true
-    @m.set_primary_key :id
     @o = @m.load({})
+    @db.sqls
   end
 
   specify "should call after_commit for save after the transaction commits if it commits" do
     @o.save
-    @logger.sqls.should == ['BEGIN', 'as', 'COMMIT', 'ac']
+    @db.sqls.should == ['BEGIN', 'as', 'COMMIT', 'ac']
   end
 
   specify "should call after_rollback for save after the transaction rolls back if it rolls back" do
     @o.rb = true
     @o.save
-    @logger.sqls.should == ['BEGIN', 'as', 'ROLLBACK', 'ar']
+    @db.sqls.should == ['BEGIN', 'as', 'ROLLBACK', 'ar']
   end
 
   specify "should have after_commit respect any surrounding transactions" do
     @db.transaction do
       @o.save
     end
-    @logger.sqls.should == ['BEGIN', 'as', 'COMMIT', 'ac']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN', 'as', 'COMMIT', 'ac']
   end
 
   specify "should have after_rollback respect any surrounding transactions" do
@@ -510,16 +452,14 @@ describe "Model#after_commit and #after_rollback" do
       @o.rb = true
       @o.save
     end
-    @logger.sqls.should == ['BEGIN', 'as', 'ROLLBACK', 'ar']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN', 'as', 'ROLLBACK', 'ar']
   end
 
   specify "should have after_commit work with surrounding transactions and sharding" do
     @db.transaction(:server=>:test) do
       @o.save
     end
-    @logger.sqls.should == ['BEGIN', 'BEGIN', 'as', 'COMMIT', 'ac', 'COMMIT']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN -- test', 'BEGIN', 'as', 'COMMIT', 'ac', 'COMMIT -- test']
   end
 
   specify "should have after_rollback work with surrounding transactions and sharding" do
@@ -527,27 +467,25 @@ describe "Model#after_commit and #after_rollback" do
       @o.rb = true
       @o.save
     end
-    @logger.sqls.should == ['BEGIN', 'BEGIN', 'as', 'ROLLBACK', 'ar', 'COMMIT']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN -- test', 'BEGIN', 'as', 'ROLLBACK', 'ar', 'COMMIT -- test']
   end
 
   specify "should call after_destroy_commit for destroy after the transaction commits if it commits" do
     @o.destroy
-    @logger.sqls.should == ['BEGIN', 'ad', 'COMMIT', 'adc']
+    @db.sqls.should == ['BEGIN', 'ad', 'COMMIT', 'adc']
   end
 
   specify "should call after_destroy_rollback for destroy after the transaction rolls back if it rolls back" do
     @o.rb = true
     @o.destroy
-    @logger.sqls.should == ['BEGIN', 'ad', 'ROLLBACK', 'adr']
+    @db.sqls.should == ['BEGIN', 'ad', 'ROLLBACK', 'adr']
   end
 
   specify "should have after_destroy_commit respect any surrounding transactions" do
     @db.transaction do
       @o.destroy
     end
-    @logger.sqls.should == ['BEGIN', 'ad', 'COMMIT', 'adc']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN', 'ad', 'COMMIT', 'adc']
   end
 
   specify "should have after_destroy_rollback respect any surrounding transactions" do
@@ -555,16 +493,14 @@ describe "Model#after_commit and #after_rollback" do
       @o.rb = true
       @o.destroy
     end
-    @logger.sqls.should == ['BEGIN', 'ad', 'ROLLBACK', 'adr']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN', 'ad', 'ROLLBACK', 'adr']
   end
 
   specify "should have after_destroy commit work with surrounding transactions and sharding" do
     @db.transaction(:server=>:test) do
       @o.destroy
     end
-    @logger.sqls.should == ['BEGIN', 'BEGIN', 'ad', 'COMMIT', 'adc', 'COMMIT']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN -- test', 'BEGIN', 'ad', 'COMMIT', 'adc', 'COMMIT -- test']
   end
 
   specify "should have after_destroy_rollback work with surrounding transactions and sharding" do
@@ -572,8 +508,6 @@ describe "Model#after_commit and #after_rollback" do
       @o.rb = true
       @o.destroy
     end
-    @logger.sqls.should == ['BEGIN', 'BEGIN', 'ad', 'ROLLBACK', 'adr', 'COMMIT']
-    @logger.sqls.clear
+    @db.sqls.should == ['BEGIN -- test', 'BEGIN', 'ad', 'ROLLBACK', 'adr', 'COMMIT -- test']
   end
-
 end
