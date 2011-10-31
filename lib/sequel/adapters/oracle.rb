cbdc1fed14a04dd7c452dcc98eef4b147c475421
Jeremy Evans <code@jeremyevans.net>
2011-10-31 09:44:16 -0700

    Allow setting the prefetch_rows attribute on connection objects in the Oracle adapter

--- lib/sequel/adapters/oracle.rb
+++ lib/sequel/adapters/oracle.rb
@@ -34,6 +34,7 @@ module Sequel
           dbname = opts[:host]
         end
         conn = OCI8.new(opts[:user], opts[:password], dbname, opts[:privilege])
+        conn.prefetch_rows = typecast_value_integer(opts[:prefetch_rows]) if opts[:prefetch_rows]
         conn.autocommit = true
         conn.non_blocking = true
         
