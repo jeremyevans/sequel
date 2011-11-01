d8b4ef6f1fe15bd530bc0aabfd98074b78bea3dc
Jeremy Evans <code@jeremyevans.net>
2011-11-01 10:08:47 -0700

    Bump version to 3.29.0

--- lib/sequel/version.rb
+++ lib/sequel/version.rb
@@ -3,7 +3,7 @@ module Sequel
   MAJOR = 3
   # The minor version of Sequel.  Bumped for every non-patch level
   # release, generally around once a month.
-  MINOR = 28
+  MINOR = 29
   # The tiny version of Sequel.  Usually 0, only bumped for bugfix
   # releases that fix regressions from previous versions.
   TINY  = 0
