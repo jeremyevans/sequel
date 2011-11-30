89371df015a34813aafa5ceb4390a4f2ee801dbc
Jeremy Evans <code@jeremyevans.net>
2011-11-30 11:57:34 -0800

    Cleanup the sql_expr spec

--- spec/extensions/sql_expr_spec.rb
+++ spec/extensions/sql_expr_spec.rb
@@ -4,86 +4,46 @@ describe "Sequel sql_expr extension" do
   specify "Object#sql_expr should wrap the object in a GenericComplexExpression" do
     o = Object.new
     s = o.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::GenericComplexExpression)
-    s.op.should == :NOOP
-    s.args.should == [o]
-    (s+1).should be_a_kind_of(Sequel::SQL::NumericExpression)
-    (s & true).should be_a_kind_of(Sequel::SQL::BooleanExpression)
-    (s < 1).should be_a_kind_of(Sequel::SQL::BooleanExpression)
-    s.sql_subscript(1).should be_a_kind_of(Sequel::SQL::Subscript)
-    s.like('a').should be_a_kind_of(Sequel::SQL::BooleanExpression)
-    s.as(:a).should be_a_kind_of(Sequel::SQL::AliasedExpression)
-    s.cast(Integer).should be_a_kind_of(Sequel::SQL::Cast)
-    s.desc.should be_a_kind_of(Sequel::SQL::OrderedExpression)
-    s.sql_string.should be_a_kind_of(Sequel::SQL::StringExpression)
+    s.should == Sequel::SQL::GenericComplexExpression.new(:NOOP, o)
+    (s+1).should == Sequel::SQL::NumericExpression.new(:+, s, 1)
+    (s & true).should == Sequel::SQL::BooleanExpression.new(:AND, s, true)
+    (s < 1).should == Sequel::SQL::BooleanExpression.new(:<, s, 1)
+    s.sql_subscript(1).should == Sequel::SQL::Subscript.new(s, [1])
+    s.like('a').should == Sequel::SQL::BooleanExpression.new(:LIKE, s, 'a')
+    s.as(:a).should == Sequel::SQL::AliasedExpression.new(s, :a)
+    s.cast(Integer).should == Sequel::SQL::Cast.new(s, Integer)
+    s.desc.should == Sequel::SQL::OrderedExpression.new(s, true)
+    s.sql_string.should == Sequel::SQL::StringExpression.new(:NOOP, s)
   end
 
   specify "Numeric#sql_expr should wrap the object in a NumericExpression" do
-    [1, 2.0, 2^40, BigDecimal.new('1.0')].each do |o|
-      s = o.sql_expr
-      s.should be_a_kind_of(Sequel::SQL::NumericExpression)
-      s.op.should == :NOOP
-      s.args.should == [o]
+    [1, 2.0, 2^70, BigDecimal.new('1.0')].each do |o|
+      o.sql_expr.should == Sequel::SQL::NumericExpression.new(:NOOP, o)
     end
   end
 
   specify "String#sql_expr should wrap the object in a StringExpression" do
-    o = ""
-    s = o.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::StringExpression)
-    s.op.should == :NOOP
-    s.args.should == [o]
+    "".sql_expr.should == Sequel::SQL::StringExpression.new(:NOOP, "")
   end
 
   specify "NilClass, TrueClass, and FalseClass#sql_expr should wrap the object in a BooleanExpression" do
     [nil, true, false].each do |o|
-      s = o.sql_expr
-      s.should be_a_kind_of(Sequel::SQL::BooleanExpression)
-      s.op.should == :NOOP
-      s.args.should == [o]
+      o.sql_expr.should == Sequel::SQL::BooleanExpression.new(:NOOP, o)
     end
   end
 
   specify "Proc#sql_expr should should treat the object as a virtual row block" do
-    s = proc{a}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.value.should == :a
-
-    s = proc{a__b}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::QualifiedIdentifier)
-    s.table.should == "a"
-    s.column.should == "b"
-
-    s = proc{a(b)}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::Function)
-    s.f.should == :a
-    s.args.length.should == 1
-    s.args.first.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.args.first.value.should == :b
+    proc{a}.sql_expr.should == Sequel::SQL::Identifier.new(:a)
+    proc{a__b}.sql_expr.should == Sequel::SQL::QualifiedIdentifier.new('a', 'b')
+    proc{a(b)}.sql_expr.should == Sequel::SQL::Function.new(:a, Sequel::SQL::Identifier.new(:b))
   end
 
   specify "Proc#sql_expr should should wrap the object in a GenericComplexExpression if the object is not already an expression" do
-    s = proc{1}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::GenericComplexExpression)
-    s.op.should == :NOOP
-    s.args.should == [1]
+    proc{1}.sql_expr.should == Sequel::SQL::GenericComplexExpression.new(:NOOP, 1)
   end
 
   specify "Proc#sql_expr should should convert a hash or array of two element arrays to a BooleanExpression" do
-    s = proc{{a=>b}}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::BooleanExpression)
-    s.op.should == :"="
-    s.args.first.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.args.first.value.should == :a
-    s.args.last.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.args.last.value.should == :b
-
-    s = proc{[[a,b]]}.sql_expr
-    s.should be_a_kind_of(Sequel::SQL::BooleanExpression)
-    s.op.should == :"="
-    s.args.first.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.args.first.value.should == :a
-    s.args.last.should be_a_kind_of(Sequel::SQL::Identifier)
-    s.args.last.value.should == :b
+    proc{{a=>b}}.sql_expr.should == Sequel::SQL::BooleanExpression.new(:'=', Sequel::SQL::Identifier.new(:a), Sequel::SQL::Identifier.new(:b))
+    proc{[[a,b]]}.sql_expr.should == Sequel::SQL::BooleanExpression.new(:'=', Sequel::SQL::Identifier.new(:a), Sequel::SQL::Identifier.new(:b))
   end
 end
