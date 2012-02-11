bb9753f5a6eefcba1a79c88cd1b73dec01f61586
Jason Rogers <jacaetevha@gmail.com>
2012-02-11 15:27:59 -0500

    added special singular rule for /status/i so that camelizing status does not return statu

--- spec/extensions/inflector_spec.rb
+++ spec/extensions/inflector_spec.rb
@@ -72,6 +72,9 @@ describe String do
   it "#classify should tranform table names to class names" do
     "egg_and_hams".classify.should == "EggAndHam"
     "post".classify.should == "Post"
+    "status".classify.should == "Status"
+    "statuses".classify.should == "Status"
+    "Status".classify.should == "Status"
   end
   
   it "#foreign_key should create a foreign key name from a class name" do
