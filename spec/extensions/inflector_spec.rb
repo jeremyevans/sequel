36b1646d89124ab077c2f93cf06c37f8941b6893
Jason Rogers <jacaetevha@gmail.com>
2012-02-11 15:30:53 -0500

    expand singular rule for /(alias|(?:stat|octop|vir|b)us)$/i

--- spec/extensions/inflector_spec.rb
+++ spec/extensions/inflector_spec.rb
@@ -73,8 +73,9 @@ describe String do
     "egg_and_hams".classify.should == "EggAndHam"
     "post".classify.should == "Post"
     "status".classify.should == "Status"
-    "statuses".classify.should == "Status"
-    "Status".classify.should == "Status"
+    "octopuses".classify.should == "Octopus"
+    "Bus".classify.should == "Bus"
+    "ALIas".classify.should == "ALIas"
   end
   
   it "#foreign_key should create a foreign key name from a class name" do
