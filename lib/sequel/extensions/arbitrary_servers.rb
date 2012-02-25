ca882f11718eb43de1306a8418610bd50345a7a0
Jeremy Evans <code@jeremyevans.net>
2012-02-24 23:00:42 -0800

    Add arbitrary_servers extension for connecting to arbitrary servers
    With this extension, you just pass a hash of connection options that
    get merged into the default options.  It's designed to work well
    with the server block plugin, making it possible for to use arbitrary
    servers easily, such as per-request in a web framework.
    
    As you can't be sure if the arbitrary server will be needed again
    do not cache the connection, disconnect as soon as the connection
    is finished being used.

---
+++ lib/sequel/extensions/arbitrary_servers.rb
@@ -0,0 +1,98 @@
+# The arbitrary_servers extension allows you to connect to arbitrary
+# servers/shards that were not defined when you created the database.
+# To use it, you first extend the Database's connection pool with the
+# Sequel::ArbitraryServers module:
+#
+#   Sequel.extension :arbitrary_servers
+#   DB.pool.extend Sequel::ArbitraryServers
+#
+# Then you can pass arbitrary connection options for the server/shard
+# to use as a hash:
+#
+#   DB[:table].server(:host=>'...', :database=>'...').all
+#
+# Because Sequel can never be sure that the connection will be reused,
+# arbitrary connections are disconnected as soon as the outermost block
+# that uses them exits.  So this example uses the same connection:
+#
+#   DB.transaction(:server=>{:host=>'...', :database=>'...'}) do |c|
+#     DB.transaction(:server=>{:host=>'...', :database=>'...'}) do |c2|
+#       # c == c2
+#     end
+#   end
+#
+# But this example does not:
+#
+#   DB.transaction(:server=>{:host=>'...', :database=>'...'}) do |c|
+#   end
+#   DB.transaction(:server=>{:host=>'...', :database=>'...'}) do |c2|
+#     # c != c2
+#   end
+#
+# You can use this extension in conjunction with the server_block
+# extension:
+#
+#   DB.with_server(:host=>'...', :database=>'...') do
+#     # All of these use the host/database given to with_server
+#     DB[:table].insert(...)
+#     DB[:table].update(...)
+#     DB.tables
+#     DB[:table].all
+#   end
+#
+# Note that this extension only works with the sharded threaded connection
+# pool.  If you are using the sharded single connection pool, you need
+# to switch to the sharded threaded connection pool before using this
+# extension.
+
+module Sequel
+  module ArbitraryServers
+    private
+
+    # If server is a hash, create a new connection for
+    # it, and cache it first by thread and then server.
+    def acquire(thread, server)
+      if server.is_a?(Hash)
+        sync{@allocated[thread] ||= {}}[server] = make_new(server)
+      else
+        super
+      end
+    end
+    
+    # If server is a hash, the entry for it probably doesn't
+    # exist in the @allocated hash, so check for existence to
+    # avoid calling nil.[]
+    def owned_connection(thread, server)
+      if server.is_a?(Hash)
+        if a = sync{@allocated[thread]}
+          a[server]
+        end
+      else
+        super
+      end
+    end
+
+    # If server is a hash, return it directly.
+    def pick_server(server)
+      if server.is_a?(Hash)
+        server
+      else
+        super
+      end
+    end
+
+    # If server is a hash, delete the thread from the allocated
+    # connections for that server.  Additionally, if this was the last thread
+    # using that server, delete the server from the @allocated hash.
+    def release(thread, conn, server)
+      if server.is_a?(Hash)
+        a = @allocated[thread]
+        a.delete(server)
+        @allocated.delete(thread) if a.empty?
+        @disconnection_proc.call(conn) if @disconnection_proc
+      else  
+        super
+      end
+    end
+  end
+end
---
+++ spec/extensions/arbitrary_servers_spec.rb
@@ -0,0 +1,114 @@
+require File.join(File.dirname(File.expand_path(__FILE__)), "spec_helper")
+
+if RUBY_VERSION >= '1.8.7'
+describe "arbtirary servers" do
+  before do
+    @db = Sequel.mock(:servers=>{})
+    @db.pool.extend Sequel::ArbitraryServers
+  end
+
+  specify "should allow arbitrary server options using a hash" do
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      c.opts[:host].should == 'host1'
+      c.opts[:database].should == 'db1'
+    end
+  end
+
+  specify "should not cache connections to arbitrary servers" do
+    x = nil
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      x = c
+    end
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c2|
+      c2.should_not equal(x)
+    end
+  end
+
+  specify "should yield same connection correctly when nesting" do
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      @db.synchronize(:host=>'host1', :database=>'db1') do |c2|
+        c2.should equal(c)
+      end
+    end
+  end
+
+  specify "should disconnect when connection is finished" do
+    x, x1 = nil, nil
+    @db.meta_def(:disconnect_connection){|c| x = c}
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      x1 = c
+      @db.synchronize(:host=>'host1', :database=>'db1') do |c2|
+        c2.should equal(c)
+      end
+      x.should equal(nil)
+    end
+    x.should equal(x1)
+  end
+
+  specify "should yield different connection correctly when nesting" do
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      c.opts[:host].should == 'host1'
+      @db.synchronize(:host=>'host2', :database=>'db1') do |c2|
+        c2.opts[:host].should == 'host2'
+        c2.should_not equal(c)
+      end
+    end
+  end
+
+  specify "should respect multithreaded access" do
+    @db.synchronize(:host=>'host1', :database=>'db1') do |c|
+      Thread.new do
+        @db.synchronize(:host=>'host1', :database=>'db1') do |c2|
+          c2.should_not equal(c)
+        end
+      end.join
+    end
+  end
+
+  specify "should work correctly with server_block plugin" do
+    @db.extend Sequel::ServerBlock
+    @db.with_server(:host=>'host1', :database=>'db1') do
+      @db.synchronize do |c|
+        c.opts[:host].should == 'host1'
+        c.opts[:database].should == 'db1'
+        @db.synchronize do |c2|
+          c2.should equal(c)
+        end
+      end
+    end
+  end
+
+
+  specify "should respect multithreaded access with server block plugin" do
+    @db.extend Sequel::ServerBlock
+    q, q1 = Queue.new, Queue.new
+    
+    t = nil
+    @db[:t].all
+    @db.with_server(:host=>'a') do
+      @db[:t].all
+      t = Thread.new do
+        @db[:t].all
+        @db.with_server(:host=>'c') do
+          @db[:t].all
+          @db.with_server(:host=>'d'){@db[:t].all}
+          q.push nil
+          q1.pop
+          @db[:t].all
+        end
+        @db[:t].all
+      end
+      q.pop
+      @db.with_server(:host=>'b'){@db[:t].all}
+      @db[:t].all
+    end
+    @db[:t].all
+    q1.push nil
+    t.join
+    @db.sqls.should == ['SELECT * FROM t', 'SELECT * FROM t -- {:host=>"a"}', 'SELECT * FROM t', 'SELECT * FROM t -- {:host=>"c"}', 'SELECT * FROM t -- {:host=>"d"}',
+      'SELECT * FROM t -- {:host=>"b"}', 'SELECT * FROM t -- {:host=>"a"}', 'SELECT * FROM t', 'SELECT * FROM t -- {:host=>"c"}', 'SELECT * FROM t']
+  end
+end
+else
+  skip_warn "arbitrary_servers plugin: only works on ruby 1.8.7+"
+end
