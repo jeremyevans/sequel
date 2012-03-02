c467b8397420912969d75f3ab6589eb2e09a0d94
Jeremy Evans <code@jeremyevans.net>
2012-03-02 10:44:53 -0800

    Add pg_auto_parameterize extension, for automatically parameterizing queries when using postgres adapter with pg driver
    Note that parameterizing queries doesn't necessarily make things
    faster or more safe by itself.  However, it opens up other
    opportunities for optimization that will be coming shortly.

---
+++ lib/sequel/extensions/pg_auto_parameterize.rb
@@ -0,0 +1,178 @@
+# This extension allows Sequel's postgres adapter to automatically
+# parameterize all common queries.  Sequel's default behavior has always
+# been to literalize all arguments unless specifically using
+# parameters (via :$arg placeholders and the prepare/call methods).
+# This extension makes Sequel take all string, numeric, date, and
+# time types and automatically turn them into parameters. Example:
+#
+#   # Default
+#   DB[:test].where(:a=>1)
+#   # SQL: SELECT * FROM test WHERE a = 1
+#
+#   DB.extend Sequel::Postgres::AutoParameterize::DatabaseMethods
+#   DB[:test].where(:a=>1)
+#   # SQL: SELECT * FROM test WHERE a = $1 (args: [1])
+#
+# This extension is not necessarily faster or more safe than the
+# default behavior.  In some cases it is faster, such as when using
+# large strings.  However, there are also some known issues with
+# this approach:
+#
+# 1. Because of the way it operates, it has no context to make a
+#    determination about whether to literalize an object or not.
+#    For example, if it comes across an integer, it will turn it
+#    into a parameter.  That breaks code such as:
+#
+#      DB[:table].select(:a, :b).order(2, 1)
+#
+#    Since it will use the following SQL (which isn't valid):
+#
+#      SELECT a, b FROM table ORDER BY $1, $2
+#     
+#    To work around this, you can either specify the columns
+#    manually or use a literal string:
+#
+#      DB[:table].select(:a, :b).order(:b, :a)
+#      DB[:table].select(:a, :b).order('2, 1'.lit)
+#
+# 2. In order to avoid many type errors, it attempts to guess the
+#    appropriate type and automatically casts all placeholders.
+#    Unfortunately, if the type guess is incorrect, the query will
+#    be rejected.  For example, the following works without
+#    automatic parameterization, but fails with it:
+#
+#      DB[:table].select(:date + '1 day')
+#
+#    To work around this, you can just add the necessary casts
+#    manually:
+#
+#      DB[:table].select(:date + '1 day'.cast(:interval))
+#
+# You can also work around any issues that come up by disabling automatic
+# parameterization by calling the no_auto_parameterize method on the
+# dataset (which returns a clone of the dataset).
+#
+# It is likely there are other corner cases I am not yet aware of
+# when using this extension, so use this extension with caution.
+#
+# This extension is only compatible when using the pg driver, not
+# when using the old postgres driver or the postgres-pr driver.
+
+module Sequel
+  module Postgres
+    # Enable automatically parameterizing queries by hijacking the
+    # SQL query string that Sequel builds to also hold the array
+    # of parameters.
+    module AutoParameterize
+      # String that holds an array of parameters
+      class StringWithArray < ::String
+        # The array of parameters used by this query.
+        attr_reader :args
+
+        # Add a new parameter to this query, which adds
+        # the parameter to the array of parameters, and an
+        # SQL placeholder to the query itself.
+        def add_arg(s, type)
+          @args ||= []
+          @args << s
+          self << "$#{@args.length}::#{type}"
+        end
+
+        # Show args when the string is inspected
+        def inspect
+          @args ? "#{self}; #{@args.inspect}".inspect : super
+        end
+      end
+
+      module DatabaseMethods
+        # Extend the database's datasets with the necessary code.
+        def self.extended(db)
+          db.extend_datasets(DatasetMethods)
+        end
+
+        # If the sql string has an embedded parameter array,
+        # extract the arguments from that.
+        def execute(sql, opts={})
+          if sql.is_a?(StringWithArray) && (args = sql.args)
+            opts = opts.merge(:arguments=>args)
+          end
+          super
+        end
+        
+        # If the sql string has an embedded parameter array,
+        # extract the arguments from that.
+        def execute_insert(sql, opts={})
+          if sql.is_a?(StringWithArray) && (args = sql.args)
+            opts = opts.merge(:arguments=>args)
+          end
+          super
+        end
+      end
+
+      module DatasetMethods
+        # Return a clone of the dataset that will not do
+        # automatic parameterization.
+        def no_auto_parameterize
+          clone(:no_auto_parameterize=>true)
+        end
+
+        protected
+
+        # Disable automatic parameterization for prepared statements,
+        # since they will use manual parameterization.
+        def to_prepared_statement(*a)
+          opts[:no_auto_parameterize] ? super : no_auto_parameterize.to_prepared_statement(*a)
+        end
+
+        private
+
+        # For strings, numeric arguments, and date/time arguments, add
+        # them as parameters to the query instead of literalizing them
+        # into the SQL.
+        def literal_append(sql, v)
+          if sql.is_a?(StringWithArray)
+            case v
+            when String
+              case v
+              when LiteralString
+                super
+              when Sequel::SQL::Blob
+                # PostgreSQL appears to require the blobs are escaped,
+                # otherwise it misses stuff after "\0".
+                sql.add_arg(db.synchronize{|c| c.escape_bytea(v)}, :bytea)
+              else
+                sql.add_arg(v, :text)
+              end
+            when Bignum
+              sql.add_arg(v, :int8)
+            when Fixnum
+              sql.add_arg(v, :int4)
+            when Float
+              sql.add_arg(v, :"double precision")
+            when BigDecimal
+              sql.add_arg(v, :numeric)
+            when Sequel::SQLTime
+              # Default pg code will not get fractional seconds
+              sql.add_arg(literal(v)[1...-1], :time)
+            when Time, DateTime
+              # Default pg code will not get fractional seconds
+              sql.add_arg(literal(v)[1...-1], :timestamp)
+            when Date
+              sql.add_arg(v, :date)
+            else
+              super
+            end
+          else
+            super
+          end
+        end
+
+        # Unless auto parameterization is turned off, use a string that
+        # can store the parameterized arguments.
+        def sql_string_origin
+          opts[:no_auto_parameterize] ? super : StringWithArray.new
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/pg_auto_parameterize_spec.rb
@@ -0,0 +1,64 @@
+require File.join(File.dirname(File.expand_path(__FILE__)), "spec_helper")
+
+describe "pg_auto_parameterize extension" do
+  before do
+    @db = Sequel.connect('mock://postgres', :quote_identifiers=>false)
+    @db.extend Sequel::Postgres::AutoParameterize::DatabaseMethods
+    @db.synchronize{|c| def c.escape_bytea(v) v*2 end}
+  end
+
+  it "should automatically parameterize queries strings, blobs, numerics, dates, and times" do
+    pr = proc do |ds, sql, *args|
+      arg = args[0]
+      parg = args[1] || arg
+      s = ds.filter(:a=>arg).sql
+      s.should == sql
+      s.args.should == (parg == :nil ? nil : [parg])
+    end
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::int4)', 1)
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::int8)', 18446744073709551616)
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::double precision)', 1.1)
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::numeric)', BigDecimal.new('1.01'))
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::text)', "a")
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::bytea)', "a\0b".to_sequel_blob, "a\0ba\0b")
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = 1)', '1'.lit, :nil)
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::time)', Sequel::SQLTime.create(1, 2, 3, 500000), "01:02:03.500000")
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::date)', Date.today)
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::timestamp)', DateTime.new(2012, 1, 2, 3, 4, 5), "2012-01-02 03:04:05.000000+0000")
+    pr.call(@db[:table], 'SELECT * FROM table WHERE (a = $1::timestamp)', Time.utc(2012, 1, 2, 3, 4, 5), "2012-01-02 03:04:05.000000+0000")
+  end
+
+  it "should extract parameters from query string when executing" do
+    @db.fetch = {:a=>1}
+    @db.numrows = 1
+    @db.autoid = 1
+
+    @db[:table].filter(:a=>1).all.should == [{:a=>1}]
+    @db.sqls.should == ['SELECT * FROM table WHERE (a = $1::int4) -- args: [1]']
+
+    @db[:table].filter(:a=>1).update(:b=>'a').should == 1
+    @db.sqls.should == ['UPDATE table SET b = $1::text WHERE (a = $2::int4) -- args: ["a", 1]']
+
+    @db[:table].filter(:a=>1).delete.should == 1
+    @db.sqls.should == ['DELETE FROM table WHERE (a = $1::int4) -- args: [1]']
+
+    @db[:table].insert(:a=>1).should == 1
+    @db.sqls.should == ['INSERT INTO table (a) VALUES ($1::int4) RETURNING id -- args: [1]']
+
+    @db.server_version = 80000
+    @db[:table].insert(:a=>1).should == 1
+    @db.sqls.should == ['INSERT INTO table (a) VALUES ($1::int4) -- args: [1]']
+  end
+
+  it "should not automatically paramiterize if no_auto_parameterize is used" do
+    @db[:table].no_auto_parameterize.filter(:a=>1).sql.should == 'SELECT * FROM table WHERE (a = 1)'
+  end
+
+  it "should not automatically parameterize prepared statements" do
+    @db[:table].filter(:a=>1, :b=>:$b).prepare(:select).sql.should == 'SELECT * FROM table WHERE ((a = 1) AND (b = $b))'
+  end
+
+  it "should show args with string when inspecting SQL " do
+    @db[:table].filter(:a=>1).sql.inspect.should == '"SELECT * FROM table WHERE (a = $1::int4); [1]"'
+  end
+end
