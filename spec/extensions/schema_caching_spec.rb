828fa1af9bb0ebe67a9fa9877cab58bc52abb431
Jeremy Evans <code@jeremyevans.net>
2012-03-14 16:12:54 -0700

    Add schema_caching extension, to speed up loading a large number of models by loading cached schema information from a file
    This idea is taken from ActiveRecord, which will be adding the
    feature in 4.0.  Basically, when loading a large number of
    models, there may be significant time spent loading the table
    schema for each table from the database, especially in high
    latency situations.
    
    To work around this, you can cheat by caching the information to
    a file, and then loading the cached file instead of querying the
    database for each table/model, which is what this extension does.
    
    To make it easy to generate the cached schema files, an -S option
    is being added to bin/sequel that creates the cached schema dump
    file.  It's up to the application to use load_schema_cache to
    preload the schema cache.

---
+++ spec/extensions/schema_caching_spec.rb
@@ -0,0 +1,41 @@
+require File.join(File.dirname(File.expand_path(__FILE__)), "spec_helper")
+
+describe "schema_caching extension" do
+  before do
+    @db = Sequel.mock
+    @schemas = {'"table"'=>[[:column, {:db_type=>"integer", :default=>"nextval('table_id_seq'::regclass)", :allow_null=>false, :primary_key=>true, :type=>:integer, :ruby_default=>nil}]]}
+    @filename = "spec/files/test_schema_#$$.dump" 
+    @db.instance_variable_set(:@schemas, @schemas)
+  end
+  after do
+    File.delete(@filename) if File.exist?(@filename)
+  end
+
+  it "Database#dump_schema_cache should dump cached schema to the given file" do
+    File.exist?(@filename).should be_false
+    @db.dump_schema_cache(@filename)
+    File.exist?(@filename).should be_true
+    File.size(@filename).should > 0
+  end
+
+  it "Database#load_schema_cache should load cached schema from the given file dumped by #dump_schema_cache" do
+    @db.dump_schema_cache(@filename)
+    db = Sequel::Database.new
+    db.load_schema_cache(@filename)
+    @db.instance_variable_get(:@schemas).should == @schemas
+  end
+
+  it "Database#dump_schema_cache? should dump cached schema to the given file unless the file exists" do
+    File.open(@filename, 'wb'){|f|}
+    File.size(@filename).should == 0
+    @db.dump_schema_cache?(@filename)
+    File.size(@filename).should == 0
+  end
+
+  it "Database#load_schema_cache? should load cached schema from the given file if it exists" do
+    db = Sequel::Database.new
+    File.exist?(@filename).should be_false
+    db.load_schema_cache?(@filename)
+    db.instance_variable_get(:@schemas).should == {}
+  end
+end
