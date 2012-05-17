db98c24e625b3855f8264424119214beb297c6e0
Jeremy Evans <code@jeremyevans.net>
2012-05-17 12:56:39 -0700

    Fix some RDoc related to default_schema

--- lib/sequel/database/dataset_defaults.rb
+++ lib/sequel/database/dataset_defaults.rb
@@ -50,7 +50,9 @@ module Sequel
     # an optional options hash.
     attr_reader :dataset_class
 
-    # The default schema to use, generally should be nil.
+    # The default schema to use, generally should be nil.  This sets
+    # the default schema used for some schema modification and
+    # introspection queries, but does not effect most dataset code.
     attr_accessor :default_schema
 
     # If the database has any dataset modules associated with it,
--- lib/sequel/database/misc.rb
+++ lib/sequel/database/misc.rb
@@ -26,7 +26,7 @@ module Sequel
     # options hash.
     #
     # Accepts the following options:
-    # :default_schema :: The default schema to use, should generally be nil
+    # :default_schema :: The default schema to use, see #default_schema.
     # :disconnection_proc :: A proc used to disconnect the connection
     # :identifier_input_method :: A string method symbol to call on identifiers going into the database
     # :identifier_output_method :: A string method symbol to call on identifiers coming from the database
