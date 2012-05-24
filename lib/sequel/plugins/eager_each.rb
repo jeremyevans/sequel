bc0a34e9cf4b117bd35ac6cf5b5d8cd77e8837d1
Jeremy Evans <code@jeremyevans.net>
2012-05-24 12:38:18 -0700

    Add eager_each plugin, which makes each on an eagerly loaded dataset do eager loading
    This behavior has been requested by multiple people over the years.
    I've always been against changing having all call a method other
    than each for eager loading.  I just recently thought of this
    approach, which doesn't require any changes to the core of Sequel.

---
+++ lib/sequel/plugins/eager_each.rb
@@ -0,0 +1,59 @@
+module Sequel
+  module Plugins
+    # The eager_each plugin makes calling each on an eager loaded dataset do eager loading.
+    # By default, each does not work on an eager loaded dataset, because each iterates
+    # over rows of the dataset as they come in, and to eagerly load you need to have all
+    # values up front.  With the default associations code, you must call #all on an eagerly
+    # loaded dataset, as calling #each on an #eager dataset skips the eager loading, and calling
+    # #each on an #eager_graph dataset makes it yield plain hashes with columns from all
+    # tables, instead of yielding the instances of the main model.
+    #
+    # This plugin makes #each call #all for eagerly loaded datasets.  As #all usually calls
+    # #each, this is a bit of issue, but this plugin resolves the issue by cloning the dataset
+    # and setting a new flag in the cloned dataset, so that each can check with the flag to
+    # determine whether it should call all.
+    # 
+    # Usage:
+    #
+    #   # Make all model subclass instances eagerly load for each (called before loading subclasses)
+    #   Sequel::Model.plugin :eager_each
+    #
+    #   # Make the Album class eagerly load for each
+    #   Album.plugin :eager_each
+    module EagerEach 
+      # Methods added to eagerly loaded datasets when the eager_each plugin is in use.
+      module EagerDatasetMethods
+        # Call #all instead of #each unless #each is being called by #all.
+        def each(&block)
+          if opts[:all_called]
+            super
+          else
+            all(&block)
+          end
+        end
+
+        # Clone the dataset and set a flag to let #each know not to call #all,
+        # to avoid the infinite loop.
+        def all(&block)
+          if opts[:all_called]
+            super
+          else
+            clone(:all_called=>true).all(&block)
+          end
+        end
+      end
+
+      module DatasetMethods
+        # Make sure calling each on this dataset will eagerly load the dataset.
+        def eager(*)
+          super.extend(EagerDatasetMethods)
+        end
+        
+        # Make sure calling each on this dataset will eagerly load the dataset.
+        def eager_graph(*)
+          super.extend(EagerDatasetMethods)
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/eager_each_spec.rb
@@ -0,0 +1,34 @@
+require File.join(File.dirname(File.expand_path(__FILE__)), "spec_helper")
+
+describe "Sequel::Plugins::EagerEach" do
+  before do
+    @c = Class.new(Sequel::Model(:items))
+    @c.columns :id, :parent_id
+    @c.plugin :eager_each
+    @c.one_to_many :children, :class=>@c, :key=>:parent_id
+    @c.db.sqls
+  end
+
+  it "should make #each on an eager dataset do eager loading" do
+    a = []
+    ds = @c.eager(:children)
+    ds._fetch = [{:id=>1, :parent_id=>nil}, {:id=>2, :parent_id=>nil}]
+    @c.dataset._fetch = [{:id=>3, :parent_id=>1}, {:id=>4, :parent_id=>1}, {:id=>5, :parent_id=>2}, {:id=>6, :parent_id=>2}]
+    ds.each{|c| a << c}
+    a.should == [@c.load(:id=>1, :parent_id=>nil), @c.load(:id=>2, :parent_id=>nil)]
+    a.map{|c| c.associations[:children]}.should == [[@c.load(:id=>3, :parent_id=>1), @c.load(:id=>4, :parent_id=>1)], [@c.load(:id=>5, :parent_id=>2), @c.load(:id=>6, :parent_id=>2)]]
+    @c.db.sqls.should == ['SELECT * FROM items', 'SELECT * FROM items WHERE (items.parent_id IN (1, 2))']
+  end
+
+  it "should make #each on an eager_graph dataset do eager loading" do
+    a = []
+    ds = @c.eager_graph(:children)
+    ds._fetch = []
+    ds._fetch = [{:id=>1, :parent_id=>nil, :children_id=>3, :children_parent_id=>1}, {:id=>1, :parent_id=>nil, :children_id=>4, :children_parent_id=>1}, {:id=>2, :parent_id=>nil, :children_id=>5, :children_parent_id=>2}, {:id=>2, :parent_id=>nil, :children_id=>6, :children_parent_id=>2}]
+    ds.each{|c| a << c}
+    a.should == [@c.load(:id=>1, :parent_id=>nil), @c.load(:id=>2, :parent_id=>nil)]
+    a.map{|c| c.associations[:children]}.should == [[@c.load(:id=>3, :parent_id=>1), @c.load(:id=>4, :parent_id=>1)], [@c.load(:id=>5, :parent_id=>2), @c.load(:id=>6, :parent_id=>2)]]
+    @c.db.sqls.should == ['SELECT items.id, items.parent_id, children.id AS children_id, children.parent_id AS children_parent_id FROM items LEFT OUTER JOIN items AS children ON (children.parent_id = items.id)']
+  end
+
+end
