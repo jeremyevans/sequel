1245f5e2acdc13f5eefa26774c3d7b908be4a07c
Hannes Georg <hannes.georg@xing.com>
2012-08-16 12:16:36 +0200

    Added a new method #meta_remove which does the opposite of #mete_def

--- lib/sequel/metaprogramming.rb
+++ lib/sequel/metaprogramming.rb
@@ -9,5 +9,22 @@ module Sequel
     def meta_def(name, &block)
       (class << self; self end).send(:define_method, name, &block)
     end
+
+    # Remove a meta method with the given name.
+    # This is useful in conjunction with meta_def to temporarily change the behavior of an object.
+    #
+    #   object = Object.new
+    #   object.extend(Sequel::Metaprogramming)
+    #   object.to_s #=> "#<Object:0x000000022b6dd8>"
+    #   
+    #   object.meta_def(:to_s){ "my fancy to_s" }
+    #   object.to_s #=> "my fancy to_s"
+    #   object.meta_remove(:to_s)
+    #   
+    #   object.to_s #=> "#<Object:0x000000022b6dd8>"
+    #
+    def meta_remove(name)
+      (class << self; self end).send(:remove_method, name)
+    end
   end
 end
