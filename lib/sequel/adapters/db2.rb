e2e8df2d101b553170b621974811dc1a61b50ced
Jeremy Evans <code@jeremyevans.net>
2012-10-25 08:19:56 -0700

    Allow disconnecting the same connection multiple times in the db2 adapter

--- lib/sequel/adapters/db2.rb
+++ lib/sequel/adapters/db2.rb
@@ -58,8 +58,8 @@ module Sequel
       end
       
       def disconnect_connection(conn)
-        checked_error("Could not disconnect from database"){DB2CLI.SQLDisconnect(conn)}
-        checked_error("Could not free Database handle"){DB2CLI.SQLFreeHandle(DB2CLI::SQL_HANDLE_DBC, conn)}
+        DB2CLI.SQLDisconnect(conn)
+        DB2CLI.SQLFreeHandle(DB2CLI::SQL_HANDLE_DBC, conn)
       end
 
       def execute(sql, opts={}, &block)
