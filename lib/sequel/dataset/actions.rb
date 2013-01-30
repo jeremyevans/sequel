69002c28c8f76e5a71aa0d352cf9c69fa1f88bc9
Jeremy Evans <code@jeremyevans.net>
2013-01-30 10:58:13 -0800

    Mark paged_each as a dataset action method
    This allows Model.paged_each to work.

--- lib/sequel/dataset/actions.rb
+++ lib/sequel/dataset/actions.rb
@@ -11,7 +11,7 @@ module Sequel
     ACTION_METHODS = (<<-METHS).split.map{|x| x.to_sym}
       << [] []= all avg count columns columns! delete each
       empty? fetch_rows first get import insert insert_multiple interval last
-      map max min multi_insert range select_hash select_hash_groups select_map select_order_map
+      map max min multi_insert paged_each range select_hash select_hash_groups select_map select_order_map
       set single_record single_value sum to_csv to_hash to_hash_groups truncate update
     METHS
 
