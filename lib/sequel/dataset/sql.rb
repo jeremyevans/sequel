83a776956888875c9a25018ae772354e61e78901
Jeremy Evans <code@jeremyevans.net>
2013-05-24 09:48:38 -0700

    Deprecated using a mismatched number of placeholders and arguments in a placeholder literal string
    A mismatched number of arguments is usually indicative of some
    sort of logic error, and it's probably something Sequel should
    complain about.
    
    Issue pointed out by Jim Hranicky.

--- lib/sequel/dataset/sql.rb
+++ lib/sequel/dataset/sql.rb
@@ -621,13 +621,21 @@ module Sequel
           sql << s
           literal_append(sql, args[i]) unless i == len
         end
+        unless str.length == args.length || str.length == args.length + 1
+          Sequel::Deprecation.deprecate("Using a mismatched number of placeholders (#{str.length}) and placeholder arguments (#{args.length}) is deprecated and will raise an Error in Sequel 4.")
+        end
       else
         i = -1
         loop do
           previous, q, str = str.partition(QUESTION_MARK)
           sql << previous
-           literal_append(sql, args.at(i+=1)) unless q.empty?
-          break if str.empty?
+          literal_append(sql, args.at(i+=1)) unless q.empty?
+          if str.empty?
+            unless i + 1 == args.length
+              Sequel::Deprecation.deprecate("Using a mismatched number of placeholders (#{i+1}) and placeholder arguments (#{args.length}) is deprecated and will raise an Error in Sequel 4.")
+            end
+            break
+          end
         end
       end
       sql << PAREN_CLOSE if pls.parens
