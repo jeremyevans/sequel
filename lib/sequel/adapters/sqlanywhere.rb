4cf897cad5e74fca586d4181f0eb059be901e5ec
Greg Ditrick <gditrick@fuse.net>
2013-10-08 14:47:10 -0400

    Initial commit to add Sybase SqlAnywhere Adapter.
    Notes:
    
    Sequel will default the SqlAnywhere adapter to default smallint datatypes to boolean
    since SqlAnywhere does not have a boolean native type.  This can be overridden either
    at the database level or at the dataset level with the method convert_smallint_to_bool=:
    
    Database override:
    Sequel::SqlAnywhere.convert_smallint_to_bool = false
    or
    db.convert_smallint_to_bool = false
    
    Dataset ovverride:
    ds.convert_smallint_to_bool = false
    
    * Added Adapter specific test for this.
    
    Integration Tests Notes:
    SqlAnywhere does not implicitly set uniqueness or not null when adding foreign keys or
    allow you to drop columns that are foreign keys or part of a constraint without dropping
    the constraint first.  So, the following schema_test tests were changed:
    
    spec/integration/schema_test.rb:208
    It "should parse foreign key information into an array of hashes"
    -    @db.create_table!(:a, :engine=>:InnoDB){primary_key :c; Integer :d; index :d, :unique=>true}
    +    @db.create_table!(:a, :engine=>:InnoDB){primary_key :c; Integer :d, :null => false, :unique => true ; index :d, :unique=>true}
    needed to make sure foreign key or uniqueness constraint is not violated.
    
    +    @db.alter_table(:a){add_unique_constraint [:d, :c]}
    needed to make sure a uniqueness contrainst is added before a composite foreign key is added to
    make sure a foreign key or uniqueness constraint exists and not violated.
    
    spec/integration/schema_test.rb:548
    It "should add unnamed unique constraints and foreign key table constraints correctly"
    -    @db.create_table!(:items, :engine=>:InnoDB){Integer :id; Integer :item_id}
    +    @db.create_table!(:items, :engine=>:InnoDB){Integer :id, :null => false; Integer :item_id, :null => false}
    needed to make sure foreign key or uniqueness constraint is not violated.
    
    spec/integration/schema_test.rb:611
    It "should remove foreign key columns from table correctly"
    -    @db.drop_column(:items, :item_id)
    +    @db.alter_table(:items){drop_foreign_key :item_id}
    Since drop column will only drop a column and drop foreign_key will drop both the fk and column. SqlAnywhere
    will not allow a column to be drop if it is part of a fk or constraint unless the fk or constraint
    is dropped first.

---
+++ lib/sequel/adapters/sqlanywhere.rb
@@ -0,0 +1,188 @@
+require 'uri'
+require 'sqlanywhere'
+
+Sequel.require %w'shared/sqlanywhere', 'adapters'
+
+module Sequel
+  # Module for holding all SqlAnywhere-related classes and modules for Sequel.
+  module SqlAnywhere
+
+    class SQLAnywhereException < StandardError
+      attr_reader :errno
+      attr_reader :sql
+
+      def initialize(message, errno, sql)
+        super(message)
+        @errno = errno
+        @sql = sql
+      end
+    end
+
+    TYPE_TRANSLATOR = tt = Class.new do
+      def blob(s) ::Sequel::SQL::Blob.new(s) unless s.nil? end
+      def boolean(s) s.to_i != 0 unless s.nil? end
+      def date(s) ::Date.strptime(s) unless s.nil? end
+      def decimal(s) ::BigDecimal.new(s) unless s.nil? end
+      def time(s) ::Sequel.string_to_time(s) unless s.nil? end
+    end.new
+
+    SQLANYWHERE_TYPES = {}
+    {
+        [0, 484] => tt.method(:decimal),
+        [384] => tt.method(:date),
+        [388] =>  tt.method(:time),
+        [500] => tt.method(:boolean),
+        [524, 528] => tt.method(:blob)
+    }.each do |k,v|
+      k.each{|n| SQLANYWHERE_TYPES[n] = v}
+    end
+
+    # Database class for SQLAnywhere databases used with Sequel.
+    class Database < Sequel::Database
+      include Sequel::SqlAnywhere::DatabaseMethods
+
+      DEFAULT_CONFIG = { :user => 'dba', :password => 'sql' }
+
+      attr_accessor :api
+
+      set_adapter_scheme :sqlanywhere
+
+      def connect(server)
+        if opts[:uri].nil?
+          connection_string = "ServerName=#{(opts[:server] || opts[:database])};DatabaseName=#{opts[:database]};UserID=#{opts[:user]};Password=#{opts[:password]};"
+          connection_string += "CommLinks=#{opts[:commlinks]};" unless opts[:commlinks].nil?
+          connection_string += "ConnectionName=#{opts[:connection_name]};" unless opts[:connection_name].nil?
+          connection_string += "CharSet=#{opts[:encoding]};" unless opts[:encoding].nil?
+          connection_string += "Idle=0" # Prevent the server from disconnecting us if we're idle for >240mins (by default)
+        else
+          uri = URI(opts[:uri])
+          connection_string = (uri.path.nil? or uri.path == "") ? "" : "DBN=#{File.basename(uri.path)};"
+          connection_string += uri.query
+        end
+
+        conn = @api.sqlany_new_connection
+
+        ret = @api.sqlany_connect(conn, connection_string)
+        if ret != 1
+          raise LoadError, "Could not connect" if conn.nil?
+        end
+
+        if Sequel.application_timezone == :utc
+          @api.sqlany_execute_immediate(conn, "SET TEMPORARY OPTION time_zone_adjustment=0")
+        end
+      end
+
+      # Closes given database connection.
+      def disconnect_connection(c)
+        @api.sqlany_disconnect(c)
+      end
+
+      # Returns number of rows affected
+      def execute_dui(sql, opts={})
+        synchronize do |conn|
+          begin
+            rs = log_yield(sql){ @api.sqlany_execute_direct(conn, sql)}
+            if rs.nil?
+              result, errstr = @api.sqlany_error(conn)
+              raise_error(SQLAnywhereException.new(errstr, result, sql))
+            end
+            affected_rows = @api.sqlany_affected_rows(rs)
+            @api.sqlany_commit(conn) unless in_transaction?
+            affected_rows
+          ensure
+            @api.sqlany_free_stmt(rs) unless rs.nil?
+          end
+        end
+      end
+
+      def execute(sql, opts={})
+        synchronize do |conn|
+          begin
+            rs = log_yield(sql){ @api.sqlany_execute_direct(conn, sql)}
+            if rs.nil?
+              result, errstr = @api.sqlany_error(conn)
+              raise_error(SQLAnywhereException.new(errstr, result, sql))
+            end
+            yield rs if block_given?
+            @api.sqlany_commit(conn) unless in_transaction?
+          ensure
+            @api.sqlany_free_stmt(rs) unless rs.nil?
+          end
+        end
+      end
+
+      def execute_insert(sql, opts={})
+        execute(sql, opts)
+        last_insert_id(opts)
+      end
+
+      private
+
+      def adapter_initialize
+        @conversion_procs = SQLANYWHERE_TYPES.dup
+        @conversion_procs[392] = method(:to_application_timestamp_sa)
+        @api = SQLAnywhere::SQLAnywhereInterface.new
+        raise LoadError, "Could not load SQLAnywhere DBCAPI library" if SQLAnywhere::API.sqlany_initialize_interface(@api) == 0
+        raise LoadError, "Could not initialize SQLAnywhere DBCAPI library" if @api.sqlany_init == 0
+      end
+
+      def log_connection_execute(conn, sql)
+        log_yield(sql){ execute(sql)}
+      end
+
+      def last_insert_id(opts={})
+        sql = 'SELECT @@IDENTITY'
+        id = nil
+        execute(sql) do |rs|
+          if @api.sqlany_fetch_next(rs) == 1
+            id = @api.sqlany_get_column(rs, 0)[1]
+          end unless rs.nil?
+        end
+        id
+      end
+
+    end
+
+    # Dataset class for SqlAnywhere datasets accessed via the native driver.
+    class Dataset < Sequel::Dataset
+      include Sequel::SqlAnywhere::DatasetMethods
+
+      Database::DatasetClass = self
+
+      # Yield all rows matching this dataset.  If the dataset is set to
+      # split multiple statements, yield arrays of hashes one per statement
+      # instead of yielding results for all statements as hashes.
+      def fetch_rows(sql)
+        db = @db
+        cps = db.conversion_procs
+        execute(sql) do |rs|
+          max_cols = db.api.sqlany_num_cols(rs)
+          col_map = {}
+          max_cols.times do |cols|
+            col_map[db.api.sqlany_get_column_info(rs, cols)[2]] =
+                output_identifier(db.api.sqlany_get_column_info(rs, cols)[2])
+          end
+
+          @columns  = col_map.values
+          convert = (convert_smallint_to_bool and db.convert_smallint_to_bool)
+
+          while db.api.sqlany_fetch_next(rs) == 1
+            max_cols = db.api.sqlany_num_cols(rs)
+            h2 = {}
+            max_cols.times do |cols|
+              h2[col_map[db.api.sqlany_get_column_info(rs, cols)[2]]||db.api.sqlany_get_column_info(rs, cols)[2]] =
+                cps[db.api.sqlany_get_column_info(rs, cols)[4]].nil? ?
+                    db.api.sqlany_get_column(rs, cols)[1] :
+                      db.api.sqlany_get_column_info(rs, cols)[4] != 500 ?
+                        cps[db.api.sqlany_get_column_info(rs, cols)[4]].call(db.api.sqlany_get_column(rs, cols)[1]) :
+                          convert ? cps[db.api.sqlany_get_column_info(rs, cols)[4]].call(db.api.sqlany_get_column(rs, cols)[1]) :
+                            db.api.sqlany_get_column(rs, cols)[1]
+            end
+            yield h2
+          end unless rs.nil?
+        end
+        self
+      end
+    end
+  end
+end
---
+++ spec/adapters/sqlanywhere_spec.rb
@@ -0,0 +1,148 @@
+SEQUEL_ADAPTER_TEST = :db2
+
+require File.join(File.dirname(File.expand_path(__FILE__)), 'spec_helper.rb')
+
+if DB.table_exists?(:test)
+  DB.drop_table(:test)
+end
+
+describe Sequel::Database do
+  before do
+    @db = DB
+    @db.create_table(:test){String :a}
+    @ds = @db[:test]
+  end
+
+  after do
+    @db.drop_table(:test)
+  end
+  
+  specify "should provide disconnect functionality after preparing a connection" do
+    @ds.prepare(:first, :a).call
+    @db.disconnect
+    @db.pool.size.should == 0
+  end
+
+  specify "should return version correctly" do
+    @db.db2_version.should match(/DB2 v/i)
+  end
+end
+
+describe "Simple Dataset operations" do
+  before(:all) do
+    Sequel::DB2.use_clob_as_blob = false
+    DB.create_table!(:items) do
+      Integer :id, :primary_key => true
+      Integer :number
+      column  :bin_string, 'varchar(20) for bit data'
+      column  :bin_blob, 'blob'
+    end
+    @ds = DB[:items]
+  end
+  after(:each) do
+    @ds.delete
+  end
+  after(:all) do
+    Sequel::DB2.use_clob_as_blob = true
+    DB.drop_table(:items)
+  end
+
+  specify "should insert with a primary key specified" do
+    @ds.insert(:id => 1,   :number => 10)
+    @ds.insert(:id => 100, :number => 20)
+    @ds.select_hash(:id, :number).should == {1 => 10, 100 => 20}
+  end
+
+  specify "should insert into binary columns" do
+    @ds.insert(:id => 1, :bin_string => Sequel.blob("\1"), :bin_blob => Sequel.blob("\2"))
+    @ds.select(:bin_string, :bin_blob).first.should == {:bin_string => "\1", :bin_blob => "\2"}
+  end
+end
+
+describe Sequel::Database do
+  before do
+    @db = DB
+  end
+  after do
+    @db.drop_table(:items)
+  end
+  specify "should parse primary keys from the schema properly" do
+    @db.create_table!(:items){Integer :number}
+    @db.schema(:items).collect{|k,v| k if v[:primary_key]}.compact.should == []
+    @db.create_table!(:items){primary_key :number}
+    @db.schema(:items).collect{|k,v| k if v[:primary_key]}.compact.should == [:number]
+    @db.create_table!(:items){Integer :number1, :null => false; Integer :number2, :null => false; primary_key [:number1, :number2]}
+    @db.schema(:items).collect{|k,v| k if v[:primary_key]}.compact.should == [:number1, :number2]
+  end
+end
+
+describe "Sequel::IBMDB.convert_smallint_to_bool" do
+  before do
+    @db = DB
+    @db.create_table!(:booltest){column :b, 'smallint'; column :i, 'integer'}
+    @ds = @db[:booltest]
+  end
+  after do
+    Sequel::IBMDB.convert_smallint_to_bool = true
+    @db.drop_table(:booltest)
+  end
+  
+  specify "should consider smallint datatypes as boolean if set, but not larger smallints" do
+    @db.schema(:booltest, :reload=>true).first.last[:type].should == :boolean
+    @db.schema(:booltest, :reload=>true).first.last[:db_type].should match /smallint/i
+    Sequel::IBMDB.convert_smallint_to_bool = false
+    @db.schema(:booltest, :reload=>true).first.last[:type].should == :integer
+    @db.schema(:booltest, :reload=>true).first.last[:db_type].should match /smallint/i
+  end
+  
+  specify "should return smallints as bools and integers as integers when set" do
+    Sequel::IBMDB.convert_smallint_to_bool = true
+    @ds.delete
+    @ds << {:b=>true, :i=>10}
+    @ds.all.should == [{:b=>true, :i=>10}]
+    @ds.delete
+    @ds << {:b=>false, :i=>0}
+    @ds.all.should == [{:b=>false, :i=>0}]
+    @ds.delete
+    @ds << {:b=>true, :i=>1}
+    @ds.all.should == [{:b=>true, :i=>1}]
+  end
+
+  specify "should return all smallints as integers when unset" do
+    Sequel::IBMDB.convert_smallint_to_bool = false
+    @ds.delete
+    @ds << {:b=>true, :i=>10}
+    @ds.all.should == [{:b=>1, :i=>10}]
+    @ds.delete
+    @ds << {:b=>false, :i=>0}
+    @ds.all.should == [{:b=>0, :i=>0}]
+    
+    @ds.delete
+    @ds << {:b=>1, :i=>10}
+    @ds.all.should == [{:b=>1, :i=>10}]
+    @ds.delete
+    @ds << {:b=>0, :i=>0}
+    @ds.all.should == [{:b=>0, :i=>0}]
+  end
+end if DB.adapter_scheme == :ibmdb
+
+describe "Simple Dataset operations in transactions" do
+  before do
+    DB.create_table!(:items_insert_in_transaction) do
+      Integer :id, :primary_key => true
+      integer :number
+    end
+    @ds = DB[:items_insert_in_transaction]
+  end
+  after do
+    DB.drop_table(:items_insert_in_transaction)
+  end
+
+  specify "should insert correctly with a primary key specified inside a transaction" do
+    DB.transaction do
+      @ds.insert(:id=>100, :number=>20)
+      @ds.count.should == 1
+      @ds.order(:id).all.should == [{:id=>100, :number=>20}]
+    end
+  end
+end
