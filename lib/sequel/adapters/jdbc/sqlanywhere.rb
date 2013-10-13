910fdb0cd01e90e5ba1bca04ed6a8f1aad5b0270
Jeremy Evans <code@jeremyevans.net>
2013-10-12 19:17:24 -0700

    Minor refactoring to jdbc/sqlanywhere adapter

--- lib/sequel/adapters/jdbc/sqlanywhere.rb
+++ lib/sequel/adapters/jdbc/sqlanywhere.rb
@@ -6,15 +6,18 @@ module Sequel
     module SqlAnywhere
       # Database instance methods for Sybase databases accessed via JDBC.
       module DatabaseMethods
+        extend Sequel::Database::ResetIdentifierMangling
         include Sequel::SqlAnywhere::DatabaseMethods
         include Sequel::JDBC::Transactions
+
+        LAST_INSERT_ID = 'SELECT @@IDENTITY'.freeze
 
         private
 
         # Get the last inserted id.
-        def last_insert_id(conn, opts={})
+        def last_insert_id(conn, opts=OPTS)
           statement(conn) do |stmt|
-            sql = 'SELECT @@IDENTITY'
+            sql = LAST_INSERT_ID
             rs = log_yield(sql){stmt.executeQuery(sql)}
             rs.next
             rs.getInt(1)
