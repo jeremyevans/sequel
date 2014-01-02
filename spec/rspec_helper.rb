e9f655507c0db32a623064c6444dba45e74e85d9
Jeremy Evans <code@jeremyevans.net>
2014-01-02 10:01:22 -0800

    Fix warning for defining same constant twice when specs

--- spec/rspec_helper.rb
+++ spec/rspec_helper.rb
@@ -1,18 +1,18 @@
-if defined?(RSpec)
-  require 'rspec/version'
-  if RSpec::Version::STRING >= '2.11.0'
-    RSpec.configure do |config|
-      config.expect_with :rspec do |c|
-        c.syntax = :should
-      end
-      config.mock_with :rspec do |c|
-        c.syntax = :should
+unless defined?(RSPEC_EXAMPLE_GROUP)
+  if defined?(RSpec)
+    require 'rspec/version'
+    if RSpec::Version::STRING >= '2.11.0'
+      RSpec.configure do |config|
+        config.expect_with :rspec do |c|
+          c.syntax = :should
+        end
+        config.mock_with :rspec do |c|
+          c.syntax = :should
+        end
       end
     end
+    RSPEC_EXAMPLE_GROUP = RSpec::Core::ExampleGroup
+  else
+    RSPEC_EXAMPLE_GROUP = Spec::Example::ExampleGroup
   end
-  RSPEC_EXAMPLE_GROUP = RSpec::Core::ExampleGroup
-else
-  RSPEC_EXAMPLE_GROUP = Spec::Example::ExampleGroup
 end
-
-
