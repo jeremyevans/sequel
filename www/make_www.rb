e7dd3abf719f1c355d9fc401392f037e651a7c2b
Jeremy Evans <code@jeremyevans.net>
2014-02-05 09:35:46 -0800

    Rename web page templates to have .html.erb extension

--- www/make_www.rb
+++ www/make_www.rb
@@ -4,8 +4,8 @@ $: << File.join(File.dirname(__FILE__), '..','lib', 'sequel')
 require 'version'
 Dir.chdir(File.dirname(__FILE__))
 erb = ERB.new(File.read('layout.html.erb'))
-Dir['pages/*'].each do |page|
-  public_loc = "#{page.gsub(/\Apages\//, 'public/')}.html"
+Dir['pages/*.html.erb'].each do |page|
+  public_loc = "#{page.gsub(/\Apages\//, 'public/').gsub('.erb', '')}"
   content = ERB.new(File.read(page)).result(binding)
   title = File.basename(page)
   File.open(public_loc, 'wb'){|f| f.write(erb.result(binding))}
