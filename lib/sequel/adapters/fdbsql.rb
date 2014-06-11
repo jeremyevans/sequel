86eb2a15e06c2c3e7dfdde42dc39a7d615515c8e
Scott Dugas <scott.dugas@foundationdb.com>
2014-06-11 11:58:48 -0400

    Moved dataset into it's own file

--- lib/sequel/adapters/fdbsql.rb
+++ lib/sequel/adapters/fdbsql.rb
@@ -23,6 +23,7 @@
 
 
 require 'sequel/adapters/fdbsql/connection'
+require 'sequel/adapters/fdbsql/dataset'
 require 'sequel/adapters/utils/pg_types'
 
 module Sequel
@@ -32,6 +33,7 @@ module Sequel
     class ExclusionConstraintViolation < Sequel::ConstraintViolation; end
 
     class Database < Sequel::Database
+      DatasetClass = Dataset
 
       set_adapter_scheme :fdbsql
 
@@ -225,87 +227,5 @@ module Sequel
 
     end
 
-
-    # TODO put in own file if this becomes more than 5 lines
-    class Dataset < Sequel::Dataset
-      Sequel::Fdbsql::Database::DatasetClass = self
-
-      def fetch_rows(sql)
-        execute(sql) do |res|
-          columns = set_columns(res)
-          yield_hash_rows(res, columns) {|h| yield h}
-        end
-      end
-
-      Dataset.def_sql_method(self, :insert, %w'with insert into columns values returning')
-
-      # Returning is always supported.
-      def supports_returning?(type)
-        true
-      end
-
-      # Insert given values into the database.
-      def insert(*values)
-        if @opts[:returning]
-          # Already know which columns to return, let the standard code handle it
-          super
-        elsif @opts[:sql] || @opts[:disable_insert_returning]
-          # Raw SQL used or RETURNING disabled, just use the default behavior
-          # and return nil since sequence is not known.
-          super
-          nil
-        else
-          # Force the use of RETURNING with the primary key value,
-          # unless it has been disabled.
-          returning(insert_pk).insert(*values){|r| return r.values.first}
-        end
-      end
-
-      # Insert a record returning the record inserted.  Always returns nil without
-      # inserting a query if disable_insert_returning is used.
-      def insert_select(*values)
-        returning.insert(*values){|r| return r} unless @opts[:disable_insert_returning]
-      end
-
-      private
-
-
-      # For each row in the result set, yield a hash with column name symbol
-      # keys and typecasted values.
-      def yield_hash_rows(res, cols)
-        res.ntuples.times do |recnum|
-          converted_rec = {}
-          cols.each do |fieldnum, type_proc, fieldsym|
-            value = res.getvalue(recnum, fieldnum)
-            converted_rec[fieldsym] = (value && type_proc) ? type_proc.call(value) : value
-          end
-          yield converted_rec
-        end
-      end
-
-
-      def set_columns(res)
-        cols = []
-        procs = Sequel::Postgres::PG_TYPES
-        res.nfields.times do |fieldnum|
-          cols << [fieldnum, procs[res.ftype(fieldnum)], output_identifier(res.fname(fieldnum))]
-        end
-        @columns = cols.map{|c| c[2]}
-        cols
-      end
-
-      # Return the primary key to use for RETURNING in an INSERT statement
-      def insert_pk
-        if (f = opts[:from]) && !f.empty?
-          case t = f.first
-          when Symbol, String, SQL::Identifier, SQL::QualifiedIdentifier
-            if pk = db.primary_key(t)
-              Sequel::SQL::Identifier.new(pk)
-            end
-          end
-        end
-      end
-
-    end
   end
 end
---
+++ lib/sequel/adapters/fdbsql/dataset.rb
@@ -0,0 +1,107 @@
+#
+# FoundationDB SQL Layer Sequel Adapter
+# Copyright (c) 2013-2014 FoundationDB, LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+module Sequel
+  module Fdbsql
+
+    class Dataset < Sequel::Dataset
+
+      def fetch_rows(sql)
+        execute(sql) do |res|
+          columns = set_columns(res)
+          yield_hash_rows(res, columns) {|h| yield h}
+        end
+      end
+
+      Dataset.def_sql_method(self, :insert, %w'with insert into columns values returning')
+
+      # Returning is always supported.
+      def supports_returning?(type)
+        true
+      end
+
+      # Insert given values into the database.
+      def insert(*values)
+        if @opts[:returning]
+          # Already know which columns to return, let the standard code handle it
+          super
+        elsif @opts[:sql] || @opts[:disable_insert_returning]
+          # Raw SQL used or RETURNING disabled, just use the default behavior
+          # and return nil since sequence is not known.
+          super
+          nil
+        else
+          # Force the use of RETURNING with the primary key value,
+          # unless it has been disabled.
+          returning(insert_pk).insert(*values){|r| return r.values.first}
+        end
+      end
+
+      # Insert a record returning the record inserted.  Always returns nil without
+      # inserting a query if disable_insert_returning is used.
+      def insert_select(*values)
+        returning.insert(*values){|r| return r} unless @opts[:disable_insert_returning]
+      end
+
+      private
+
+
+      # For each row in the result set, yield a hash with column name symbol
+      # keys and typecasted values.
+      def yield_hash_rows(res, cols)
+        res.ntuples.times do |recnum|
+          converted_rec = {}
+          cols.each do |fieldnum, type_proc, fieldsym|
+            value = res.getvalue(recnum, fieldnum)
+            converted_rec[fieldsym] = (value && type_proc) ? type_proc.call(value) : value
+          end
+          yield converted_rec
+        end
+      end
+
+
+      def set_columns(res)
+        cols = []
+        procs = Sequel::Postgres::PG_TYPES
+        res.nfields.times do |fieldnum|
+          cols << [fieldnum, procs[res.ftype(fieldnum)], output_identifier(res.fname(fieldnum))]
+        end
+        @columns = cols.map{|c| c[2]}
+        cols
+      end
+
+      # Return the primary key to use for RETURNING in an INSERT statement
+      def insert_pk
+        if (f = opts[:from]) && !f.empty?
+          case t = f.first
+          when Symbol, String, SQL::Identifier, SQL::QualifiedIdentifier
+            if pk = db.primary_key(t)
+              Sequel::SQL::Identifier.new(pk)
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
