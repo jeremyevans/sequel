63c5ee983a63351f58069cce6546851c355e715b
Chris Hanks <christopher.m.hanks@gmail.com>
2014-06-26 10:26:48 -0700

    Documentation fix
    The method is add_hook_type, not define_hook.

--- lib/sequel/plugins/hook_class_methods.rb
+++ lib/sequel/plugins/hook_class_methods.rb
@@ -62,7 +62,7 @@ module Sequel
         # Example of usage:
         #
         #  class MyModel
-        #   define_hook :before_move_to
+        #   add_hook_type :before_move_to
         #   before_move_to(:check_move_allowed){|o| o.allow_move?}
         #   def move_to(there)
         #     return if before_move_to == false
