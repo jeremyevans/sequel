dafb439ec4c49bc5c4503750fe522b0a93fb0f4e
Jeremy Evans <code@jeremyevans.net>
2014-07-11 10:41:43 -0700

    Use all_tables and all_views for Database#tables and #views on Oracle
    This works better for users with limited rights.  Similar to other
    databases, we try to hide the system tables.  In the case of Oracle,
    that's not all that simple to do, as there are a lot of system users,
    so we may want to expand the IGNORE_OWNERS constant in the future.

--- lib/sequel/adapters/shared/oracle.rb
+++ lib/sequel/adapters/shared/oracle.rb
@@ -59,19 +59,33 @@ module Sequel
         false
       end
 
+      IGNORE_OWNERS = %w'APEX_040000 CTXSYS EXFSYS MDSYS OLAPSYS ORDDATA ORDSYS SYS SYSTEM XDB XDBMETADATA XDBPM XFILES WMSYS'
+
       def tables(opts=OPTS)
         m = output_identifier_meth
-        metadata_dataset.from(:tabs).server(opts[:server]).select(:table_name).map{|r| m.call(r[:table_name])}
+        metadata_dataset.from(:all_tables).
+          server(opts[:server]).
+          where(:dropped=>'NO').
+          exclude(:owner=>IGNORE_OWNERS).
+          select(:table_name).
+          map{|r| m.call(r[:table_name])}
       end
 
       def views(opts=OPTS) 
         m = output_identifier_meth
-        metadata_dataset.from(:tab).server(opts[:server]).select(:tname).filter(:tabtype => 'VIEW').map{|r| m.call(r[:tname])}
+        metadata_dataset.from(:all_views).
+          server(opts[:server]).
+          exclude(:owner=>IGNORE_OWNERS).
+          select(:view_name).
+          map{|r| m.call(r[:view_name])}
       end 
  
       def view_exists?(name) 
         m = input_identifier_meth
-        metadata_dataset.from(:tab).filter(:tname =>m.call(name), :tabtype => 'VIEW').count > 0 
+        metadata_dataset.from(:all_views).
+          exclude(:owner=>IGNORE_OWNERS).
+          where(:view_name=>m.call(name)).
+          count > 0
       end 
 
       # Oracle supports deferrable constraints.
