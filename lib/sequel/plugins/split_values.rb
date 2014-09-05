cf430e34f7259f9bdbb4ce22e86cf700645e6f46
Jeremy Evans <code@jeremyevans.net>
2014-09-05 11:58:21 -0700

    Add split_values plugin, for moving noncolumn entries from the values hash into a separate hash (Fixes #868)
    I don't recommend usage of this plugin by default, since it hurts
    performance, but if you want to be able to add additional selected
    expressions when retrieving model objects, and later save those
    objects or compare them to other objects without the additional
    expressions, this will allow you to do so.

---
+++ lib/sequel/plugins/split_values.rb
@@ -0,0 +1,64 @@
+module Sequel
+  module Plugins
+    # The split_values plugin splits the values hash retreived from the
+    # database, and moves keys from the values hash that are not columns
+    # in the model's dataset to a separate hash.  This makes it so the
+    # values hash only stores columns from the model's dataset.
+    #
+    # Among other things, this allows you to save model objects even if
+    # they were retrieved with additional columns, and have equality
+    # comparisons with other instances not care about non-column values.
+    #
+    # Example:
+    #
+    #   class Album < Sequel::Model
+    #     plugin :split_values
+    #   end
+    #   a1 = Album[1]
+    #   a2 = Album.select_append(Sequel.as(true, :exists))[1]
+    #   a1.name # => 'Album Name'
+    #   a2.name # => 'Album Name'
+    #   a1[:exists] # => nil
+    #   a2[:exists] # => true
+    #   a1 == a2 # => true
+    #   a2.values # => {:id=>1, :name=>'Album Name'}
+    #   a2.save # Works
+    # 
+    # Usage:
+    #
+    #   # Make all model subclass instances split values
+    #   # (called before loading subclasses)
+    #   Sequel::Model.plugin :split_values
+    #
+    #   # Make the Album class split values
+    #   Album.plugin :split_values
+    module SplitValues
+      module ClassMethods
+        # Split the noncolumn values when creating a new object retrieved from
+        # the database.
+        def call(_)
+          super.split_noncolumn_values
+        end
+      end
+
+      module InstanceMethods
+        # If there isn't an entry in the values hash, but there is a noncolumn_values
+        # hash, look in that hash for the value.
+        def [](k)
+          super || (@noncolumn_values[k] if !@values.has_key?(k) && @noncolumn_values)
+        end
+
+        # Check all entries in the values hash.  If any of the keys are not columns,
+        # move the entry into the noncolumn_values hash.
+        def split_noncolumn_values
+          @values.keys.each do |k|
+            unless columns.include?(k)
+              (@noncolumn_values ||= {})[k] = @values.delete(k)
+            end
+          end
+          self
+        end
+      end
+    end
+  end
+end
