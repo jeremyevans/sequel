746065aae7edef5c505bfb881dd140d9920db0ca
Scott Dugas <scott.dugas@foundationdb.com>
2014-09-09 17:47:29 -0400

    Moved connection.rb into fdbsql.rb

--- lib/sequel/adapters/fdbsql.rb
+++ lib/sequel/adapters/fdbsql.rb
@@ -21,8 +21,9 @@
 # THE SOFTWARE.
 #
 
+# FoundationDB SQL Layer currently uses the Postgres protocol
+require 'pg'
 
-require 'sequel/adapters/fdbsql/connection'
 require 'sequel/adapters/fdbsql/dataset'
 require 'sequel/adapters/fdbsql/features'
 require 'sequel/adapters/fdbsql/prepared_statements'
@@ -238,6 +239,153 @@ module Sequel
           yield
         rescue => e
           raise_error(e, :classes=>CONVERTED_EXCEPTIONS)
+        end
+      end
+
+    end
+
+    class Connection
+      CONNECTION_OK = -1
+      DISCONNECT_ERROR_RE = /\A(?:could not receive data from server|no connection to the server|connection not open|connection is closed)/
+
+      # These sql states are used to indicate that fdbvql should automatically
+      # retry the statement if it's not in a transaction
+      RETRY_SQLSTATES = %w'40002'.freeze.each{|s| s.freeze}
+
+      NUMBER_OF_NOT_COMMITTED_RETRIES = 10
+
+      attr_accessor :in_transaction
+
+      attr_accessor :prepared_statements
+
+      def initialize(db, opts)
+        @db = db
+        @config = opts
+        @connection_hash = {
+          :host => @config[:host] || 'localhost',
+          :port => @config[:port] || 15432,
+          :dbname => @config[:database],
+          :user => @config[:user],
+          :password => @config[:password],
+          :hostaddr => @config[:hostaddr],
+          :connect_timeout => @config[:connect_timeout] || 20,
+          :sslmode => @config[:sslmode]
+        }.delete_if { |key, value| value.nil? or (value.respond_to?(:empty?) and value.empty?)}
+        @prepared_statements = {}
+        connect
+      end
+
+      def close
+        # Just like postgres, ignore any errors here
+        begin
+          @connection.close
+        rescue PGError, IOError
+        end
+      end
+
+      def query(sql, args=nil)
+        args = args.map{|v| @db.bound_variable_arg(v, self)} if args
+        check_disconnect_errors do
+          retry_on_not_committed do
+            @connection.query(sql, args)
+          end
+        end
+      end
+
+      # Execute the given SQL with this connection.  If a block is given,
+      # yield the results, otherwise, return the number of changed rows.
+      def execute(sql, args=nil)
+        q = query(sql, args)
+        block_given? ? yield(q) : q.cmd_tuples
+      end
+
+      def prepare(name, sql)
+        check_disconnect_errors do
+          retry_on_not_committed do
+            @connection.prepare(name, sql)
+          end
+        end
+      end
+
+      def execute_prepared_statement(name, args)
+        check_disconnect_errors do
+          retry_on_not_committed do
+            @connection.exec_prepared(name, args)
+          end
+        end
+      end
+
+      private
+
+      def connect
+        @connection = PG::Connection.new(@connection_hash)
+        if (@config[:notice_receiver])
+          @connection.set_notice_receiver(@config[:notice_receiver])
+        else
+          # Swallow warnings
+          @connection.set_notice_receiver { |proc| }
+        end
+        check_version
+      end
+
+      def check_version
+        ver = execute('SELECT VERSION()') do |res|
+          version = res.first['_SQL_COL_1']
+          m = version.match('^.* (\d+)\.(\d+)\.(\d+)')
+          if m.nil?
+            raise "No match when checking FDB SQL Layer version: #{version}"
+          end
+          m
+        end
+
+        # Combine into single number, two digits per part: 1.9.3 => 10903
+        @sql_layer_version = (100 * ver[1].to_i + ver[2].to_i) * 100 + ver[3].to_i
+        if @sql_layer_version < 10906
+          raise Sequel::DatabaseError.new("Unsupported FDB SQL Layer version: #{ver[1]}.#{ver[2]}.#{ver[3]}")
+        end
+      end
+
+      def status
+        CONNECTION_OK
+      end
+
+      def database_exception_sqlstate(exception, opts)
+        if exception.respond_to?(:result) && (result = exception.result)
+          result.error_field(::PGresult::PG_DIAG_SQLSTATE)
+        end
+      end
+
+      def retry_on_not_committed
+        retries = NUMBER_OF_NOT_COMMITTED_RETRIES
+        begin
+          yield
+        rescue PG::TRIntegrityConstraintViolation => e
+          if (!in_transaction and RETRY_SQLSTATES.include? database_exception_sqlstate(e, :classes=>CONVERTED_EXCEPTIONS))
+            retry if (retries -= 1) > 0
+          end
+          raise
+        end
+      end
+
+      # Raise a Sequel::DatabaseDisconnectError if a PGError is raised and
+      # the connection status cannot be determined or it is not OK.
+      def check_disconnect_errors
+        begin
+          yield
+        rescue PGError => e
+          disconnect = false
+          begin
+            s = status
+          rescue PGError
+            disconnect = true
+          end
+          status_ok = (s == CONNECTION_OK)
+          disconnect ||= !status_ok
+          disconnect ||= e.message =~ DISCONNECT_ERROR_RE
+          disconnect ? raise(Sequel.convert_exception_class(e, Sequel::DatabaseDisconnectError)) : raise
+        rescue IOError, Errno::EPIPE, Errno::ECONNRESET => e
+          disconnect = true
+          raise(Sequel.convert_exception_class(e, Sequel::DatabaseDisconnectError))
         end
       end
 
