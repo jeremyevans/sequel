e8d887ff1652c72d958e0f9417009f63df73db9d
Jeremy Evans <code@jeremyevans.net>
2014-12-08 12:04:02 -0800

    Skip autoincrementing primary key spec on fdbsql
    fdbsql uses the information schema views for metadata, and they
    don't contain whether the columns was a serial column or not.
    So keep the default behavior of assuming integer primary keys
    are auto incrementing, but skip the spec.

--- spec/integration/schema_test.rb
+++ spec/integration/schema_test.rb
@@ -88,7 +88,7 @@ describe "Database schema parser" do
     DB.schema(:items).collect{|k,v| k if v[:primary_key]}.compact.should == [:number1, :number2]
   end
 
-  cspecify "should parse autoincrementing primary keys from the schema properly", :sqlite, :oracle do
+  cspecify "should parse autoincrementing primary keys from the schema properly", :sqlite, :oracle, :fdbsql do
     DB.create_table!(:items){Integer :number}
     DB.schema(:items).collect{|k,v| k if v[:primary_key] && v[:auto_increment]}.compact.should == []
     DB.create_table!(:items){primary_key :number}
