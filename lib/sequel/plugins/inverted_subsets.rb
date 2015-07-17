5a1dc4400913cbf81976caaefcf48e91dbeb2c73
Jeremy Evans <code@jeremyevans.net>
2015-07-17 16:29:13 -0700

    Minor refactor for inverted_subsets and update CHANGELOG

--- lib/sequel/plugins/inverted_subsets.rb
+++ lib/sequel/plugins/inverted_subsets.rb
@@ -1,6 +1,5 @@
 module Sequel
   module Plugins
-
     # The inverted_subsets plugin adds another method for each defined
     # subset, which inverts the condition supplied. By default, inverted
     # subset method names are prefixed with not_.
@@ -9,7 +8,7 @@ module Sequel
     # by passing a block to the plugin configuration:
     #
     #   # Use an exclude_ prefix for inverted subsets instead of not_
-    #   Album.plugin(:inverted_subsets){|name| "exclude_#{name}" }
+    #   Album.plugin(:inverted_subsets){|name| "exclude_#{name}"}
     #
     # Usage:
     #
@@ -17,7 +16,7 @@ module Sequel
     #   Album.plugin :inverted_subsets
     #
     #   # This will now create two methods, published and not_published
-    #   Album.subset :published, { published: true }
+    #   Album.subset :published, published: true
     #
     #   Album.published.sql
     #   # SELECT * FROM albums WHERE (published IS TRUE)
@@ -26,31 +25,24 @@ module Sequel
     #   # SELECT * FROM albums WHERE (published IS NOT TRUE)
     #
     module InvertedSubsets
-      module ClassMethods
-
-        Plugins.inherited_instance_variables(self, :@inverted_subsets_name_block => :dup)
-
-        # Define a not_ prefixed subset which inverts the subset condition.
-        def subset(name, *args, &block)
-          super
-
-          inverted_name = @inverted_subsets_name_block.call(name)
-          def_dataset_method(inverted_name){exclude(*args, &block)}
-        end
-      end
+      # Default naming for inverted subsets
+      DEFAULT_NAME_BLOCK = lambda{|name| "not_#{name}"}
 
       # Store the supplied block for calling later when subsets are defined, or
       # create a default one if we need to.
       def self.configure(model, &block)
-        model.instance_eval do
-          unless block_given?
-            block = Proc.new{|name| "not_#{name}" }
-          end
-          @inverted_subsets_name_block = block
-        end
+        model.instance_variable_set(:@inverted_subsets_name_block, block || DEFAULT_NAME_BLOCK)
       end
 
-    end
+      module ClassMethods
+        Plugins.inherited_instance_variables(self, :@inverted_subsets_name_block => nil)
 
+        # Define a not_ prefixed subset which inverts the subset condition.
+        def subset(name, *args, &block)
+          super
+          def_dataset_method(@inverted_subsets_name_block.call(name)){exclude(*args, &block)}
+        end
+      end
+    end
   end
 end
--- spec/extensions/inverted_subsets_spec.rb
+++ spec/extensions/inverted_subsets_spec.rb
@@ -4,14 +4,23 @@ describe "Sequel::Plugins::InvertedSubsets" do
   it "should add an inverted subset method which inverts the condition" do
     c = Class.new(Sequel::Model(:a))
     c.plugin :inverted_subsets
-    c.subset(:published, {published: true})
+    c.subset(:published, published: true)
     c.not_published.sql.must_equal 'SELECT * FROM a WHERE (published IS NOT TRUE)'
   end
 
   it "should support a configuration block to customise the inverted method name" do
     c = Class.new(Sequel::Model(:a))
-    c.plugin(:inverted_subsets){|name| "exclude_#{name}" }
-    c.subset(:published, {published: true})
+    c.plugin(:inverted_subsets){|name| "exclude_#{name}"}
+    c.subset(:published, published: true)
+    c.exclude_published.sql.must_equal 'SELECT * FROM a WHERE (published IS NOT TRUE)'
+  end
+
+  it "should work in subclasses" do
+    c = Class.new(Sequel::Model)
+    c.plugin(:inverted_subsets){|name| "exclude_#{name}"}
+    c = Class.new(c)
+    c.dataset = :a
+    c.subset(:published, published: true)
     c.exclude_published.sql.must_equal 'SELECT * FROM a WHERE (published IS NOT TRUE)'
   end
 end
