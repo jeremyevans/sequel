2e97bfe6f8fd5bfa697edd1675ccc9dbffeb55d1
knut2 <knut@lickert.net>
2015-07-24 08:11:56 -0700

    Support case sensitive SQL-Server 2012 in MSSQL schema methods

--- lib/sequel/adapters/shared/mssql.rb
+++ lib/sequel/adapters/shared/mssql.rb
@@ -131,10 +131,10 @@ module Sequel
         schema, table = schema_and_table(table)
         current_schema = m.call(get(Sequel.function('schema_name')))
         fk_action_map = FOREIGN_KEY_ACTION_MAP
-        ds = metadata_dataset.from(:sys__foreign_keys___fk).
-          join(:sys__foreign_key_columns___fkc, :constraint_object_id => :object_id).
-          join(:sys__all_columns___pc, :object_id => :fkc__parent_object_id,     :column_id => :fkc__parent_column_id).
-          join(:sys__all_columns___rc, :object_id => :fkc__referenced_object_id, :column_id => :fkc__referenced_column_id).
+        ds = metadata_dataset.from(Sequel.as(Sequel.lit('[sys].[foreign_keys]'), 'fk')).
+          join(Sequel.as(Sequel.lit('[sys].[foreign_key_columns]'), 'fkc'), :constraint_object_id => :object_id).
+          join(Sequel.as(Sequel.lit('[sys].[all_columns]'),'pc'), :object_id => :fkc__parent_object_id,     :column_id => :fkc__parent_column_id).
+          join(Sequel.as(Sequel.lit('[sys].[all_columns]'),'rc'), :object_id => :fkc__referenced_object_id, :column_id => :fkc__referenced_column_id).
           where{{object_schema_name(:fk__parent_object_id) => im.call(schema || current_schema)}}.
           where{{object_name(:fk__parent_object_id) => im.call(table)}}.
           select{[:fk__name, 
@@ -169,10 +169,10 @@ module Sequel
         m = output_identifier_meth
         im = input_identifier_meth
         indexes = {}
-        ds = metadata_dataset.from(:sys__tables___t).
-         join(:sys__indexes___i, :object_id=>:object_id).
-         join(:sys__index_columns___ic, :object_id=>:object_id, :index_id=>:index_id).
-         join(:sys__columns___c, :object_id=>:object_id, :column_id=>:column_id).
+        ds = metadata_dataset.from(Sequel.as(Sequel.lit('[sys].[tables]'),'t')).
+         join(Sequel.as(Sequel.lit('[sys].[indexes]'),'i'), :object_id=>:object_id).
+         join(Sequel.as(Sequel.lit('[sys].[index_columns]'),'ic'), :object_id=>:object_id, :index_id=>:index_id).
+         join(Sequel.as(Sequel.lit('[sys].[columns]'), 'c'), :object_id=>:object_id, :column_id=>:column_id).
          select(:i__name, :i__is_unique, :c__name___column).
          where{{t__name=>im.call(table)}}.
          where(:i__is_primary_key=>0, :i__is_disabled=>0).
@@ -433,15 +433,15 @@ module Sequel
         inf_sch_qual = lambda{|s| info_sch_sch ? Sequel.qualify(info_sch_sch, s) : Sequel.expr(s)}
         sys_qual = lambda{|s| info_sch_sch ? Sequel.qualify(info_sch_sch, Sequel.qualify(Sequel.lit(''), s)) : Sequel.expr(s)}
 
-        identity_cols = metadata_dataset.from(:sys__columns).
-          where(:object_id=>table_id, :is_identity=>true).
+       identity_cols = metadata_dataset.from(Sequel.lit('[sys].[columns]')).
+             where(:object_id=>table_id, :is_identity=>true).
           select_map(:name)
 
-        pk_index_id = metadata_dataset.from(sys_qual.call(:sysindexes)).
+        pk_index_id = metadata_dataset.from(sys_qual.call(Sequel.lit('sysindexes'))).
           where(:id=>table_id, :indid=>1..254){{(status & 2048)=>2048}}.
           get(:indid)
-        pk_cols = metadata_dataset.from(sys_qual.call(:sysindexkeys).as(:sik)).
-          join(sys_qual.call(:syscolumns).as(:sc), :id=>:id, :colid=>:colid).
+        pk_cols = metadata_dataset.from(sys_qual.call(Sequel.lit('sysindexkeys')).as(:sik)).
+          join(sys_qual.call(Sequel.lit('syscolumns')).as(:sc), :id=>:id, :colid=>:colid).
           where(:sik__id=>table_id, :sik__indid=>pk_index_id).
           select_order_map(:sc__name)
 
