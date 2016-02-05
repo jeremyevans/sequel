40a63ece01a5900d19844ddd35463259b1832eb9
Jeremy Evans <code@jeremyevans.net>
2016-02-05 15:52:47 -0800

    Make eager_each plugin handle eager loading for Dataset#first and similar methods
    The eager_each plugin kind of worked for Dataset#first before Sequel
    4.27.0, but the behavior was unspecified, and it only worked correctly
    in the eager case, not in the eager_graph case.  Due to refactoring in
    4.27.0, it no longer eager loaded in either case.
    
    This brings eager loading for #first support back to eager_each, but it
    fixes the eager_graph issue by doing a 2nd query in the eager_graph case
    to eagerly all load associated records related to the main record.

--- lib/sequel/plugins/eager_each.rb
+++ lib/sequel/plugins/eager_each.rb
@@ -15,6 +15,11 @@ module Sequel
     # and setting a new flag in the cloned dataset, so that each can check with the flag to
     # determine whether it should call all.
     #
+    # This plugin also makes #first and related methods that load single records work with
+    # eager loading.  Note that when using eager_graph, calling #first or a similar method
+    # will result in two queries, one to load the main object, and one to eagerly load all associated
+    # objects to that main object.
+    #
     # Usage:
     #
     #   # Make all model subclass instances eagerly load for each (called before loading subclasses)
@@ -48,6 +53,23 @@ module Sequel
         def all(&block)
           if use_eager_all?
             clone(:all_called=>true).all(&block)
+          else
+            super
+          end
+        end
+
+        # Handle eager loading when calling first and related methods.  For eager_graph,
+        # this does an additional query after retrieving a single record, because otherwise
+        # the associated records won't get eager loaded correctly.
+        def single_record!
+          if use_eager_all?
+            obj = clone(:all_called=>true).all.first
+
+            if opts[:eager_graph]
+              obj = clone(:all_called=>true).where(obj.qualified_pk_hash).unlimited.all.first
+            end
+
+            obj
           else
             super
           end
--- spec/extensions/eager_each_spec.rb
+++ spec/extensions/eager_each_spec.rb
@@ -26,12 +26,31 @@ describe "Sequel::Plugins::EagerEach" do
   it "should make #each on an eager_graph dataset do eager loading" do
     a = []
     ds = @c.eager_graph(:children)
-    ds._fetch = []
     ds._fetch = [{:id=>1, :parent_id=>nil, :children_id=>3, :children_parent_id=>1}, {:id=>1, :parent_id=>nil, :children_id=>4, :children_parent_id=>1}, {:id=>2, :parent_id=>nil, :children_id=>5, :children_parent_id=>2}, {:id=>2, :parent_id=>nil, :children_id=>6, :children_parent_id=>2}]
     ds.each{|c| a << c}
     a.must_equal [@c.load(:id=>1, :parent_id=>nil), @c.load(:id=>2, :parent_id=>nil)]
     a.map{|c| c.associations[:children]}.must_equal [[@c.load(:id=>3, :parent_id=>1), @c.load(:id=>4, :parent_id=>1)], [@c.load(:id=>5, :parent_id=>2), @c.load(:id=>6, :parent_id=>2)]]
     @c.db.sqls.must_equal ['SELECT items.id, items.parent_id, children.id AS children_id, children.parent_id AS children_parent_id FROM items LEFT OUTER JOIN items AS children ON (children.parent_id = items.id)']
+  end
+
+  it "should make #first on an eager dataset do eager loading" do
+    ds = @c.eager(:children)
+    ds._fetch = [{:id=>1, :parent_id=>nil}]
+    @c.dataset._fetch = [{:id=>3, :parent_id=>1}, {:id=>4, :parent_id=>1}]
+    a = ds.first
+    a.values.must_equal(:id=>1, :parent_id=>nil)
+    a.associations[:children].must_equal [@c.load(:id=>3, :parent_id=>1), @c.load(:id=>4, :parent_id=>1)]
+    @c.db.sqls.must_equal ['SELECT * FROM items LIMIT 1','SELECT * FROM items WHERE (items.parent_id IN (1))'] 
+  end
+
+  it "should make #first on an eager_graph dataset do eager loading" do
+    ds = @c.eager_graph(:children)
+    ds._fetch = [[{:id=>1, :parent_id=>nil, :children_id=>3, :children_parent_id=>1}], [{:id=>1, :parent_id=>nil, :children_id=>3, :children_parent_id=>1}, {:id=>1, :parent_id=>nil, :children_id=>4, :children_parent_id=>1}]]
+    a = ds.first
+    a.values.must_equal(:id=>1, :parent_id=>nil)
+    a.associations[:children].must_equal [@c.load(:id=>3, :parent_id=>1), @c.load(:id=>4, :parent_id=>1)]
+    @c.db.sqls.must_equal ['SELECT items.id, items.parent_id, children.id AS children_id, children.parent_id AS children_parent_id FROM items LEFT OUTER JOIN items AS children ON (children.parent_id = items.id) LIMIT 1',
+      'SELECT items.id, items.parent_id, children.id AS children_id, children.parent_id AS children_parent_id FROM items LEFT OUTER JOIN items AS children ON (children.parent_id = items.id) WHERE (items.id = 1)']
   end
 
   it "should not attempt to eager load when getting the columns" do
