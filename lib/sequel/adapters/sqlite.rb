d2bfbe4118c4c023be8b171807d1465668e6ebf2
T. Wenrich <karfas@gmail.com>
2016-07-13 22:17:54 +0200

    Load Sqlite3 extensions during connect

--- lib/sequel/adapters/sqlite.rb
+++ lib/sequel/adapters/sqlite.rb
@@ -101,6 +101,8 @@ module Sequel
       #              static data that you do not want to modify
       # :timeout :: how long to wait for the database to be available if it
       #             is locked, given in milliseconds (default is 5000)
+      # :extensions :: array of SQLITE3 extension files (shared objects) to load
+      # 
       def connect(server)
         opts = server_opts(server)
         opts[:database] = ':memory:' if blank_object?(opts[:database])
@@ -109,6 +111,13 @@ module Sequel
         db = ::SQLite3::Database.new(opts[:database].to_s, sqlite3_opts)
         db.busy_timeout(opts.fetch(:timeout, 5000))
         
+        if opts.has_key?(:extensions)
+          ext = opts[:extensions]
+          db.enable_load_extension(1)
+          ext.each { |file| db.load_extensison file }
+          db.enable_load_extension(0)
+        end
+
         connection_pragmas.each{|s| log_connection_yield(s, db){db.execute_batch(s)}}
         
         class << db
