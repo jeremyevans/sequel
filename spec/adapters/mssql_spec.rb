bca7e7251d54f5ff87e4d02f57104bf71c99ada8
Michaël van Diemen <van.diemen.michael@gmail.com>
2016-08-03 19:48:16 +0200

    Added test for `#mssql_sproc_ds`

--- spec/adapters/mssql_spec.rb
+++ spec/adapters/mssql_spec.rb
@@ -667,6 +667,14 @@ describe "MSSQL Stored Procedure support" do
     @db.execute('DROP PROCEDURE dbo.SequelTest')
   end
 
+  describe "with dataset" do
+    it "should return a Dataset with the hash of output variables" do
+      r = @db.mssql_sproc_ds(:SequelTest, {:args => [@now, 1, :output, :output]})
+      r.must_be_kind_of(Dataset)
+      r.first.must_be_kind_of(Hash)
+    end
+  end
+
   describe "with unnamed parameters" do
     it "should return a hash of output variables" do
       r = @db.call_mssql_sproc(:SequelTest, {:args => [@now, 1, :output, :output]})
