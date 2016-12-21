ad135580c41ba07dc24e1ec0bb5417d59c51ce8c
Jeremy Evans <code@jeremyevans.net>
2016-12-21 14:14:24 -0800

    Cache Dataset#from calls with a single symbol argument in freeze_datasets extension
    This provides a decent speedup to Dataset#from by itself, but
    the big gain here is that you can use the cache.  For example
    
      DB[:foo].first(10)
    
    Usually creates a bunch of intermediate datasets and has build
    an SQL string everytime.  This makes it create no intermediate
    datasets, build a placeholder literalizer (on the 3rd call), and
    there doesn't have to build the SQL string everytime, resulting
    in about a 35% speedup.

--- lib/sequel/extensions/freeze_datasets.rb
+++ lib/sequel/extensions/freeze_datasets.rb
@@ -5,6 +5,16 @@
 # frozen.  This makes sure you can never accidentally modify a
 # dataset that may be used elsewhere (such as a model class's
 # dataset or the same dataset being used in another thread).
+# Frozen datasets can also perform caching for many
+# different methods that can significantly improve performance.
+#
+# In addition to the caching provided by frozen datasets, this
+# also adds caching of Database#from calls if the method is
+# called with a single Symbol argument and not a block.  In
+# addition to speeding up Dataset#from itself, because it
+# returns a cached dataset, all caching done by that dataset
+# can also improve performance.
+#
 # Usage:
 #
 #   DB.extension(:freeze_datasets)
@@ -27,12 +37,30 @@ module Sequel
       # loading the extension.
       def self.extended(db)
         db.extend_datasets(DatasetMethods)
-        db.send(:reset_default_dataset)
+      end
+
+      # Cache returned dataset if given a single argument and no block.
+      def from(*args, &block)
+        if !block && args.length == 1 && (table = args[0]).is_a?(Symbol)
+           @default_dataset.send(:cached_dataset, :"_from_#{table}_ds"){super}
+        else
+          super
+        end
       end
 
       # Freeze datasets created from this dataset.
       def dataset
         super.freeze
+      end
+
+      private
+
+      # Clear the cache of the default dataset when removing a cached
+      # schema, in order to clear the from table cache.
+      def remove_cached_schema(table)
+        cache = @default_dataset.send(:cache)
+        Sequel.synchronize{cache.clear}
+        super
       end
     end
 
--- spec/extensions/freeze_datasets_spec.rb
+++ spec/extensions/freeze_datasets_spec.rb
@@ -15,4 +15,17 @@ describe "freeze_datasets extension" do
   it "should have dataset#dup return frozen dataset" do
     @db.dataset.dup.frozen?.must_equal true
   end
+
+  it "should cache Database#from calls with single symbol tables" do
+    @db.from(:foo).must_be_same_as @db.from(:foo)
+    @db.from(Sequel[:foo]).wont_be_same_as @db.from(Sequel[:foo])
+  end
+
+  it "should clear Database#from cache when modifying the schema" do
+    ds = @db.from(:foo)
+    ds.columns(:foo, :bar)
+    @db[:foo].columns.must_equal [:foo, :bar]
+    @db.create_table!(:foo){Integer :x}
+    @db[:foo].columns.wont_equal [:foo, :bar]
+  end
 end
