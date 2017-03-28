a4db0dbf40cf984db0e0a7f52cf5c80113907cc3
Jeremy Evans <code@jeremyevans.net>
2017-03-28 07:22:02 -0700

    Run specs on 2.4 if warning library is not available

--- spec/sequel_warning.rb
+++ spec/sequel_warning.rb
@@ -5,6 +5,11 @@ end
 
 # SEQUEL5: Remove
 if RUBY_VERSION >= '2.4'
-  require 'warning'
-  Warning.ignore(/warning: constant Sequel::[a-zA-z:]+ is deprecated/, File.dirname(File.dirname(__FILE__)))
+  begin
+    require 'warning'
+  rescue LoadError
+    # nothing
+  else
+    Warning.ignore(/warning: constant Sequel::[a-zA-z:]+ is deprecated/, File.dirname(File.dirname(__FILE__)))
+  end
 end
