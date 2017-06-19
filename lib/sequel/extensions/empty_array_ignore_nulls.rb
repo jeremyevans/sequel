63f3162b53d3f8bd25cdbc93a00163e5fa072e6d
Jeremy Evans <code@jeremyevans.net>
2017-06-19 14:59:32 -0700

    Deprecate empty_array_ignore_nulls extension

--- lib/sequel/extensions/empty_array_ignore_nulls.rb
+++ lib/sequel/extensions/empty_array_ignore_nulls.rb
@@ -2,4 +2,7 @@
 #
 # This only exists for backwards compatibility, as the behavior
 # added by this extension is now the default Sequel behavior. 
+
+Sequel::Deprecation.deprecate("The empty_array_ignore_nulls", "It has been a no-op since 4.25.0")
+
 Sequel::Dataset.register_extension(:empty_array_ignore_nulls){}
