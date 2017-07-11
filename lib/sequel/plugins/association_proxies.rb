867a324d8f0e27d9129528f55cb3f0bd85c4c6ea
Jeremy Evans <code@jeremyevans.net>
2017-07-11 11:11:43 -0700

    Make association method in association_pks only support single argument

--- lib/sequel/plugins/association_proxies.rb
+++ lib/sequel/plugins/association_proxies.rb
@@ -100,9 +100,8 @@ module Sequel
         # directly.
         def def_association_method(opts)
           if opts.returns_array?
-            association_module_def(opts.association_method, opts) do |*dynamic_opts, &block|
-              Sequel::Deprecation.deprecate("Passing multiple arguments to ##{opts.association_method}", "Additional arguments are currently ignored") if dynamic_opts.length > 1
-              AssociationProxy.new(self, opts, dynamic_opts.length == 0 ? OPTS : dynamic_opts[0], &block)
+            association_module_def(opts.association_method, opts) do |dynamic_opts=OPTS, &block|
+              AssociationProxy.new(self, opts, dynamic_opts, &block)
             end
           else
             super
