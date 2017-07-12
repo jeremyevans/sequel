dc0236bfa0ee3aef53d6e2bdabf996e77ab04212
Jeremy Evans <code@jeremyevans.net>
2017-07-11 17:16:35 -0700

    Remove filtering of constant warnings

--- spec/sequel_warning.rb
+++ spec/sequel_warning.rb
@@ -2,14 +2,3 @@ if ENV['WARNING']
   require 'warning'
   Warning.ignore([:missing_ivar, :method_redefined], File.dirname(File.dirname(__FILE__)))
 end
-
-# SEQUEL5: Remove
-if RUBY_VERSION >= '2.4'
-  begin
-    require 'warning'
-  rescue LoadError
-    # nothing
-  else
-    Warning.ignore(/warning: constant Sequel::[a-zA-z:]+ is deprecated/, File.dirname(File.dirname(__FILE__)))
-  end
-end
