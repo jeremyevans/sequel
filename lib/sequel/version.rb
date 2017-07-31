113cae26acb3a99c05a5ea440a4625d74cf6e4b3
Jeremy Evans <code@jeremyevans.net>
2017-07-31 07:24:51 -0700

    Bump version to 5.0
    This is being done early instead of right before release, because
    most of the Sequel 5 changes have already been made.

--- lib/sequel/version.rb
+++ lib/sequel/version.rb
@@ -2,10 +2,10 @@
 
 module Sequel
   # The major version of Sequel.  Only bumped for major changes.
-  MAJOR = 4
+  MAJOR = 5
   # The minor version of Sequel.  Bumped for every non-patch level
   # release, generally around once a month.
-  MINOR = 49
+  MINOR = 0
   # The tiny version of Sequel.  Usually 0, only bumped for bugfix
   # releases that fix regressions from previous versions.
   TINY  = 0
