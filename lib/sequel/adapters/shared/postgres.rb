f50ef8c7fd6e99f2669f7e493407dd94b99e9251
Jeremy Evans <code@jeremyevans.net>
2018-03-26 18:52:00 -0700

    Fix Database#server_version on PostgreSQL

--- lib/sequel/adapters/shared/postgres.rb
+++ lib/sequel/adapters/shared/postgres.rb
@@ -598,7 +598,7 @@ module Sequel
       # The version of the PostgreSQL server, used for determining capability.
       def server_version(server=nil)
         return @server_version if @server_version
-        ds = DB.dataset
+        ds = dataset
         ds = ds.server(server) if server
         @server_version ||= ds.with_sql("SELECT CAST(current_setting('server_version_num') AS integer) AS v").single_value rescue 0
       end
