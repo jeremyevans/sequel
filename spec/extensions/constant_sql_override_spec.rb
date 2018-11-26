f9d8c73b38fa3256b7b3231a6c4fc9cc790fa91d
Jeremy Evans <code@jeremyevans.net>
2018-11-26 07:58:30 -0800

    Add a spec for the constant_sql_override extension for better coverage

--- spec/extensions/constant_sql_override_spec.rb
+++ spec/extensions/constant_sql_override_spec.rb
@@ -11,8 +11,14 @@ describe "constant_sql_override extension" do
     @db.sqls.must_equal ["SELECT * FROM tbl WHERE (foo = CURRENT TIMESTAMP AT TIME ZONE 'UTC') LIMIT 1"]
   end
 
-  it 'freezes the constant_sqls hash' do
+  it 'does not change behavior for unconfigured constants' do
+    @db[:tbl].where(foo: Sequel::CURRENT_TIMESTAMP).first
+    @db.sqls.must_equal ["SELECT * FROM tbl WHERE (foo = CURRENT_TIMESTAMP) LIMIT 1"]
+  end
+
+  it 'freezes the constant_sqls hash when frozen' do
     @db.freeze
     @db.constant_sqls.frozen?.must_equal true
+    proc{@db.set_constant_sql(Sequel::CURRENT_TIMESTAMP, "CURRENT TIMESTAMP AT TIME ZONE 'UTC'")}.must_raise RuntimeError
   end
 end
