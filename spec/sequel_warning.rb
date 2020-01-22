eeeb339dbc70bf9d5a101118e7487380ca0acb36
Jeremy Evans <code@jeremyevans.net>
2020-01-21 16:45:44 -0800

    Dedup warnings when running test in warnings mode
    Mostly helps in 2.7 with adapters that emit a warning per query.

--- spec/sequel_warning.rb
+++ spec/sequel_warning.rb
@@ -1,4 +1,5 @@
 if ENV['WARNING']
   require 'warning'
   Warning.ignore([:missing_ivar, :method_redefined, :not_reached], File.dirname(File.dirname(__FILE__)))
+  Warning.dedup if Warning.respond_to?(:dedup)
 end
