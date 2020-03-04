951c88a1b8a9f9abdf43f2698c8e0114481765a9
Jeremy Evans <code@jeremyevans.net>
2020-03-04 09:59:47 -0800

    Add forbid_lazy_load plugin for forbidding lazy loading of associations, to help find N+1 issues
    Sequel has shipped with the tactical_eager_loading plugin for over
    10 years, which I think is a better approach for N+1 issues, as
    it handles them automatically in most cases.  However, if you don't
    want the additional memory retention of the tactical_eager_loading
    plugin, or you want to explicitly control what associations get
    loaded at the dataset level, this is an alternative approach.
    
    ActiveRecord 6.1 will ship with a similar feature called strict
    loading.  ActiveRecord's feature requires configuration on a
    per-dataset basis in cases where you want to forbid the lazy
    loading. Sequel's forbid_lazy_load plugin tries to identify cases
    where lazy loading could cause N+1 issues, and forbid only those
    cases.  It should be possible to load this plugin into any
    application that does not have N+1 query issues and have it
    result in no changes. However, loading this plugin in an
    application that has N+1 query issues should result in an
    exception being raised instead of issuing the N queries.
    
    One way to use this plugin is to only enable it when testing,
    or in certain test configurations.  That way you don't pay any
    performance cost in production.

---
+++ lib/sequel/plugins/forbid_lazy_load.rb
@@ -0,0 +1,214 @@
+# frozen-string-literal: true
+
+module Sequel
+  module Plugins
+    # The forbid_lazy_load plugin forbids lazy loading of associations
+    # for objects in cases where the object wasn't loaded with a
+    # method that only returns a single object.
+    #
+    # The main reason for doing this is it makes it easier to detect
+    # N+1 query issues. Note that Sequel also offers a
+    # tactical_eager_loading plugin which will automatically eagerly
+    # load associations for all objects retrived in the same query
+    # if any object would attempt to lazily load an association. That
+    # approach may be simpler if you are trying to prevent N+1 issues,
+    # though it does retain more objects in memory.
+    #
+    # This plugin offers multiple different ways to forbid lazy
+    # loading.  You can forbid lazy loading associations for individual
+    # model instances:
+    #
+    #   obj = Album[1]
+    #   obj.forbid_lazy_load
+    #   obj.artist # raises Sequel::Plugins::ForbidLazyLoad::Error
+    #
+    # +forbid_lazy_load+ is automatically called on instances if the
+    # instances are loaded via a method such as Dataset#all,
+    # Dataset#each, and other methods that load multiple instances
+    # at once.  These are the cases where lazily loading associations
+    # for such instances can cause N+1 issues.
+    #
+    #   Album.all.first.artist
+    #   objs.first.artist # raises Sequel::Plugins::ForbidLazyLoad::Error
+    #
+    #   Album.each do |obj|
+    #     obj.artist # raises Sequel::Plugins::ForbidLazyLoad::Error
+    #   end
+    #
+    #   Album[1].artist # no error
+    #
+    #   Album.first.artist # no error
+    #
+    # You can allow lazy loading associations for an instance that it
+    # was previously forbidden for:
+    # 
+    #   obj = Album.all.first
+    #   obj.allow_lazy_load
+    #   obj.artist # no error
+    #
+    # You can forbid lazy loading associations on a per-call basis,
+    # even if lazy loading of associations is allowed for the instance:
+    #
+    #   obj = Album[1]
+    #   obj.artist(forbid_lazy_load: true)
+    #   # raises Sequel::Plugins::ForbidLazyLoad::Error
+    #
+    # This also works for allowing lazy loading associations for a
+    # specific association load even if it is forbidden for the instance:
+    #  
+    #   obj = Album.all.first
+    #   obj.artist(forbid_lazy_load: false)
+    #   # nothing raised
+    #
+    # You can also forbid lazy loading on a per-association basis using the
+    # +:forbid_lazy_load+ association option with a +true+ value:
+    #
+    #   Album.many_to_one :artist, forbid_lazy_load: true
+    #   Album[1].artist # raises Sequel::Plugins::ForbidLazyLoad::Error
+    #
+    # However, you probably don't want to do this as it will forbid any
+    # lazy loading of the association, even if the loading could not
+    # result in an N+1 issue.
+    #
+    # On the flip side, you can allow lazy loading using the 
+    # +:forbid_lazy_load+ association option with a +false+ value:
+    #
+    #   Album.many_to_one :artist, forbid_lazy_load: false
+    #   Album.all.first.artist # no error
+    #
+    # One reason to do this is when using a plugin like static_cache
+    # on the associated model, where a query is not actually issued
+    # when doing a lazy association load.  To make that particular
+    # case easier, this plugin makes Model.finalize_associations
+    # automatically set the association option if the associated
+    # class uses the static_cache plugin.
+    #
+    # Note that even with this plugin, there can still be N+1 issues,
+    # such as:
+    #
+    #   Album.each do |obj| # 1 query for all albums
+    #     Artist[obj.artist_id] # 1 query per album for each artist
+    #   end
+    # 
+    # Usage:
+    #
+    #   # Make all model subclasses support forbidding lazy load
+    #   # (called before loading subclasses)
+    #   Sequel::Model.plugin :forbid_lazy_load
+    #
+    #   # Make the Album class support forbidding lazy load
+    #   Album.plugin :forbid_lazy_load
+    module ForbidLazyLoad
+      # Error raised when attempting to lazy load an association when
+      # lazy loading has been forbidden.
+      class Error < StandardError
+      end
+
+      module ClassMethods
+        Plugins.def_dataset_methods(self, :forbid_lazy_load)
+
+        # If the static_cache plugin is used by the associated class for
+        # an association, allow lazy loading that association, since the
+        # lazy association load will use a hash table lookup and not a query.
+        def allow_lazy_load_for_static_cache_associations
+          if defined?(::Sequel::Plugins::StaticCache::ClassMethods)
+            @association_reflections.each_value do |ref|
+              if ref.associated_class.is_a?(::Sequel::Plugins::StaticCache::ClassMethods)
+                ref[:forbid_lazy_load] = false
+              end
+            end
+          end
+        end
+
+        # Allow lazy loading for static cache associations before finalizing.
+        def finalize_associations
+          allow_lazy_load_for_static_cache_associations
+          super
+        end
+      end
+
+      module InstanceMethods
+        # Set this model instance to allow lazy loading of associations.
+        def allow_lazy_load
+          @forbid_lazy_load = false
+          self
+        end
+
+        # Set this model instance to not allow lazy loading of associations.
+        def forbid_lazy_load
+          @forbid_lazy_load = true
+          self
+        end
+
+        private
+
+        # Allow lazy loading for objects returned by singular associations.
+        def _load_associated_object(opts, dynamic_opts)
+          # The implementation that loads these associations does
+          # .all.first, which would result in the object returned being
+          # marked as forbidding lazy load.
+          obj = super
+          obj.allow_lazy_load if obj.is_a?(InstanceMethods)
+          obj
+        end
+
+        # Raise an Error if lazy loading has been forbidden for
+        # the instance, association, or call.
+        def _load_associated_objects(opts, dynamic_opts=OPTS)
+          case dynamic_opts[:forbid_lazy_load]
+          when false
+            # nothing
+          when nil
+            unless dynamic_opts[:reload]
+              case opts[:forbid_lazy_load]
+              when nil
+                raise Error, "lazy loading forbidden for this object (association: #{opts.inspect}, object: #{inspect})" if @forbid_lazy_load
+              when false
+                # nothing
+              else
+                raise Error, "lazy loading forbidden for this association (#{opts.inspect})"
+              end
+            end
+          else
+            raise Error, "lazy loading forbidden for this association method call (association: #{opts.inspect})"
+          end
+
+          super
+        end
+      end
+
+      module DatasetMethods
+        # Mark model instances retrieved in this call as forbidding lazy loading.
+        def each
+          if row_proc
+            super do |obj|
+              obj.forbid_lazy_load if obj.is_a?(InstanceMethods)
+              yield obj
+            end
+          else
+            super
+          end
+        end
+
+        # Mark model instances retrieved in this call as forbidding lazy loading.
+        def with_sql_each(sql)
+          if row_proc
+            super(sql) do |obj|
+              obj.forbid_lazy_load if obj.is_a?(InstanceMethods)
+              yield obj
+            end
+          else
+            super
+          end
+        end
+
+        # Mark model instances retrieved in this call as allowing lazy loading.
+        def with_sql_first(sql)
+          obj = super
+          obj.allow_lazy_load if obj.is_a?(InstanceMethods)
+          obj
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/forbid_lazy_load_spec.rb
@@ -0,0 +1,278 @@
+require_relative "spec_helper"
+
+describe "forbid_lazy_load plugin" do
+  before do
+    @c = Class.new(Sequel::Model)
+    @c.set_dataset Sequel::Model.db[:ts].with_fetch({:id=>2, :t_id=>3})
+    @c.plugin :forbid_lazy_load
+    @c.columns :id, :t_id
+    @c.many_to_one :t, :class=>@c, :key=>:t_id
+    @c.one_to_many :ts, :class=>@c, :key=>:t_id
+    @c.many_to_many :mtm_ts, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id
+    @c.one_to_one :otoo_t, :class=>@c, :key=>:t_id
+    @c.one_through_one :oto_t, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id
+    @o1 = @c.load(:id=>1, :t_id=>2)
+    @o2 = @c.load(:id=>2, :t_id=>3)
+  end
+
+  it "should not forbid lazy load if not set at instance level" do
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+  end
+
+  it "should forbid lazy load when using :forbid_lazy_load true association method option" do
+    proc{@o1.t(:forbid_lazy_load=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.ts(:forbid_lazy_load=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.mtm_ts(:forbid_lazy_load=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.otoo_t(:forbid_lazy_load=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.oto_t(:forbid_lazy_load=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should forbid lazy load if set at instance level" do
+    @o1.forbid_lazy_load.must_be_same_as @o1
+    proc{@o1.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should allow lazy load for instance if set at instance level" do
+    o = @c.all.first
+    o.allow_lazy_load.must_be_same_as o
+    o.t.must_equal @o2
+    o.ts.must_equal [@o2]
+    o.mtm_ts.must_equal [@o2]
+    o.otoo_t.must_equal @o2
+    o.oto_t.must_equal @o2
+  end
+
+  it "should forbid lazy load if retrieved by dataset via Dataset#all" do
+    o = @c.all.first
+    proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should forbid lazy load if retrieved by dataset via Dataset#each" do
+    o = @c.each{|x| break x}
+    proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should forbid lazy load if retrieved by dataset via Dataset#where_each" do
+    5.times do
+      o = @c.where_each(:id=>1){|x| break x}
+      proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    end
+  end
+
+  it "should forbid lazy load if retrieved by dataset via Dataset#first with integer argument" do
+    5.times do
+      o = @c.first(2)[0]
+      proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+
+      o = @c.first(2){id > 0}[0]
+      proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    end
+  end
+
+  it "should not forbid lazy load if retrieved by dataset via Dataset#first with no arguments" do
+    5.times do
+      o = @c.first
+      o.t.must_equal @o2
+      o.ts.must_equal [@o2]
+      o.mtm_ts.must_equal [@o2]
+      o.otoo_t.must_equal @o2
+      o.oto_t.must_equal @o2
+    end
+  end
+
+  it "should not forbid lazy load if retrieved by dataset via Dataset#first with hash argument" do
+    5.times do
+      o = @c.first(id: 2)
+      o.t.must_equal @o2
+      o.ts.must_equal [@o2]
+      o.mtm_ts.must_equal [@o2]
+      o.otoo_t.must_equal @o2
+      o.oto_t.must_equal @o2
+    end
+  end
+
+  it "should not forbid lazy load if retrieved by dataset via Dataset#first with block" do
+    5.times do
+      o = @c.first{id > 1}
+      o.t.must_equal @o2
+      o.ts.must_equal [@o2]
+      o.mtm_ts.must_equal [@o2]
+      o.otoo_t.must_equal @o2
+      o.oto_t.must_equal @o2
+    end
+  end
+
+  it "should not forbid lazy load if retrieved by dataset via Dataset#with_pk" do
+    5.times do
+      o = @c.dataset.with_pk(1)
+      o.t.must_equal @o2
+      o.ts.must_equal [@o2]
+      o.mtm_ts.must_equal [@o2]
+      o.otoo_t.must_equal @o2
+      o.oto_t.must_equal @o2
+    end
+  end
+
+  it "should not forbid lazy load for associated objects returned by singular associations" do
+    [@o1.t, @o1.otoo_t, @o1.oto_t].each do |o|
+      o.associations.clear
+      o.t.must_equal @o2
+      o.ts.must_equal [@o2]
+      o.mtm_ts.must_equal [@o2]
+      o.otoo_t.must_equal @o2
+      o.oto_t.must_equal @o2
+    end
+  end
+
+  it "should forbid lazy load for associated objects returned by plural associations" do
+    [@o1.ts, @o1.mtm_ts].each do |os|
+      o = os.first
+      o.associations.clear
+      proc{o.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+      proc{o.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    end
+  end
+
+  it "should allow association access if cached even if forbidding lazy loading" do
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+
+    @o1.forbid_lazy_load
+
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+  end
+
+  it "should forbid lazy load for associations if forbid_lazy_load true association option is used" do
+    @c.many_to_one :t, :class=>@c, :key=>:t_id, :forbid_lazy_load=>true
+    @c.one_to_many :ts, :class=>@c, :key=>:t_id, :forbid_lazy_load=>true
+    @c.many_to_many :mtm_ts, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id, :forbid_lazy_load=>true
+    @c.one_to_one :otoo_t, :class=>@c, :key=>:t_id, :forbid_lazy_load=>true
+    @c.one_through_one :oto_t, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id, :forbid_lazy_load=>true
+
+    proc{@o1.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should allow lazy load for associations even if instances have it forbidden if forbid_lazy_load false association option is used" do
+    @c.many_to_one :t, :class=>@c, :key=>:t_id, :forbid_lazy_load=>false
+    @c.one_to_many :ts, :class=>@c, :key=>:t_id, :forbid_lazy_load=>false
+    @c.many_to_many :mtm_ts, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id, :forbid_lazy_load=>false
+    @c.one_to_one :otoo_t, :class=>@c, :key=>:t_id, :forbid_lazy_load=>false
+    @c.one_through_one :oto_t, :class=>@c, :join_table=>:ts, :left_key=>:id, :right_key=>:t_id, :forbid_lazy_load=>false
+
+    o = @c.all.first
+    o.t.must_equal @o2
+    o.ts.must_equal [@o2]
+    o.mtm_ts.must_equal [@o2]
+    o.otoo_t.must_equal @o2
+    o.oto_t.must_equal @o2
+  end
+
+  it "should forbid lazy load after finalizing associations if not using static_cache in associated class" do
+    @c.finalize_associations
+    @o1.forbid_lazy_load
+
+    proc{@o1.t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.mtm_ts}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.otoo_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.oto_t}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should set forbid_lazy_load false association option if using static_cache in associated class and using allow_lazy_load_for_static_cache_associations" do
+    @c.plugin :static_cache
+    @c.allow_lazy_load_for_static_cache_associations
+    @o1.forbid_lazy_load
+
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+  end
+
+  it "should automatically set forbid_lazy_load false association option when finalizing associations if using static_cache in associated class" do
+    @c.plugin :static_cache
+    @c.finalize_associations
+    @o1.forbid_lazy_load
+
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+  end
+
+  it "should allow lazy load when forbidden when using :reload association method option" do
+    @o1.forbid_lazy_load
+    @o1.t(:reload=>true).must_equal @o2
+    @o1.ts(:reload=>true).must_equal [@o2]
+    @o1.mtm_ts(:reload=>true).must_equal [@o2]
+    @o1.otoo_t(:reload=>true).must_equal @o2
+    @o1.oto_t(:reload=>true).must_equal @o2
+  end
+
+  it "should allow lazy load when forbidden when using :forbid_lazy_load false association option" do
+    @o1.forbid_lazy_load
+    @o1.t(:forbid_lazy_load=>false).must_equal @o2
+    @o1.ts(:forbid_lazy_load=>false).must_equal [@o2]
+    @o1.mtm_ts(:forbid_lazy_load=>false).must_equal [@o2]
+    @o1.otoo_t(:forbid_lazy_load=>false).must_equal @o2
+    @o1.oto_t(:forbid_lazy_load=>false).must_equal @o2
+  end
+
+  it "should forbid lazy load when using :forbid_lazy_load true association method option even when using :reload association method option" do
+    proc{@o1.t(:forbid_lazy_load=>true, :reload=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.ts(:forbid_lazy_load=>true, :reload=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.mtm_ts(:forbid_lazy_load=>true, :reload=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.otoo_t(:forbid_lazy_load=>true, :reload=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+    proc{@o1.oto_t(:forbid_lazy_load=>true, :reload=>true)}.must_raise Sequel::Plugins::ForbidLazyLoad::Error
+  end
+
+  it "should not effect naked datasets" do
+    @c.naked.all.must_equal [{:id=>2, :t_id=>3}]
+    @c.naked.where_each(:id=>1){|x| break x}.must_equal(:id=>2, :t_id=>3)
+  end
+end
