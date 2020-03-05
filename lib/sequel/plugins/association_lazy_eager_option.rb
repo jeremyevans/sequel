d137a4c5647e1b9368c36db50c26bd7d33749e45
Jeremy Evans <code@jeremyevans.net>
2020-03-05 15:40:38 -0800

    Add association_lazy_eager_option plugin for supporting :eager option for association method
    This plugin makes it easier to eagerly load nested associations
    when lazy loading associations.  That was already possible by
    passing a block to the association method and calling eager inside
    of it.  However, passing a block ignores any cached association.
    
    The main advantage of this plugin is that it does not ignore
    cached associations, it just returns the cached association without
    doing any loading.  This is useful in case you have code where the
    association and the nested associations are both already cached.

---
+++ lib/sequel/plugins/association_lazy_eager_option.rb
@@ -0,0 +1,64 @@
+# frozen-string-literal: true
+
+module Sequel
+  module Plugins
+    # The association_lazy_eager_option plugin supports passing
+    # an +:eager+ option to an association method.  If the related
+    # association is already cached, the cached version will be
+    # returned.  If the association is not already cached, it will
+    # be loaded, and the value of the +:eager+ option will be used
+    # to perform an eager load of the given associations.
+    # the plural versions.
+    #
+    # With Sequel's default behavior, you can already perform an
+    # eager load when lazy loading using a block:
+    #
+    #   obj.association{|ds| ds.eager(:nested_association)}
+    #
+    # However, this will ignore any cached version.  In more
+    # complex software, the association may already be cached
+    # and have the nested association cached inside of it, and
+    # using this callback approach then requires 2 unnecessary
+    # queries. This plugin will not perform any queries if the
+    # association is already cached, preventing duplicate work.
+    # However, you should make sure that an already loaded
+    # association has the nested association already eagerly
+    # loaded.
+    # 
+    # Usage:
+    #
+    #   # Make all model subclasses support the :eager association
+    #   # method option (called before loading subclasses)
+    #   Sequel::Model.plugin :association_lazy_eager_option
+    #
+    #   # Make the Album class support the :eager association
+    #   # method option
+    #   Album.plugin :association_lazy_eager_option
+    module AssociationLazyEagerOption
+      module InstanceMethods
+        # Return a dataset for the association after applying any dynamic callback.
+        def _associated_dataset(opts, dynamic_opts)
+          ds = super
+
+          if eager = dynamic_opts[:eager]
+            ds = ds.eager(eager)
+          end
+
+          ds
+        end
+        
+        # A placeholder literalizer that can be used to load the association, or nil to not use one.
+        def _associated_object_loader(opts, dynamic_opts)
+          return if dynamic_opts[:eager]
+          super
+        end
+
+        # Whether to use a simple primary key lookup on the associated class when loading.
+        def load_with_primary_key_lookup?(opts, dynamic_opts)
+          return false if dynamic_opts[:eager]
+          super
+        end
+      end
+    end
+  end
+end
---
+++ spec/extensions/association_lazy_eager_option_spec.rb
@@ -0,0 +1,69 @@
+require_relative "spec_helper"
+
+describe "association_lazy_eager_option plugin" do
+  before do
+    @c = Class.new(Sequel::Model)
+    @db = Sequel.mock
+    @c.set_dataset @db[:ts]
+    @c.plugin :association_lazy_eager_option
+    @c.columns :id, :t_id
+    @c.many_to_one :t, :class=>@c, :key=>:t_id
+    @c.one_to_many :ts, :class=>@c, :key=>:t_id
+    @c.many_to_many :mtm_ts, :class=>@c, :join_table=>:ts, :left_primary_key=>:t_id, :left_key=>:id, :right_key=>:t_id
+    @c.one_to_one :otoo_t, :class=>@c, :key=>:t_id
+    @c.one_through_one :oto_t, :class=>@c, :join_table=>:ts, :left_primary_key=>:t_id, :left_key=>:id, :right_key=>:t_id
+    @o1 = @c.load(:id=>1, :t_id=>2)
+    @o2 = @c.load(:id=>2, :t_id=>3)
+    @o3 = @c.load(:id=>3, :t_id=>4)
+    @db.sqls
+  end
+
+  it "should support the :eager association method option when doing lazy association loads" do
+    @db.fetch = [[{:id=>2, :t_id=>3}], [{:id=>3, :t_id=>4}]]
+    o = @o1.t(:eager=>:t)
+    @db.sqls.must_equal ["SELECT * FROM ts WHERE (ts.id = 2) LIMIT 1", "SELECT * FROM ts WHERE (ts.id IN (3))"]
+    o.must_equal @o2
+    o.associations[:t].must_equal @o3
+
+    @db.fetch = [[{:id=>2, :t_id=>3}], [{:id=>3, :t_id=>4}]]
+    o = @o3.ts(:eager=>[:t])
+    @db.sqls.must_equal ["SELECT * FROM ts WHERE (ts.t_id = 3)", "SELECT * FROM ts WHERE (ts.id IN (3))"]
+    o.must_equal [@o2]
+    o.first.associations[:t].must_equal @o3
+
+    @db.fetch = [[{:id=>2, :t_id=>3}], [{:id=>3, :t_id=>4}]]
+    o = @o1.mtm_ts(:eager=>{:t=>{}})
+    @db.sqls.must_equal ["SELECT ts.* FROM ts INNER JOIN ts ON (ts.t_id = ts.id) WHERE (ts.id = 2)", "SELECT * FROM ts WHERE (ts.id IN (3))"]
+    o.must_equal [@o2]
+    o.first.associations[:t].must_equal @o3
+
+    @db.fetch = [[{:id=>2, :t_id=>3}], [{:id=>1, :t_id=>2}]]
+    o = @o1.otoo_t(:eager=>:ts)
+    @db.sqls.must_equal ["SELECT * FROM ts WHERE (ts.t_id = 1) LIMIT 1", "SELECT * FROM ts WHERE (ts.t_id IN (2))"]
+    o.must_equal @o2
+    o.associations[:ts].must_equal [@o1]
+
+    @db.fetch = [[{:id=>2, :t_id=>3}], [{:id=>3, :t_id=>4}]]
+    o = @o1.oto_t(:eager=>:t)
+    @db.sqls.must_equal ["SELECT ts.* FROM ts INNER JOIN ts ON (ts.t_id = ts.id) WHERE (ts.id = 2) LIMIT 1", "SELECT * FROM ts WHERE (ts.id IN (3))"]
+    o.must_equal @o2
+    o.associations[:t].must_equal @o3
+  end
+
+  it "should ignore the :eager and :eager_graph options and return cached result when association is already loaded" do
+    @db.fetch = [{:id=>2, :t_id=>3}]
+    @o1.t.must_equal @o2
+    @o1.ts.must_equal [@o2]
+    @o1.mtm_ts.must_equal [@o2]
+    @o1.otoo_t.must_equal @o2
+    @o1.oto_t.must_equal @o2
+    @db.sqls
+
+    @o1.t(:eager=>:t).must_equal @o2
+    @o1.ts(:eager=>:t).must_equal [@o2]
+    @o1.mtm_ts(:eager=>:t).must_equal [@o2]
+    @o1.otoo_t(:eager=>:t).must_equal @o2
+    @o1.oto_t(:eager=>:t).must_equal @o2
+    @db.sqls.must_equal []
+  end
+end
