b4405a3492b8dc2ff186948682db890f24be783d
Jeremy Evans <code@jeremyevans.net>
2020-03-05 12:03:23 -0800

    Before descriptions in singular_table_names spec

--- spec/extensions/singular_table_names_spec.rb
+++ spec/extensions/singular_table_names_spec.rb
@@ -1,6 +1,6 @@
 require_relative "spec_helper"
 
-describe "Sequel::Plugins::SingularTableNames" do
+describe "singular_table_names plugin" do
   before do
     @c = Class.new(Sequel::Model)
     @c.plugin :singular_table_names
@@ -9,12 +9,12 @@ describe "Sequel::Plugins::SingularTableNames" do
     Object.send(:remove_const, :Foo)
   end
 
-  it "should not used the pluralized table name" do
+  it "should use the singular form of model name for table name" do
     class ::Foo < @c; end
     Foo.table_name.must_equal :foo
   end
 
-  it "should handle nested moedls name" do
+  it "should handle namespaced models using single form of last component of model name" do
     module ::Foo; end
     class Foo::Bar < @c; end
     Foo::Bar.table_name.must_equal :bar
