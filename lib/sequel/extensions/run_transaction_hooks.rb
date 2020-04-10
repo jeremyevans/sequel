19ca3c2d6210458c57ed2e6847f22141676fb945
Jeremy Evans <code@jeremyevans.net>
2020-04-10 15:47:47 -0700

    Add run_transaction_hooks Database extension, allowing for running the transaction hooks before commit/rollback, for use with transactional testing

---
+++ lib/sequel/extensions/run_transaction_hooks.rb
@@ -0,0 +1,72 @@
+# frozen-string-literal: true
+#
+# The run_transaction_hooks extension allows for running after_commit or
+# after_rollback extensions before commit or rollback.  It then removes
+# the hook after running it, so it will not be run twice.
+#
+# This extension should only be used in transactional tests where the
+# transaction always rolls back, to test the behavior of the after_commit
+# and after_rollback hooks.  Any other usage is probably a bad idea.
+#
+# Example:
+#
+#   DB.extension :run_transaction_hooks
+#   x = 1
+#   DB.transaction(rollback: :always) do
+#     DB.after_rollback{x = 3}
+#     DB.after_commit{x = 2}
+#
+#     x # => 1
+#     DB.run_after_rollback_hooks
+#     x # => 3
+#     DB.run_after_commit_hooks
+#     x # => 2
+#   end
+#   x # => 2
+
+#
+class Sequel::Database
+  module RunTransactionHooks
+    # Run all savepoint and transaction after_commit hooks for the current transaction,
+    # and remove the hooks after running them.
+    # Options:
+    # :server :: The server/shard to use.
+    def run_after_commit_hooks(opts=OPTS)
+      _run_transaction_hooks(:after_commit, opts)
+    end
+
+    # Run all savepoint and transaction after_rollback hooks for the current transaction,
+    # and remove the hooks after running them.
+    # Options:
+    # :server :: The server/shard to use.
+    def run_after_rollback_hooks(opts=OPTS)
+      _run_transaction_hooks(:after_rollback, opts)
+    end
+
+    private
+
+    def _run_transaction_hooks(type, opts)
+      synchronize(opts[:server]) do |conn|
+        unless h = _trans(conn)
+          raise Error, "Cannot call run_#{type}_hooks outside of a transaction"
+        end
+
+        if hooks = h[type]
+          hooks.each(&:call)
+          hooks.clear
+        end
+
+        if (savepoints = h[:savepoints])
+          savepoints.each do |savepoint|
+            if hooks = savepoint[type]
+              hooks.each(&:call)
+              hooks.clear
+            end
+          end
+        end
+      end
+    end
+  end
+
+  register_extension(:run_transaction_hooks, RunTransactionHooks)
+end
---
+++ spec/extensions/run_transaction_hooks_spec.rb
@@ -0,0 +1,108 @@
+require_relative "spec_helper"
+
+describe "run_transaction_hooks extension" do
+  before do
+    @db = Sequel.mock.extension(:run_transaction_hooks)
+  end
+
+  it "should support #run_after_{commit,rollback} hooks to run the hooks early" do
+    @db.transaction do
+      @db.after_commit{@db.run "C"}
+      @db.after_commit{@db.run "C2"}
+      @db.after_rollback{@db.run "R"}
+      @db.after_rollback{@db.run "R2"}
+      @db.sqls.must_equal ["BEGIN"]
+      @db.run_after_commit_hooks
+      @db.sqls.must_equal ["C", "C2"]
+      @db.run_after_rollback_hooks
+      @db.sqls.must_equal ["R", "R2"]
+    end
+    @db.sqls.must_equal ["COMMIT"]
+
+    @db.transaction(:rollback=>:always) do
+      @db.after_commit{@db.run "C"}
+      @db.after_commit{@db.run "C2"}
+      @db.after_rollback{@db.run "R"}
+      @db.after_rollback{@db.run "R2"}
+      @db.sqls.must_equal ["BEGIN"]
+      @db.run_after_commit_hooks
+      @db.sqls.must_equal ["C", "C2"]
+      @db.run_after_rollback_hooks
+      @db.sqls.must_equal ["R", "R2"]
+    end
+    @db.sqls.must_equal ["ROLLBACK"]
+  end
+
+  it "should not same hook on transaction completion when using #run_after_{commit,rollback} hooks" do
+    @db.transaction do
+      @db.after_commit{@db.run "C"}
+      @db.after_commit{@db.run "C2"}
+      @db.after_rollback{@db.run "R"}
+      @db.after_rollback{@db.run "R2"}
+      @db.sqls.must_equal ["BEGIN"]
+      @db.run_after_commit_hooks
+      @db.run_after_rollback_hooks
+      @db.sqls
+    end
+    @db.sqls.must_equal ["COMMIT"]
+
+    @db.transaction(:rollback=>:always) do
+      @db.after_commit{@db.run "C"}
+      @db.after_commit{@db.run "C2"}
+      @db.after_rollback{@db.run "R"}
+      @db.after_rollback{@db.run "R2"}
+      @db.sqls.must_equal ["BEGIN"]
+      @db.run_after_commit_hooks
+      @db.run_after_rollback_hooks
+      @db.sqls
+    end
+    @db.sqls.must_equal ["ROLLBACK"]
+  end
+
+  it "should handle savepoint hooks in #run_after_{commit,rollback} hooks" do
+    @db.transaction do
+      @db.after_commit{@db.run "C"}
+      @db.after_rollback{@db.run "R"}
+      @db.transaction(:savepoint=>:true) do
+        @db.after_commit(:savepoint=>true){@db.run "SC"}
+        @db.after_rollback(:savepoint=>true){@db.run "SR"}
+        @db.sqls.must_equal ["BEGIN", "SAVEPOINT autopoint_1"]
+        @db.run_after_commit_hooks
+        @db.sqls.must_equal ["C", "SC"]
+        @db.run_after_rollback_hooks
+        @db.sqls.must_equal ["R", "SR"]
+      end
+    end
+    @db.sqls.must_equal ["RELEASE SAVEPOINT autopoint_1", "COMMIT"]
+
+    @db.transaction(:rollback=>:always) do
+      @db.after_commit{@db.run "C"}
+      @db.after_rollback{@db.run "R"}
+      @db.transaction(:savepoint=>:true) do
+        @db.after_commit(:savepoint=>true){@db.run "SC"}
+        @db.after_rollback(:savepoint=>true){@db.run "SR"}
+      end
+      @db.sqls.must_equal ["BEGIN", "SAVEPOINT autopoint_1", "RELEASE SAVEPOINT autopoint_1"]
+      @db.run_after_commit_hooks
+      @db.sqls.must_equal ["C", "SC"]
+      @db.run_after_rollback_hooks
+      @db.sqls.must_equal ["R", "SR"]
+    end
+    @db.sqls.must_equal ["ROLLBACK"]
+
+    @db.transaction(:rollback=>:always) do
+      @db.after_commit{@db.run "C"}
+      @db.after_rollback{@db.run "R"}
+      @db.transaction(:savepoint=>:true, :rollback=>:always) do
+        @db.after_commit(:savepoint=>true){@db.run "SC"}
+        @db.after_rollback(:savepoint=>true){@db.run "SR"}
+      end
+      @db.sqls.must_equal ["BEGIN", "SAVEPOINT autopoint_1", "ROLLBACK TO SAVEPOINT autopoint_1", "SR"]
+      @db.run_after_commit_hooks
+      @db.sqls.must_equal ["C"]
+      @db.run_after_rollback_hooks
+      @db.sqls.must_equal ["R"]
+    end
+    @db.sqls.must_equal ["ROLLBACK"]
+  end
+end
