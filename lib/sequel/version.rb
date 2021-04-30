df0a3c06f230ed95ea1501ba1e37969e933c052b
Jeremy Evans <code@jeremyevans.net>
2021-04-30 14:59:17 -0700

    Bump version to 5.44.0

--- lib/sequel/version.rb
+++ lib/sequel/version.rb
@@ -6,7 +6,7 @@ module Sequel
 
   # The minor version of Sequel.  Bumped for every non-patch level
   # release, generally around once a month.
-  MINOR = 43
+  MINOR = 44
 
   # The tiny version of Sequel.  Usually 0, only bumped for bugfix
   # releases that fix regressions from previous versions.
