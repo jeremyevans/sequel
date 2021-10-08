57707b840145bdeeada584134bf1185bcd93d009
Jeremy Evans <code@jeremyevans.net>
2021-10-08 11:28:04 -0700

    Add sql_log_normalizer extension for normalizing logged SQL, helpful for analytics and sensitive data
    Update the plugins page on the website to add a link to pg_multirange,
    as that was missed when the extension was added.

---
+++ lib/sequel/extensions/sql_log_normalizer.rb
@@ -0,0 +1,107 @@
+# frozen-string-literal: true
+#
+# The sql_log_normalizer extension normalizes the SQL that is logged,
+# removing the literal strings and numbers in the SQL, and removing the
+# logging of any bound variables:
+#
+#   ds = DB[:table].first(a: 1, b: 'something')
+#   # Without sql_log_normalizer extension
+#   # SELECT * FROM "table" WHERE (("a" = 1) AND ("b" = 'something')) LIMIT 1
+#
+#   # With sql_log_normalizer_extension
+#   # SELECT * FROM "table" WHERE (("a" = ?) AND ("b" = ?)) LIMIT ?
+#
+# The normalization is done by scanning the SQL string being executed
+# for literal strings and numbers, and replacing them with question
+# marks.  While this should work for all or almost all production queries,
+# there are pathlogical queries that will not be handled correctly, such as
+# the use of apostrophes in identifiers:
+#
+#   DB[:"asf'bar"].where(a: 1, b: 'something').first
+#   # Logged as:
+#   # SELECT * FROM "asf?something')) LIMIT ?
+#
+# The expected use case for this extension is when you want to normalize
+# logs to group similar queries, or when you want to protect sensitive
+# data from being stored in the logs.
+#
+# Related module: Sequel::SQLLogNormalizer
+
+#
+module Sequel
+  module SQLLogNormalizer
+    def self.extended(db)
+      type = case db.literal("'")
+      when "''''"
+        :standard
+      when "'\\''"
+        :backslash
+      when "N''''"
+        :n_standard
+      else
+        raise Error, "SQL log normalization is not supported on this database (' literalized as #{db.literal("'").inspect})"
+      end
+      db.instance_variable_set(:@sql_string_escape_type, type)
+    end
+
+    # Normalize the SQL before calling super.
+    def log_connection_yield(sql, conn, args=nil)
+      return yield if skip_logging?
+      sql = normalize_logged_sql(sql)
+      args = nil
+      super
+    end
+
+    # Replace literal strings and numbers in SQL with question mark placeholders.
+    def normalize_logged_sql(sql)
+      sql = sql.dup
+      sql.force_encoding('BINARY')
+      start_index = 0
+      check_n = @sql_string_escape_type == :n_standard
+      outside_string = true
+
+      if @sql_string_escape_type == :backslash
+        search_char = /[\\']/
+        escape_char_offset = 0
+        escape_char_value = 92 # backslash
+      else
+        search_char = "'"
+        escape_char_offset = 1
+        escape_char_value = 39 # apostrophe
+      end
+
+      # The approach used here goes against Sequel's philosophy of never attempting
+      # to parse SQL.  However, parsing the SQL is basically the only way to implement
+      # this support with Sequel's design, and it's better to be pragmatic and accept
+      # this than not be able to support this.
+
+      # Replace literal strings
+      while outside_string && (index = start_index = sql.index("'", start_index))
+        if check_n && index != 0 && sql.getbyte(index-1) == 78 # N' start
+          start_index -= 1
+        end
+        index += 1
+        outside_string = false
+
+        while (index = sql.index(search_char, index)) && (sql.getbyte(index + escape_char_offset) == escape_char_value)
+          # skip escaped characters inside string literal
+          index += 2
+        end
+
+        if index
+          # Found end of string
+          sql[start_index..index] = '?'
+          start_index += 1
+          outside_string = true
+        end
+      end
+
+      # Replace integer and decimal floating point numbers
+      sql.gsub!(/\b-?\d+(?:\.\d+)?\b/, '?')
+
+      sql
+    end
+  end
+
+  Database.register_extension(:sql_log_normalizer, SQLLogNormalizer)
+end
---
+++ spec/extensions/sql_log_normalizer_spec.rb
@@ -0,0 +1,83 @@
+require_relative "spec_helper"
+
+describe "sql_log_normalizer extension" do
+  def db(opts={})
+    return @db if @db
+    @sql = String.new
+    def @sql.info(sql)
+      replace(sql.sub(/\A\(.*?\) /, ''))
+    end
+    if opts[:logger] == false
+      loggers = []
+    else
+      loggers = @sql
+    end
+    @db = Sequel.mock(:loggers=>loggers)
+    case opts[:type]
+    when :backslash
+      @db.extend_datasets do
+        def literal_string_append(sql, v)
+          sql << "'" << v.gsub(/(\\|')/){"\\#{$1}"} << "'"
+        end
+      end
+    when :n_standard
+      @db.extend_datasets do
+        def literal_string_append(sql, v)
+          sql << "N'" << v.gsub("'", "''") << "'"
+        end
+      end
+    when :bad
+      @db.extend_datasets do
+        def literal_string_append(sql, v)
+          sql << "X'" << v.gsub("'", "''") << "'"
+        end
+      end
+    end
+    @db.extension(:sql_log_normalizer)
+    @db
+  end
+
+  it "should normalize literal strings and numbers for standard escaping" do
+    db[:ts].first(:a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM ts WHERE ((a = 1) AND (b = 2.3) AND (c = 'd') AND (d = 'e\\f\\''g''')) LIMIT 1"
+    @sql.must_equal "SELECT * FROM ts WHERE ((a = ?) AND (b = ?) AND (c = ?) AND (d = ?)) LIMIT ?"
+  end
+
+  it "should normalize literal strings and numbers for backslash escaping" do
+    db(:type=>:backslash)[:ts].first(:a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM ts WHERE ((a = 1) AND (b = 2.3) AND (c = 'd') AND (d = 'e\\\\f\\\\\\'g\\'')) LIMIT 1"
+    @sql.must_equal "SELECT * FROM ts WHERE ((a = ?) AND (b = ?) AND (c = ?) AND (d = ?)) LIMIT ?"
+  end
+
+  it "should normalize literal strings and numbers for N' escaping" do
+    db(:type=>:n_standard)[:ts].first(:a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM ts WHERE ((a = 1) AND (b = 2.3) AND (c = N'd') AND (d = N'e\\f\\''g''')) LIMIT 1"
+    @sql.must_equal "SELECT * FROM ts WHERE ((a = ?) AND (b = ?) AND (c = ?) AND (d = ?)) LIMIT ?"
+  end
+
+  it "should normalize literal strings and numbers for N' escaping when using non N' string" do
+    db(:type=>:n_standard)[:ts].first(:a=>1, :b=>2.3, :c=>Sequel.lit("'d'"), :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM ts WHERE ((a = 1) AND (b = 2.3) AND (c = 'd') AND (d = N'e\\f\\''g''')) LIMIT 1"
+    @sql.must_equal "SELECT * FROM ts WHERE ((a = ?) AND (b = ?) AND (c = ?) AND (d = ?)) LIMIT ?"
+  end
+
+  it "should raise an error if you attempt to load it into a database that doesn't literalize strings in an expected way" do
+    proc{db(:type=>:bad)}.must_raise Sequel::Error
+  end
+
+  it "should not affect cases where no logger is used" do
+    db(:logger=>false)[:ts].first(:a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM ts WHERE ((a = 1) AND (b = 2.3) AND (c = 'd') AND (d = 'e\\f\\''g''')) LIMIT 1"
+  end
+
+  it "should handle case where identifier contains apostrophe (will not remove all strings in this case)" do
+    db[:"'ts"].first(:a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'")
+    db.sqls.last.must_equal "SELECT * FROM 'ts WHERE ((a = 1) AND (b = 2.3) AND (c = 'd') AND (d = 'e\\f\\''g''')) LIMIT 1"
+    @sql.must_equal "SELECT * FROM ?d?e\\f\\?g''')) LIMIT ?"
+  end
+
+  it "should not include bound variables when logging" do
+    db.log_connection_yield("X", nil, :a=>1, :b=>2.3, :c=>'d', :d=>"e\\f\\'g'"){}
+    @sql.must_equal "X"
+  end
+end
