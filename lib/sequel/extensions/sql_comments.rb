5804fe231a2904b9f5faa4399bd7a0ac219fdd90
Jeremy Evans <code@jeremyevans.net>
2021-12-02 12:17:16 -0800

    Make sql_comments Database extension support Database#with_comments, for automatically including comments for queries executed inside the block
    This is useful when analyzing query logs, where you want to group
    all queries related to a certain action by looking for the same
    comment.
    
    This also fixes a bug that would cause an SQL comment to be appended
    multiple time if the dataset already has cached SQL.

--- lib/sequel/extensions/sql_comments.rb
+++ lib/sequel/extensions/sql_comments.rb
@@ -44,11 +44,51 @@
 #
 #   DB.extension(:sql_comments)
 #
+# Loading the sql_comments extension into the database also adds 
+# support for block-level comment support via Database#with_comments.
+# You call #with_comments with a hash.  Queries inside the hash will
+# include a comment based on the hash (assuming they are inside the
+# same thread):
+#
+#   DB.with_comments(model: Album, action: :all) do
+#     DB[:albums].all
+#     # SELECT * FROM albums -- model:Album,action:all
+#   end
+#
+# You can nest calls to #with_comments, which will combine the
+# entries from both calls:
+#
+#   DB.with_comments(application: App, path: :scrubbed_path) do
+#     DB.with_comments(model: Album, action: :all) do
+#       ds = DB[:albums].all
+#       # SELECT * FROM albums
+#       # -- application:App,path:scrubbed_path,model:Album,action:all
+#     end
+#   end
+#
+# You can override comment entries specified in earlier blocks, or
+# remove entries specified earlier using a nil value:
+#
+#   DB.with_comments(application: App, path: :scrubbed_path) do
+#     DB.with_comments(application: Foo, path: nil) do
+#       ds = DB[:albums].all
+#       # SELECT * FROM albums # -- application:Foo
+#     end
+#   end
+#
+# You can combine block-level comments with dataset-specific
+# comments:
+#
+#   DB.with_comments(model: Album, action: :all) do
+#     DB[:table].comment("Some Comment").all
+#     # SELECT * FROM albums -- model:Album,action:all -- Some Comment
+#   end
+#
 # Note that Microsoft Access does not support inline comments,
 # and attempting to use comments on it will result in SQL syntax
 # errors.
 #
-# Related module: Sequel::SQLComments
+# Related modules: Sequel::SQLComments, Sequel::Database::SQLComments
 
 #
 module Sequel
@@ -62,7 +102,7 @@ module Sequel
     %w'select insert update delete'.each do |type|
       define_method(:"#{type}_sql") do |*a|
         sql = super(*a)
-        if comment = @opts[:comment]
+        if comment = _sql_comment
           # This assumes that the comment stored in the dataset has
           # already been formatted. If not, this could result in SQL
           # injection.
@@ -74,8 +114,10 @@ module Sequel
           if sql.frozen?
             sql += comment
             sql.freeze
-          else
+          elsif @opts[:append_sql] || @opts[:placeholder_literalizer]
             sql << comment
+          else
+            sql += comment
           end
         end
         sql
@@ -83,6 +125,11 @@ module Sequel
     end
 
     private
+
+    # The comment to include in the SQL query, if any.
+    def _sql_comment
+      @opts[:comment]
+    end
 
     # Format the comment.  For maximum compatibility, this uses a
     # single line SQL comment, and converts all consecutive whitespace
@@ -92,5 +139,63 @@ module Sequel
     end
   end
 
+  module Database::SQLComments
+    def self.extended(db)
+      db.instance_variable_set(:@comment_hashes, {})
+      db.extend_datasets DatasetSQLComments
+    end
+
+    # A map of threads to comment hashes, used for correctly setting
+    # comments for all queries inside #with_comments blocks.
+    attr_reader :comment_hashes
+
+    # Store the comment hash and use it to create comments inside the block
+    def with_comments(comment_hash)
+      hashes = @comment_hashes
+      t = Sequel.current
+      new_hash = if hash = Sequel.synchronize{hashes[t]}
+        hash.merge(comment_hash)
+      else
+        comment_hash.dup
+      end
+      yield Sequel.synchronize{hashes[t] = new_hash}
+    ensure
+      if hash
+        Sequel.synchronize{hashes[t] = hash}
+      else
+        t && Sequel.synchronize{hashes.delete(t)}
+      end
+    end
+
+    module DatasetSQLComments
+      include Sequel::SQLComments
+
+      # Include comments added via Database#with_comments in the output SQL.
+      def _sql_comment
+        specific_comment = super
+        return specific_comment if @opts[:append_sql]
+
+        t = Sequel.current
+        hashes = db.comment_hashes
+        block_comment = if comment_hash = Sequel.synchronize{hashes[t]}
+          comment_array = comment_hash.map{|k,v| "#{k}:#{v}" unless v.nil?}
+          comment_array.compact!
+          comment_array.join(",")
+        end
+
+        if block_comment
+          if specific_comment
+            format_sql_comment(block_comment + specific_comment)
+          else
+            format_sql_comment(block_comment)
+          end
+        else
+          specific_comment
+        end
+      end
+    end
+  end
+
   Dataset.register_extension(:sql_comments, SQLComments)
+  Database.register_extension(:sql_comments, Database::SQLComments)
 end
--- spec/extensions/sql_comments_spec.rb
+++ spec/extensions/sql_comments_spec.rb
@@ -1,9 +1,7 @@
 require_relative "spec_helper"
 
-describe "sql_comments extension" do
-  before do
-    @ds = Sequel.mock[:t].extension(:sql_comments)
-  end
+shared_specs = Module.new do
+  extend Minitest::Spec::DSL
 
   it "should not add a comment if one is not set for the dataset" do
     @ds.select_sql.must_equal 'SELECT * FROM t'
@@ -18,6 +16,20 @@ describe "sql_comments extension" do
     ds.insert_sql(:a=>1).must_equal "INSERT INTO t (a) VALUES (1) -- Some Comment Here\n"
     ds.delete_sql.must_equal "DELETE FROM t -- Some Comment Here\n"
     ds.update_sql(:a=>1).must_equal "UPDATE t SET a = 1 -- Some Comment Here\n"
+  end
+
+  it "should not add a comment multiple times" do
+    ds = @ds.comment("Some\nComment\r\n Here")
+    ds.sql.must_equal "SELECT * FROM t -- Some Comment Here\n"
+    ds.sql.must_equal "SELECT * FROM t -- Some Comment Here\n"
+  end
+
+  it "should not add a comment multiple times" do
+    ds = @ds.comment("Some\nComment\r\n Here")
+    5.times do
+      ds.first(:x=>1)
+      ds.db.sqls.must_equal ["SELECT * FROM t WHERE (x = 1) LIMIT 1 -- Some Comment Here\n"]
+    end
   end
 
   it "should handle comments used in nested datasets" do
@@ -37,5 +49,83 @@ describe "sql_comments extension" do
     @ds = Sequel.mock[:t].with_extend{def select_sql; super.freeze; end}.extension(:sql_comments)
     ds = @ds.comment("Some\nComment\r\n Here")
     ds.select_sql.must_equal "SELECT * FROM t -- Some Comment Here\n"
+  end
+end
+
+describe "sql_comments dataset extension" do
+  before do
+    @ds = Sequel.mock[:t].extension(:sql_comments)
+  end
+  
+  include shared_specs
+end
+
+describe "sql_comments database extension" do
+  before do
+    @db = Sequel.mock.extension(:sql_comments)
+    @ds = @db[:t]
+  end
+  
+  include shared_specs
+
+  it "should support setting comments for all queries executed inside a with_comments block" do
+    @db.with_comments(:foo=>'bar', :baz=>'quux') do
+      @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+    end
+  end
+
+  it "should work if loading the extension multiple times" do
+    @db.with_comments(:foo=>'bar', :baz=>'quux') do
+      @db.extension :sql_comments
+      @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+    end
+  end
+
+  it "should support nesting with_comments blocks" do
+    @db.with_comments(:foo=>'bar') do
+      @db.with_comments(:baz=>'quux') do
+        @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+      end
+    end
+  end
+
+  it "should support nesting with_comments blocks multiple times" do
+    @db.with_comments(:foo=>'bar') do
+      @db.with_comments(:baz=>'quux') do
+        @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+      end
+      @db.with_comments(:x=>'y') do
+        @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,x:y\n"
+      end
+    end
+  end
+
+  it "should support overridding values in nested blocks" do
+    @db.with_comments(:foo=>'bar', :baz=>'q') do
+      @db.with_comments(:baz=>'quux') do
+        @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+      end
+    end
+  end
+
+  it "should support removing values in nested using nil" do
+    @db.with_comments(:foo=>'bar', :bat=>'q') do
+      @db.with_comments(:baz=>'quux', :bat=>nil) do
+        @ds.select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux\n"
+      end
+    end
+  end
+
+  it "should support combining with dataset-specific comments" do
+    @db.with_comments(:foo=>'bar', :baz=>'quux') do
+      @ds.comment('specific').select_sql.must_equal "SELECT * FROM t -- foo:bar,baz:quux -- specific \n"
+    end
+  end
+
+  it "should only use block level comments for main dataset, not for nested datasets" do
+    @db.with_comments(:foo=>'bar', :baz=>'quux') do
+      ds = @ds.comment("Some\nComment\r\n Here")
+      ds.where(:id=>ds).select_sql.must_equal "SELECT * FROM t WHERE (id IN (SELECT * FROM t -- Some Comment Here\n)) -- foo:bar,baz:quux -- Some Comment Here \n"
+    end
   end
 end
