8b9597530aaa3bbf852fc6f9ec0b70f15ddfb08f
Jeremy Evans <code@jeremyevans.net>
2021-12-15 15:18:57 -0800

    Remove coverage workaround not needed in Ruby 3

--- spec/sequel_coverage.rb
+++ spec/sequel_coverage.rb
@@ -6,49 +6,6 @@ def SimpleCov.sequel_coverage(opts = {})
     enable_coverage :branch
     command_name SEQUEL_COVERAGE unless SEQUEL_COVERAGE == "1"
 
-    # Work around Ruby Bug #16967
-    Coverage.singleton_class.prepend(Module.new do
-      def result
-        res = super
-        check_branch = true
-        skip_2nd = lambda do |ary|
-          ary = ary.dup
-          ary.slice!(1)
-          ary
-        end
-        res.values.each do |hash|
-          if check_branch
-            unless hash.is_a?(Hash) && hash[:branches]
-              return res
-            end
-            check_branch = false
-          end
-          unique_branches = {}
-          branch_counters = {}
-          new_branches = {}
-          branches = hash[:branches]
-          branches.each do |k, v|
-            new_k = skip_2nd[k]
-            if branch_values = unique_branches[new_k]
-              v.each do |k1, v1|
-                branch_counters[skip_2nd[k1]] += v1
-              end
-              branch_values.keys.each do |k1|
-                branch_values[k1] = branch_counters[skip_2nd[k1]]
-              end
-            else
-              unique_branches[new_k] = new_branches[k] = v
-              v.each do |k1, v1|
-                branch_counters[skip_2nd[k1]] = v1
-              end
-            end
-          end
-          hash[:branches] = new_branches
-        end
-        res
-      end
-    end)
-
     add_filter "/spec/"
     add_group('Missing-Revelent'){|src| src.filename =~ opts[:group] && src.covered_percent < 100} if opts[:group]
     add_group('Missing'){|src| src.covered_percent < 100}
