e1e91549e3485f221f06bb8f5e14af725dfc7777
Jeremy Evans <code@jeremyevans.net>
2022-09-30 13:29:52 -0700

    Bump version to 5.61.0

--- lib/sequel/version.rb
+++ lib/sequel/version.rb
@@ -6,11 +6,11 @@ module Sequel
 
   # The minor version of Sequel.  Bumped for every non-patch level
   # release, generally around once a month.
-  MINOR = 60
+  MINOR = 61
 
   # The tiny version of Sequel.  Usually 0, only bumped for bugfix
   # releases that fix regressions from previous versions.
-  TINY  = 1
+  TINY  = 0
   
   # The version of Sequel you are using, as a string (e.g. "2.11.0")
   VERSION = [MAJOR, MINOR, TINY].join('.').freeze
