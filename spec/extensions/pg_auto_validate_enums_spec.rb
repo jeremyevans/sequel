aa5ae71a664c129890fdab655ae72fc979c28c4c
Jeremy Evans <code@jeremyevans.net>
2025-03-31 08:49:19 -0700

    Test case where a column doesn't have enum values in pg_auto_validate_enums spec

--- spec/extensions/pg_auto_validate_enums_spec.rb
+++ spec/extensions/pg_auto_validate_enums_spec.rb
@@ -24,6 +24,19 @@ describe "pg_auto_validate_enums plugin" do
     @o.errors.must_equal({:c => ['is not in range or set: ["a", "b", "test"]']})
   end
 
+  it "handles case where no enum columns exist in model's table" do
+    @db = Sequel.mock(:host=>'postgres')
+    def @db.schema(table, *)
+      table = table.first_source if table.is_a?(Sequel::Dataset)
+      [[:c, {}]]
+    end
+    @c = Sequel::Model(@db[:test])
+    @c.plugin :pg_auto_validate_enums
+    @o = @c.new
+    @o.c = "a"
+    @o.valid?.must_equal true
+  end
+
   it "validates underlying enum value, not method return value" do
     @o.c = "a"
     def @o.c; "invalid" end
