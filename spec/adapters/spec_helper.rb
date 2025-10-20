0aef818d3b031b6606efc0db3cf287c826b86a82
Jeremy Evans <code@jeremyevans.net>
2025-10-20 09:20:07 -0700

    Allow running integration/adapter specs with split_values plugin using SEQUEL_SPLIT_VALUES

--- spec/adapters/spec_helper.rb
+++ spec/adapters/spec_helper.rb
@@ -22,6 +22,7 @@ Sequel::Model.cache_associations = false if ENV['SEQUEL_NO_CACHE_ASSOCIATIONS']
 Sequel::Model.plugin :prepared_statements if ENV['SEQUEL_MODEL_PREPARED_STATEMENTS']
 Sequel::Model.plugin :throw_failures if ENV['SEQUEL_MODEL_THROW_FAILURES']
 Sequel::Model.plugin :primary_key_lookup_check_values if ENV['SEQUEL_PRIMARY_KEY_LOOKUP_CHECK_VALUES']
+Sequel::Model.plugin :split_values if ENV["SEQUEL_SPLIT_VALUES"]
 Sequel::Model.use_transactions = false
 Sequel::Model.cache_anonymous_models = false
 
